59878d62e074f11bef74770c4e6c87beb9dc21c9
bob <bob@vbox-ubuntu-16.04>
2018-07-01 18:05:17 -0700

    allow running on Bonn cloudlet. ACME and cloudflare used to register FQDN with proper certificates. Added https endpoint which can reverse proxy services in private networks.

--- openstack-tenant/agent/server/handlers.go
+++ openstack-tenant/agent/server/handlers.go
@@ -3,14 +3,27 @@ package server
 import (
 	"fmt"
 
+	log "gitlab.com/bobbae/logrus"
+
 	"golang.org/x/net/context"
 
 	"github.com/mobiledgex/edge-cloud-infra/k8s-prov/k8sopenstack"
 	"github.com/mobiledgex/edge-cloud-infra/openstack-tenant/agent/api"
+
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
 )
 
 //Server for GRPC
 type Server struct{}
+
+var proxyMap map[string]string
+
+func init() {
+	proxyMap = make(map[string]string)
+}
 
 //Provision a kubernetes cluster on openstack for the given Tenant
 func (srv *Server) Provision(ctx context.Context, req *api.ProvisionRequest) (res *api.ProvisionResponse, err error) {
@@ -23,7 +36,7 @@ func (srv *Server) Provision(ctx context.Context, req *api.ProvisionRequest) (re
 
 	res = &api.ProvisionResponse{
 		Message: req.Message,
-		Status:  "ok",
+		Status:  "OK",
 	}
 
 	return res, nil
@@ -48,26 +61,70 @@ func (srv *Server) Destroy(ctx context.Context, req *api.ProvisionRequest) (res 
 
 //Proxy traffic betwwen kubernetes pods, via openstack subnet/network, iptables, and external network
 func (srv *Server) Proxy(ctx context.Context, req *api.ProxyRequest) (res *api.ProxyResponse, err error) {
+	if len(req.Proxies) < 1 {
+		return nil, fmt.Errorf("missing proxy definitions")
+	}
+	if req.Message == "add" {
+		//TODO layer 4 proxy
+		for _, p := range req.Proxies {
+			err := addOrigin(p.Path, p.Origin)
+			if err != nil {
+				res := &api.ProxyResponse{
+					Message: fmt.Sprintf("Error, cannot add proxy %s %s, %v", p.Path, p.Origin, err),
+				}
+				return res, err
+			}
+		}
+	} else {
+		//TODO list
+		res := &api.ProxyResponse{
+			Message: fmt.Sprintf("Error, invalid request %s", req.Message),
+		}
+		return res, err
+	}
+
 	res = &api.ProxyResponse{
-		Message: req.Message,
+		Message: "OK",
 	}
 
 	return res, nil
 }
 
-//FQDN serves name to IP address mapping for potentially ephemeral kubernetes services/deployments/pods
-func (srv *Server) FQDN(ctx context.Context, req *api.FQDNRequest) (res *api.FQDNResponse, err error) {
-	res = &api.FQDNResponse{
-		Message: req.Message,
+func addOrigin(path, origin string) error {
+	log.Debugf("addOrigin path %s origin %s", path, origin)
+
+	originURL, err := url.Parse(origin)
+	if err != nil {
+		return fmt.Errorf("Cannot parse origin %s,%v", origin, err)
 	}
 
-	return res, nil
+	director := func(req *http.Request) {
+		req.Header.Add("X-Forwarded-Host", req.Host)
+		req.Header.Add("X-Origin-Host", originURL.Host)
+		req.URL.Scheme = originURL.Scheme
+		req.URL.Host = originURL.Host
+		if strings.HasPrefix(req.URL.Path, path) {
+			req.URL.Path = strings.Replace(req.URL.Path, path, "/", 1)
+		} else {
+			log.Warningf("invalid URL path %s missing %s", req.URL.Path, path)
+		}
+		log.Debugf("director req %v", req)
+	}
+
+	proxy := &httputil.ReverseProxy{Director: director}
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		log.Debugf("path %s, request %v", path, r)
+		proxy.ServeHTTP(w, r)
+	})
+
+	proxyMap[path] = origin
+	return nil
 }
 
 //Status returns status information of the kubernetes cluster on openstack for a given tenant
 func (srv *Server) Status(ctx context.Context, req *api.StatusRequest) (res *api.StatusResponse, err error) {
 	res = &api.StatusResponse{
-		Message: req.Message,
+		Message: "OK",
 	}
 
 	return res, nil
--- openstack-tenant/qcow2/mobiledgex-16.04-qcow2/mobiledgex-init.sh
+++ openstack-tenant/qcow2/mobiledgex-16.04-qcow2/mobiledgex-init.sh
@@ -10,13 +10,25 @@ mkdir $MCONF
 mount `blkid -t LABEL="config-2" -odevice` $MCONF
 skipinit=`cat $MCONF/openstack/latest/meta_data.json |jq .meta.skipinit | sed -e 's/"//'g`
 if [ "$skipinit" != "yes" ]; then
-	echo not skipping >> /tmp/mobiledgex.log
+	echo mobiledgex initialization >> /tmp/mobiledgex.log
 	echo nameserver 8.8.8.8 > /etc/resolv.conf
 	ipaddress=`cat $MCONF/openstack/latest/network_data.json |jq .networks[0].ip_address | sed -e 's/"//'g` 
-	ifconfig ens3  $ipaddress up
-	ifconfig ens3 netmask `cat $MCONF/openstack/latest/network_data.json |jq .networks[0].netmask | sed -e 's/"//'g` up
-	edgeproxy=`cat $MCONF/openstack/latest/meta_data.json| jq .meta.edgeproxy | sed -e 's/"//'g` 
-	ip route add default via $edgeproxy dev ens3
+	nettype=`cat $MCONF/openstack/latest/network_data.json |jq .networks[0].type | sed -e 's/"//'g` 
+	if [ "$nettype" = "ipv4_dhcp" ]; then
+		echo using dhcp >> /tmp/mobiledgex.log
+		dhclient ens3
+	else
+		ifconfig ens3  $ipaddress up
+		ifconfig ens3 netmask `cat $MCONF/openstack/latest/network_data.json |jq .networks[0].netmask | sed -e 's/"//'g` up
+		edgeproxy=`cat $MCONF/openstack/latest/meta_data.json| jq .meta.edgeproxy | sed -e 's/"//'g` 
+		ip route add default via $edgeproxy dev ens3
+	fi
+	privatenet=`cat $MCONF/openstack/latest/meta_data.json |jq .meta.privatenet | sed -e 's/"//'g`
+	privaterouter=`cat $MCONF/openstack/latest/meta_data.json |jq .meta.privaterouter | sed -e 's/"//'g`
+	if [ "$privatenet" != "" -a "$privaterouter" != "" ]; then
+		echo private route entry $privatenet via $privaterouter >> /tmp/mobiledgex.log
+		ip route add $privatenet via $privaterouter dev ens3
+	fi
 	ifconfig -a >>/tmp/mobiledgex.log
 	ip route >> /tmp/mobiledgex.log
 	hostname `cat $MCONF/openstack/latest/meta_data.json |jq .name | sed -e 's/"//'g`
@@ -27,26 +39,33 @@ if [ "$skipinit" != "yes" ]; then
 	echo 127.0.1.1 `hostname` >> /tmp/hosts
 	cp /tmp/hosts /etc/hosts
 	skipk8s=`cat $MCONF/openstack/latest/meta_data.json |jq .meta.skipk8s | sed -e 's/"//'g`
-	if [ "$skipk8s" != "yes" ]; then
-		echo not skipping k8s init >> /tmp/mobiledgex.log
-		masteraddr=`cat $MCONF/openstack/latest/meta_data.json |jq .meta.k8smaster | sed -e 's/"//'g`
-		if [ "$role" = "k8s-master" ]; then
-			echo k8s-master init >> /tmp/mobiledgex.log
-			/home/mobiledgex/k8s/install-k8s-base.sh >> /tmp/mobiledgex.log
-			echo k8s-base installed >> /tmp/mobiledgex.log
-			/home/mobiledgex/k8s/install-k8s-master.sh ens3 $masteraddr $ipaddress >> /tmp/mobiledgex.log
-			echo k8s-master installed >> /tmp/mobiledgex.log
+	if [ "$role" = "mex-agent-node" ]; then
+		echo "initializing mex agent node" >> /tmp/mobiledgex.log
+		/home/mobiledgex/k8s/install-k8s-base.sh >> /tmp/mobiledgex.log
+		chmod a+rw /var/run/docker.sock
+	else 
+		if [ "$skipk8s" != "yes" ]; then
+			echo doing k8s init >> /tmp/mobiledgex.log
+			masteraddr=`cat $MCONF/openstack/latest/meta_data.json |jq .meta.k8smaster | sed -e 's/"//'g`
+			if [ "$role" = "k8s-master" ]; then
+				echo k8s-master init >> /tmp/mobiledgex.log
+				/home/mobiledgex/k8s/install-k8s-base.sh >> /tmp/mobiledgex.log
+				echo k8s-base installed >> /tmp/mobiledgex.log
+				/home/mobiledgex/k8s/install-k8s-master.sh ens3 $masteraddr $ipaddress >> /tmp/mobiledgex.log
+				echo k8s-master installed >> /tmp/mobiledgex.log
+			elif [ "$role" = "k8s-node" ]; then
+				echo k8s-node init >> /tmp/mobiledgex.log
+				/home/mobiledgex/k8s/install-k8s-base.sh >> /tmp/mobiledgex.log
+				echo k8s-base installed >> /tmp/mobiledgex.log
+				/home/mobiledgex/k8s/install-k8s-node.sh  ens3 $masteraddr $ipaddress >> /tmp/mobiledgex.log
+				echo k8s-node installed >> /tmp/mobiledgex.log
+			else
+				echo "role is " $role " and not k8s" >> /tmp/mobiledgex.log
+			fi
+			echo done k8s init for role $role >> /tmp/mobiledgex.log
+		else
+			echo skipping k8s init for role $role >> /tmp/mobiledgex.log
 		fi
-		if [ "$role" = "k8s-node" ]; then
-			echo k8s-node init >> /tmp/mobiledgex.log
-			/home/mobiledgex/k8s/install-k8s-base.sh >> /tmp/mobiledgex.log
-			echo k8s-base installed >> /tmp/mobiledgex.log
-			/home/mobiledgex/k8s/install-k8s-node.sh  ens3 $masteraddr $ipaddress >> /tmp/mobiledgex.log
-			echo k8s-node installed >> /tmp/mobiledgex.log
-		fi
-		echo done k8s init for role $role >> /tmp/mobiledgex.log
-	else
-		echo skipping k8s init for role $role >> /tmp/mobiledgex.log
 	fi
 	echo $role > /etc/mobiledgex-role.txt
 	echo done mobiledgex init >> /tmp/mobiledgex.log

4f17e9a944d5006c5e43e9dbe45fe4b45e95a55a
Jim <jim.morris@mobiledgex.com>
2018-12-08 13:56:54 -0600

    update external network for router

--- openstack-prov/oscliapi/tdg.go
+++ openstack-prov/oscliapi/tdg.go
@@ -163,7 +163,7 @@ func PrepNetwork() error {
 		if err != nil {
 			return fmt.Errorf("cannot create %s, %v", eMEXExternalRouter, err)
 		}
-		err = SetRouter(eMEXExternalRouter, defaultMEXExternalNetwork)
+		err = SetRouter(eMEXExternalRouter, eMEXExternalNetwork)
 		if err != nil {
 			return fmt.Errorf("cannot set default network to router %s, %v", eMEXExternalRouter, err)
 		}

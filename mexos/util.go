bf86510db059858303ed0bc6008cab0f2a248268
Jim <jim.morris@mobiledgex.com>
2019-03-11 16:35:37 -0500

    CRM AppInst speedup part3 (#48)
    * speedup part 3
    
    * cleanup

--- mexos/util.go
+++ mexos/util.go
@@ -4,15 +4,29 @@ import (
 	"fmt"
 
 	"github.com/mobiledgex/edge-cloud/edgeproto"
+	"github.com/mobiledgex/edge-cloud/log"
 )
 
 // AddProxySecurityRulesAndPatchDNS Adds security rules and dns records in parallel
 func AddProxySecurityRulesAndPatchDNS(rootLB *MEXRootLB, kp *kubeParam, kubeNames *KubeNames, appInst *edgeproto.AppInst) error {
 	secchan := make(chan string)
 	dnschan := make(chan string)
+	proxychan := make(chan string)
 
+	ports, err := GetPortDetail(appInst)
+	if err != nil {
+		return err
+	}
 	go func() {
-		err := AddProxySecurityRules(rootLB, kp.ipaddr, kubeNames.appName, appInst)
+		err = AddNginxProxy(rootLB.Name, kp.ipaddr, kubeNames.appName, ports, "")
+		if err == nil {
+			proxychan <- ""
+		} else {
+			proxychan <- err.Error()
+		}
+	}()
+	go func() {
+		err := AddSecurityRules(ports)
 		if err == nil {
 			secchan <- ""
 		} else {
@@ -27,11 +41,20 @@ func AddProxySecurityRulesAndPatchDNS(rootLB *MEXRootLB, kp *kubeParam, kubeName
 			dnschan <- err.Error()
 		}
 	}()
+	proxyerr := <-proxychan
 	secerr := <-secchan
 	dnserr := <-dnschan
 
-	if secerr != "" || dnserr != "" {
-		return fmt.Errorf("AddProxySecurityRulesAndPatchDNS error -- secerr: %v dnserr: %v", secerr, dnserr)
+	if proxyerr != "" || secerr != "" || dnserr != "" {
+		if proxyerr == "" {
+			// delete the nginx proxy if it worked but something else failed because it can cause subequent attempts to fail
+			// cleanup of security rules and DNS is not as important
+			err := DeleteNginxProxy(rootLB.Name, kubeNames.appName)
+			if err != nil {
+				log.InfoLog("cleanup nginx proxy Failed", "err", err)
+			}
+		}
+		return fmt.Errorf("AddProxySecurityRulesAndPatchDNS error -- proxyerr: %v secerr: %v dnserr: %v", proxyerr, secerr, dnserr)
 	}
 	return nil
 }

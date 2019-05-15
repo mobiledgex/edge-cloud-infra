b01ea064d96e220ecd43b54fb7cf9334f424979d
mchu1195 <matthew.chu@mobiledgex.com>
2019-05-15 10:14:18 -0700

    EDGECLOUD-538 give the correct error when no token is provided (#138)

--- mc/orm/userauth.go
+++ mc/orm/userauth.go
@@ -126,15 +126,11 @@ func AuthCookie(next echo.HandlerFunc) echo.HandlerFunc {
 		l := len(scheme)
 		if len(auth) <= len(scheme) || !strings.HasPrefix(auth, scheme) {
 			//if no token provided, return a 400 err
-			err := fmt.Errorf("no token found for Authorization Bearer")
-			if auth == scheme {
-				return &echo.HTTPError{
-					Code:     http.StatusBadRequest,
-					Message:  "no token found",
-					Internal: err,
-				}
+			return &echo.HTTPError{
+				Code:     http.StatusBadRequest,
+				Message:  "no bearer token found",
+				Internal: fmt.Errorf("no token found for Authorization Bearer"),
 			}
-			return err
 		}
 
 		cookie := auth[l+1:]

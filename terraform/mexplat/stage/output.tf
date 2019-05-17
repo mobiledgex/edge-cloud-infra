122d7e6cd175aaec5a82a13a21c0f95bc6aac5e3
Venky <venky.tumkur@mobiledgex.com>
2019-05-18 00:02:17 +0530

    EDGECLOUT- 383 Stage 2 (#145)
    * Gitlab access token creation
    * Include postgres in the deployment
    * Prompt for edge-cloud version to deploy when deploying manually
    * Switch Jenkins job to use staging playbook
    * Multiple CRM support
    * Auto-upgrade controller etcd
    * Backup etcd database to Artifactory before controller upgrade

--- terraform/mexplat/stage/output.tf
+++ terraform/mexplat/stage/output.tf
@@ -16,6 +16,11 @@ output "mc_external_ip" {
 	value = "${module.gitlab.external_ip}"
 }
 
+# Same as the Gitlab VM
+output "postgres_external_ip" {
+	value = "${module.gitlab.external_ip}"
+}
+
 output "k8s_cluster_name" {
 	value = "${var.cluster_name}"
 }

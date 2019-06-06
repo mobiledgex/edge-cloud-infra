7c4b3c1a37d25afd9685f53e7db1219b33336f59
mchu1195 <matthew.chu@mobiledgex.com>
2019-06-06 14:57:14 -0700

    EDGECLOUD-583 added a check for flavor sizes in cluster create (#169)
    * EDGECLOUD-583 added a check for flavor sizes in cluster create

--- crm-platforms/openstack/openstack-cluster.go
+++ crm-platforms/openstack/openstack-cluster.go
@@ -1,6 +1,7 @@
 package openstack
 
 import (
+	"fmt"
 	"github.com/mobiledgex/edge-cloud-infra/mexos"
 	"github.com/mobiledgex/edge-cloud/cloudcommon"
 	"github.com/mobiledgex/edge-cloud/edgeproto"
@@ -18,6 +19,13 @@ func (s *Platform) CreateClusterInst(clusterInst *edgeproto.ClusterInst) error {
 	lbName := s.rootLBName
 	if clusterInst.IpAccess == edgeproto.IpAccess_IP_ACCESS_DEDICATED {
 		lbName = cloudcommon.GetDedicatedLBFQDN(s.cloudletKey, &clusterInst.Key.ClusterKey)
+	}
+	//find the flavor and check the disk size
+	for _, flavor := range s.flavorList {
+		if flavor.Name == clusterInst.NodeFlavor && flavor.Disk < MINIMUM_DISK_SIZE {
+
+			return fmt.Errorf("Insufficient disk size, please specify a flavor with at least %dgb", MINIMUM_DISK_SIZE)
+		}
 	}
 	return mexos.CreateCluster(lbName, clusterInst)
 }

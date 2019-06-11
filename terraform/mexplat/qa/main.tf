f651f37aeb996535b8a79940d556fdfed9f918e0
Venky <venky.tumkur@mobiledgex.com>
2019-06-11 23:02:46 +0530

    Console VMs need more disk space (#180)
    Dockerising console increased the disk space requirements for the VMs.

--- terraform/mexplat/qa/main.tf
+++ terraform/mexplat/qa/main.tf
@@ -57,6 +57,7 @@ module "console" {
 
   instance_name       = "${var.console_instance_name}"
   zone                = "${var.gcp_zone}"
+  boot_disk_size      = 100
   tags                = [ "http-server", "https-server", "console-debug", "mc" ]
 }
 

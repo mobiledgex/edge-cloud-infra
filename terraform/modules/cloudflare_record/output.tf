a3c88bf517edf2aded396a83196a078947761e71
Venky Tumkur <venky.tumkur@mobiledgex.com>
2019-06-21 12:19:11 +0530

    Initial commit for registry replicas

---
+++ terraform/modules/cloudflare_record/output.tf
@@ -0,0 +1,3 @@
+output "hostname" {
+  value = "${cloudflare_record.vm.hostname}"
+}
--- terraform/modules/vm_gcp/output.tf
+++ terraform/modules/vm_gcp/output.tf
@@ -1,3 +1,7 @@
 output "external_ip" {
   value = "${google_compute_instance.vm.network_interface.0.access_config.0.nat_ip}"
 }
+
+output "zone" {
+  value = "${google_compute_instance.vm.zone}"
+}

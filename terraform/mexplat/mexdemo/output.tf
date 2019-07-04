85298a61d3fa7da9c66a8d3c45978fa575075596
Venky <venky.tumkur@mobiledgex.com>
2019-07-04 10:57:42 +0530

    EU controller/cluster has been removed from mexdemo (#227)

--- terraform/mexplat/mexdemo/output.tf
+++ terraform/mexplat/mexdemo/output.tf
@@ -6,14 +6,6 @@ output "k8s_cluster_name" {
   value = "${var.cluster_name}"
 }
 
-output "eu_kube_config" {
-  value = "${module.k8s_eu.kube_config}"
-}
-
-output "eu_k8s_cluster_name" {
-  value = "${var.eu_cluster_name}"
-}
-
 output "k8s_clusters" {
   value = [
     {
@@ -21,10 +13,5 @@ output "k8s_clusters" {
       "kube_config" = "${module.k8s.kube_config}"
       "region" = "US"
     },
-    {
-      "name" = "${var.eu_cluster_name}"
-      "kube_config" = "${module.k8s_eu.kube_config}"
-      "region" = "EU"
-    }
   ]
 }

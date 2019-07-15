bf4ee9a318cc523ad016d9c6a0c2985f2d434a59
Jim <jim.morris@mobiledgex.com>
2019-07-15 17:10:50 -0500

    dme plugins

--- operator-api-gw/defaultoperator/defaultoperator.go
+++ operator-api-gw/defaultoperator/defaultoperator.go
@@ -1,16 +1,20 @@
 package defaultoperator
 
 import (
+	simulatedqos "github.com/mobiledgex/edge-cloud-infra/operator-api-gw/defaultoperator/simulated-qos"
 	dmecommon "github.com/mobiledgex/edge-cloud/d-match-engine/dme-common"
 	dme "github.com/mobiledgex/edge-cloud/d-match-engine/dme-proto"
+	"github.com/mobiledgex/edge-cloud/log"
 
 	"fmt"
 )
 
 //OperatorApiGw respresent an Operator API Gateway
 type OperatorApiGw struct {
-	LocationVerificationURL string
-	QosPosEndpoint          string
+	VaultAddr string
+	QosPosUrl string
+	LocVerUrl string
+	TokSrvUrl string
 }
 
 func (OperatorApiGw) GetOperatorName() string {
@@ -18,14 +22,20 @@ func (OperatorApiGw) GetOperatorName() string {
 }
 
 // Init is called once during startup.
-func (*OperatorApiGw) Init(operatorName string) error {
-	return nil // nothing to do
+func (o *OperatorApiGw) Init(operatorName, vaultAddr, qosPosUrl, locVerUrl, tokSrvUrl string) error {
+	log.DebugLog(log.DebugLevelDmereq, "init for default operator")
+	o.QosPosUrl = qosPosUrl
+	o.LocVerUrl = locVerUrl
+	o.TokSrvUrl = tokSrvUrl
+	o.VaultAddr = vaultAddr
+	return nil
 }
 
-func (*OperatorApiGw) VerifyLocation(mreq *dme.VerifyLocationRequest, mreply *dme.VerifyLocationReply, ckey *dmecommon.CookieKey, locVerUrl string, tokSrvUrl string) (*dme.VerifyLocationReply, error) {
+func (*OperatorApiGw) VerifyLocation(mreq *dme.VerifyLocationRequest, mreply *dme.VerifyLocationReply, ckey *dmecommon.CookieKey) (*dme.VerifyLocationReply, error) {
 	return nil, fmt.Errorf("Verify Location not supported for this operator")
 }
 
-func (*OperatorApiGw) GetQOSPositionKPI(req *dme.QosPositionKpiRequest, getQosSvr dme.MatchEngineApi_GetQosPositionKpiServer) error {
-	return fmt.Errorf("Get QOS Position KPI not supported for this operator")
+func (*OperatorApiGw) GetQOSPositionKPI(mreq *dme.QosPositionKpiRequest, getQosSvr dme.MatchEngineApi_GetQosPositionKpiServer) error {
+	log.DebugLog(log.DebugLevelDmereq, "getting simulated results for operator with no QOS Pos implementation")
+	return simulatedqos.GetSimulatedQOSPositionKPI(mreq, getQosSvr)
 }
--- operator-api-gw/tdg/tdg.go
+++ operator-api-gw/tdg/tdg.go
@@ -6,64 +6,21 @@ import (
 	"github.com/mobiledgex/edge-cloud/log"
 
 	"fmt"
-	"math/rand"
-	"time"
+
+	simulatedqos "github.com/mobiledgex/edge-cloud-infra/operator-api-gw/defaultoperator/simulated-qos"
+	tdgclient "github.com/mobiledgex/edge-cloud-infra/operator-api-gw/tdg/tdg-qos/qosclient"
 )
+
+var QosClientCert = "qosclient.crt"
+var QosClientKey = "qosclient.key"
+var QoServerCert = "qosserver.crt"
 
 //OperatorApiGw respresent an Operator API Gateway
 type OperatorApiGw struct {
-	LocationVerificationURL string
-	QosPosEndpoint          string
-}
-
-// make a random number near the range of the base value (plus or minus 10)
-func randomInRange(baseval int) float32 {
-	rand.Seed(time.Now().UnixNano())
-	min := baseval - 10
-	if min < 0 {
-		min = 0
-	}
-	max := baseval + 10
-	result := rand.Intn(max-min) + min
-	// add some fraction less than 1
-	return float32(result) + rand.Float32()
-}
-
-// getQosResults currently just returns some fake results
-func getQosResults(qosres *dme.QosPositionResult) {
-	qosres.DluserthroughputMin = randomInRange(1)
-	qosres.DluserthroughputMax = randomInRange(100)
-	qosres.DluserthroughputAvg = randomInRange(50)
-	qosres.UluserthroughputMin = randomInRange(1)
-	qosres.UluserthroughputMax = randomInRange(50)
-	qosres.UluserthroughputAvg = randomInRange(25)
-	qosres.LatencyMin = randomInRange(20)
-	qosres.LatencyMax = randomInRange(60)
-	qosres.LatencyAvg = randomInRange(40)
-}
-
-func getQosPositionKpi(mreq *dme.QosPositionKpiRequest, getQosSvr dme.MatchEngineApi_GetQosPositionKpiServer) error {
-	log.DebugLog(log.DebugLevelDmereq, "getQosPositionKpi", "request", mreq)
-
-	var mreply dme.QosPositionKpiReply
-
-	mreply.Status = dme.ReplyStatus_RS_SUCCESS
-
-	for _, p := range mreq.Positions {
-		pid := p.Positionid
-		var qosres dme.QosPositionResult
-
-		qosres.Positionid = pid
-		qosres.GpsLocation = p.GpsLocation
-		getQosResults(&qosres)
-		log.DebugLog(log.DebugLevelDmereq, "Position", "pid", pid, "qosres", qosres)
-
-		mreply.PositionResults = append(mreply.PositionResults, &qosres)
-	}
-
-	getQosSvr.Send(&mreply)
-	return nil
-
+	VaultAddr string
+	QosPosUrl string
+	LocVerUrl string
+	TokSrvUrl string
 }
 
 func (OperatorApiGw) GetOperatorName() string {
@@ -71,34 +28,32 @@ func (OperatorApiGw) GetOperatorName() string {
 }
 
 // Init is called once during startup.
-func (*OperatorApiGw) Init(operatorName string) error {
-	return nil // nothing to do
+func (o *OperatorApiGw) Init(operatorName, vaultAddr, qosPosUrl, locVerUrl, tokSrvUrl string) error {
+	log.DebugLog(log.DebugLevelDmereq, "init for tdg operator")
+	o.QosPosUrl = qosPosUrl
+	o.LocVerUrl = locVerUrl
+	o.TokSrvUrl = tokSrvUrl
+	o.VaultAddr = vaultAddr
+
+	if o.QosPosUrl != "" {
+		err := tdgclient.GetQosCertsFromVault(o.VaultAddr)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func (*OperatorApiGw) VerifyLocation(mreq *dme.VerifyLocationRequest, mreply *dme.VerifyLocationReply, ckey *dmecommon.CookieKey, locVerUrl string, tokSrvUrl string) (*dme.VerifyLocationReply, error) {
+func (*OperatorApiGw) VerifyLocation(mreq *dme.VerifyLocationRequest, mreply *dme.VerifyLocationReply, ckey *dmecommon.CookieKey) (*dme.VerifyLocationReply, error) {
 	return nil, fmt.Errorf("Verify Location not yet implemented")
 }
 
-func (*OperatorApiGw) GetQOSPositionKPI(mreq *dme.QosPositionKpiRequest, getQosSvr dme.MatchEngineApi_GetQosPositionKpiServer) error {
-	log.DebugLog(log.DebugLevelDmereq, "TDG GetQOSPositionKPI", "request", mreq)
+func (o *OperatorApiGw) GetQOSPositionKPI(mreq *dme.QosPositionKpiRequest, getQosSvr dme.MatchEngineApi_GetQosPositionKpiServer) error {
+	log.DebugLog(log.DebugLevelDmereq, "TDG GetQOSPositionKPI", "QosPosUrl", o.QosPosUrl, "request", mreq)
 
-	var mreply dme.QosPositionKpiReply
-
-	mreply.Status = dme.ReplyStatus_RS_SUCCESS
-
-	for _, p := range mreq.Positions {
-		pid := p.Positionid
-		var qosres dme.QosPositionResult
-
-		qosres.Positionid = pid
-		qosres.GpsLocation = p.GpsLocation
-		getQosResults(&qosres)
-		log.DebugLog(log.DebugLevelDmereq, "Position", "pid", pid, "qosres", qosres)
-
-		mreply.PositionResults = append(mreply.PositionResults, &qosres)
+	if o.QosPosUrl == "" {
+		log.DebugLog(log.DebugLevelDmereq, "No QosPosUrl, getting simulated results")
+		return simulatedqos.GetSimulatedQOSPositionKPI(mreq, getQosSvr)
 	}
-
-	getQosSvr.Send(&mreply)
-	return nil
-
+	return tdgclient.GetQOSPositionKPIFromApiGW(o.QosPosUrl, mreq, getQosSvr)
 }

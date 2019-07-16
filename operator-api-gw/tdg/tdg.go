97cd3eb37a1e3b2c5c7769643b996c5900fa0655
Jim <jim.morris@mobiledgex.com>
2019-07-15 20:31:44 -0500

    DME plugin for Operator API GW (#241)
    * playbook and config updates for CRM with the various new parameters
    
    * dme plugin
    
    * dme plugins
    
    * remove file
    
    * remove certs
    
    * remove comment
    
    * remove file
    
    * add simulated qos
    
    * updates per review: delete binary and use common struct for
    server names

---
+++ operator-api-gw/tdg/tdg.go
@@ -0,0 +1,53 @@
+package tdg
+
+import (
+	"fmt"
+
+	simulatedqos "github.com/mobiledgex/edge-cloud-infra/operator-api-gw/defaultoperator/simulated-qos"
+	tdgclient "github.com/mobiledgex/edge-cloud-infra/operator-api-gw/tdg/tdg-qos/qosclient"
+	dmecommon "github.com/mobiledgex/edge-cloud/d-match-engine/dme-common"
+	dme "github.com/mobiledgex/edge-cloud/d-match-engine/dme-proto"
+	operator "github.com/mobiledgex/edge-cloud/d-match-engine/operator"
+	"github.com/mobiledgex/edge-cloud/log"
+)
+
+var QosClientCert = "qosclient.crt"
+var QosClientKey = "qosclient.key"
+var QoServerCert = "qosserver.crt"
+
+//OperatorApiGw respresent an Operator API Gateway
+type OperatorApiGw struct {
+	Servers *operator.OperatorApiGwServers
+}
+
+func (OperatorApiGw) GetOperatorName() string {
+	return "TDG"
+}
+
+// Init is called once during startup.
+func (o *OperatorApiGw) Init(operatorName string, servers *operator.OperatorApiGwServers) error {
+	log.DebugLog(log.DebugLevelDmereq, "init for tdg operator", "servers", servers)
+	o.Servers = servers
+
+	if o.Servers.QosPosUrl != "" {
+		err := tdgclient.GetQosCertsFromVault(o.Servers.VaultAddr)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (*OperatorApiGw) VerifyLocation(mreq *dme.VerifyLocationRequest, mreply *dme.VerifyLocationReply, ckey *dmecommon.CookieKey) (*dme.VerifyLocationReply, error) {
+	return nil, fmt.Errorf("Verify Location not yet implemented")
+}
+
+func (o *OperatorApiGw) GetQOSPositionKPI(mreq *dme.QosPositionKpiRequest, getQosSvr dme.MatchEngineApi_GetQosPositionKpiServer) error {
+	log.DebugLog(log.DebugLevelDmereq, "TDG GetQOSPositionKPI", "QosPosUrl", o.Servers.QosPosUrl, "request", mreq)
+
+	if o.Servers.QosPosUrl == "" {
+		log.DebugLog(log.DebugLevelDmereq, "No QosPosUrl, getting simulated results")
+		return simulatedqos.GetSimulatedQOSPositionKPI(mreq, getQosSvr)
+	}
+	return tdgclient.GetQOSPositionKPIFromApiGW(o.Servers.QosPosUrl, mreq, getQosSvr)
+}

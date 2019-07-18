e8ca432cc3321a953d6979739583a990104e35e5
Ashish Jain <ashish.jain@mobiledgex.com>
2019-07-18 13:17:23 +0530

    fix return value for mexdind cloudlet

--- crm-platforms/mexdind/mexdind-cloudlet.go
+++ crm-platforms/mexdind/mexdind-cloudlet.go
@@ -7,12 +7,10 @@ import (
 
 func (s *Platform) CreateCloudlet(cloudlet *edgeproto.Cloudlet, flavor *edgeproto.Flavor, updateCallback edgeproto.CacheUpdateCallback) error {
 	log.DebugLog(log.DebugLevelMexos, "create cloudlet for mexdind")
-	s.generic.CreateCloudlet(cloudlet, flavor, updateCallback)
-	return nil
+	return s.generic.CreateCloudlet(cloudlet, flavor, updateCallback)
 }
 
 func (s *Platform) DeleteCloudlet(cloudlet *edgeproto.Cloudlet) error {
 	log.DebugLog(log.DebugLevelMexos, "delete cloudlet for mexdind")
-	s.generic.DeleteCloudlet(cloudlet)
-	return nil
+	return s.generic.DeleteCloudlet(cloudlet)
 }

edb45386e28e9f82a79173bf4fd7993126f57789
Ashish Jain <ashish.jain@mobiledgex.com>
2019-07-19 16:19:29 +0530

    fixed issue: for deploymentlocal, ssh acl was not added to Openstack

--- mexos/rootlb.go
+++ mexos/rootlb.go
@@ -124,6 +124,18 @@ func SetupRootLB(rootLBName string, createRootLBFlavor string, updateCallback ed
 		}
 	}
 
+	// setup SSH access to cloudlet for CRM
+	log.DebugLog(log.DebugLevelMexos, "setup security group for SSH access")
+	groupName := GetCloudletSecurityGroup()
+	my_ip, err := GetExternalPublicAddr()
+	if err != nil {
+		return fmt.Errorf("unable to fetch public facing IP: %v", err)
+	}
+	if err := AddSecurityRuleCIDR(my_ip, "tcp", groupName, "22"); err != nil {
+		log.DebugLog(log.DebugLevelMexos, "cannot add security rule for ssh access", "error", err, "ip", my_ip)
+		return fmt.Errorf("unable to add security rule for ssh access, err: %v", err)
+	}
+
 	err = WaitForRootLB(rootLB)
 	if err != nil {
 		log.DebugLog(log.DebugLevelMexos, "timeout waiting for agent to run", "name", rootLB.Name)
--- mexos/uri.go
+++ mexos/uri.go
@@ -216,10 +216,14 @@ func DeleteFile(filePath string) error {
 
 // Get the externally visible public IP address
 func GetExternalPublicAddr() (string, error) {
-	out, err := sh.Command("dig", "@resolver1.opendns.com", "ANY", "myip.opendns.com", "+short").Output()
-	log.DebugLog(log.DebugLevelMexos, "dig to resolver1.opendns.com called", "out", string(out), "err", err)
+	resp, err := SendHTTPReq("GET", "http://ifconfig.me")
 	if err != nil {
 		return "", err
 	}
-	return strings.TrimSpace(string(out)), err
+	defer resp.Body.Close()
+	ip, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(ip)), err
 }

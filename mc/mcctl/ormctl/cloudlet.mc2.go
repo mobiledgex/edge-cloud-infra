b815ba5b153ccb363a7e6a0a6435b64b9ce778db
Lev Shvarts <lev@mobiledgex.com>
2019-08-01 13:58:11 +0800

    EDGECLOUD-1048 Automatically deploy shepherd services on a cloudlet creation (#268)
    * Implement changes required to do an auto-deployment of shepherd service on a cloudlet similar to what is done with CRM.
    * Add shepherd service module for handling shepherd process actions
    * Add testMode to mexos package for handling local tests with MexDind platform

--- mc/mcctl/ormctl/cloudlet.mc2.go
+++ mc/mcctl/ormctl/cloudlet.mc2.go
@@ -235,6 +235,7 @@ var PlatformConfigOptionalArgs = []string{
 	"crmroleid",
 	"crmsecretid",
 	"platformtag",
+	"influxaddr",
 }
 var PlatformConfigAliasArgs = []string{
 	"registrypath=platformconfig.registrypath",
@@ -245,6 +246,7 @@ var PlatformConfigAliasArgs = []string{
 	"crmroleid=platformconfig.crmroleid",
 	"crmsecretid=platformconfig.crmsecretid",
 	"platformtag=platformconfig.platformtag",
+	"influxaddr=platformconfig.influxaddr",
 }
 var CloudletRequiredArgs = []string{
 	"operator",

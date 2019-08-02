17cd16b96c2bd489bcd216174d3440c371adb23f
Venky Tumkur <venky.tumkur@mobiledgex.com>
2019-08-02 21:57:22 +0530

    Add KR region controller in the main mexdemo setup
    Also removed the (non-Ansible) main mexdemo cluster from terraform.
    Importing its state into terraform and trying to keep it in sync turned
    out to be more trouble that it was worth.
    
    The mexdemo inventory now manages only the EU & KR controllers in
    our main setup.

--- terraform/mexplat/mexdemo/output.tf
+++ terraform/mexplat/mexdemo/output.tf
@@ -1,17 +1,30 @@
-output "kube_config" {
-  value = "${module.k8s.kube_config}"
+output "eu_kube_config" {
+  value = "${module.k8s_eu.kube_config}"
 }
 
-output "k8s_cluster_name" {
-  value = "${var.cluster_name}"
+output "eu_k8s_cluster_name" {
+  value = "${var.eu_cluster_name}"
+}
+
+output "kr_kube_config" {
+  value = "${module.k8s_kr.kube_config}"
+}
+
+output "kr_k8s_cluster_name" {
+  value = "${var.kr_cluster_name}"
 }
 
 output "k8s_clusters" {
   value = [
     {
-      "name" = "${var.cluster_name}"
-      "kube_config" = "${module.k8s.kube_config}"
-      "region" = "US"
+      "name" = "${var.kr_cluster_name}"
+      "kube_config" = "${module.k8s_kr.kube_config}"
+      "region" = "KR"
+    },
+    {
+      "name" = "${var.eu_cluster_name}"
+      "kube_config" = "${module.k8s_eu.kube_config}"
+      "region" = "EU"
     },
   ]
 }

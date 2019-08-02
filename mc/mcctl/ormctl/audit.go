1d02f5035c3e92ab761a9468d9ce88711bca0ced
Jon <38672162+gainsley@users.noreply.github.com>
2019-08-02 08:48:24 -0700

    EDGECLOUD-590 mc audit api (#277)

---
+++ mc/mcctl/ormctl/audit.go
@@ -0,0 +1,24 @@
+package ormctl
+
+import (
+	"github.com/mobiledgex/edge-cloud-infra/mc/ormapi"
+	"github.com/spf13/cobra"
+)
+
+func GetAuditCommand() *cobra.Command {
+	cmds := []*Command{&Command{
+		Use:          "showself",
+		OptionalArgs: "limit",
+		ReqData:      &ormapi.AuditQuery{},
+		ReplyData:    &[]ormapi.AuditResponse{},
+		Path:         "/auth/audit/showself",
+	}, &Command{
+		Use:          "showorg",
+		RequiredArgs: "org",
+		OptionalArgs: "limit",
+		ReqData:      &ormapi.AuditQuery{},
+		ReplyData:    &[]ormapi.AuditResponse{},
+		Path:         "/auth/audit/showorg",
+	}}
+	return genGroup("audit", "show audit logs", cmds)
+}

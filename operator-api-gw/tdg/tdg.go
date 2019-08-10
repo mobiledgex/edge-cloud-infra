71a3e86a8657ce073fa5f7b0f2bdfbc46e321dea
Jim <jim.morris@mobiledgex.com>
2019-08-10 13:00:45 -0500

    decouple TDG implementation of location apis by moving all of it to infra.

--- operator-api-gw/tdg/tdg.go
+++ operator-api-gw/tdg/tdg.go
@@ -3,11 +3,12 @@ package tdg
 import (
 	"fmt"
 
-	tdgclient "github.com/mobiledgex/edge-cloud-infra/operator-api-gw/tdg/tdg-qos/qosclient"
-	dmecommon "github.com/mobiledgex/edge-cloud/d-match-engine/dme-common"
+	locclient "github.com/mobiledgex/edge-cloud-infra/operator-api-gw/tdg/tdg-loc/locclient"
+	qosclient "github.com/mobiledgex/edge-cloud-infra/operator-api-gw/tdg/tdg-qos/qosclient"
 	dme "github.com/mobiledgex/edge-cloud/d-match-engine/dme-proto"
 	operator "github.com/mobiledgex/edge-cloud/d-match-engine/operator"
-	simulatedqos "github.com/mobiledgex/edge-cloud/d-match-engine/operator/simulated-qos"
+	simulatedloc "github.com/mobiledgex/edge-cloud/d-match-engine/operator/defaultoperator/simulated-location"
+	simulatedqos "github.com/mobiledgex/edge-cloud/d-match-engine/operator/defaultoperator/simulated-qos"
 	"github.com/mobiledgex/edge-cloud/log"
 )
 
@@ -30,7 +31,7 @@ func (o *OperatorApiGw) Init(operatorName string, servers *operator.OperatorApiG
 	o.Servers = servers
 
 	if o.Servers.QosPosUrl != "" {
-		err := tdgclient.GetQosCertsFromVault(o.Servers.VaultAddr)
+		err := qosclient.GetQosCertsFromVault(o.Servers.VaultAddr)
 		if err != nil {
 			return err
 		}
@@ -38,8 +39,37 @@ func (o *OperatorApiGw) Init(operatorName string, servers *operator.OperatorApiG
 	return nil
 }
 
-func (*OperatorApiGw) VerifyLocation(mreq *dme.VerifyLocationRequest, mreply *dme.VerifyLocationReply, ckey *dmecommon.CookieKey) (*dme.VerifyLocationReply, error) {
-	return nil, fmt.Errorf("Verify Location not yet implemented")
+func (o *OperatorApiGw) VerifyLocation(mreq *dme.VerifyLocationRequest, mreply *dme.VerifyLocationReply) error {
+
+	log.DebugLog(log.DebugLevelDmereq, "TDG VerifyLocation", "request", mreq)
+
+	if o.Servers.LocVerUrl == "" {
+		// because this is so often used for demos, it is better to fail in a clear way
+		// than to give a defaultoperator/fake result
+		return fmt.Errorf("DME has no location verification server")
+	}
+	if o.Servers.TokSrvUrl == "" {
+		return fmt.Errorf("DME has no token server")
+	}
+	if mreq.VerifyLocToken == "" {
+		return fmt.Errorf("no VerifyLocToken in request")
+	}
+	// this is checked in the DME, but since we dereference it we will check it in this code as well
+	if mreq.GpsLocation == nil {
+		return fmt.Errorf("no GpsLocation in request")
+	}
+
+	result := locclient.CallTDGLocationVerifyAPI(o.Servers.LocVerUrl, mreq.GpsLocation.Latitude, mreq.GpsLocation.Longitude, mreq.VerifyLocToken, o.Servers.TokSrvUrl)
+	mreply.GpsLocationStatus = result.MatchEngineLocStatus
+	mreply.GpsLocationAccuracyKm = result.DistanceRange
+	log.DebugLog(log.DebugLevelDmereq, "TDG VerifyLocation result", "mreply", mreply)
+	return nil
+}
+
+func (o *OperatorApiGw) GetLocation(mreq *dme.GetLocationRequest, mreply *dme.GetLocationReply) error {
+	log.DebugLog(log.DebugLevelDmereq, "TDG GetLocation", "request", mreq)
+	// We have no real implementation of this
+	return simulatedloc.GetSimulatedClientLoc(mreq, mreply)
 }
 
 func (o *OperatorApiGw) GetQOSPositionKPI(mreq *dme.QosPositionKpiRequest, getQosSvr dme.MatchEngineApi_GetQosPositionKpiServer) error {
@@ -49,5 +79,5 @@ func (o *OperatorApiGw) GetQOSPositionKPI(mreq *dme.QosPositionKpiRequest, getQo
 		log.DebugLog(log.DebugLevelDmereq, "No QosPosUrl, getting simulated results")
 		return simulatedqos.GetSimulatedQOSPositionKPI(mreq, getQosSvr)
 	}
-	return tdgclient.GetQOSPositionKPIFromApiGW(o.Servers.QosPosUrl, mreq, getQosSvr)
+	return qosclient.GetQOSPositionKPIFromApiGW(o.Servers.QosPosUrl, mreq, getQosSvr)
 }

0ee6673f097cee7f9ff5615d11309b84f523171d
Venky <venky.tumkur@mobiledgex.com>
2019-08-22 09:35:40 +0530

    EDGECLOUD-1158 TICK stack for infrastructure monitoring (#307)

---
+++ vault/ansible-internal-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# exit immediately on failure
+set -e
+
+# Set up profiles for ansible access
+
+# You may need to set the following env vars before running:
+# VAULT_ADDR=http://127.0.0.1:8200
+# VAULT_TOKEN=<my auth token>
+
+echo "Setting up Vault for ansible internal environment"
+
+cat > /tmp/ansible-pol.hcl <<EOF
+path "auth/approle/login" {
+  capabilities = [ "create", "read" ]
+}
+
+path "secret/data/ansible/internal/*" {
+  capabilities = [ "read" ]
+}
+EOF
+vault policy write internal.ansible /tmp/ansible-pol.hcl
+rm /tmp/ansible-pol.hcl
+
+vault write auth/approle/role/internal.ansible policies="internal.ansible" token_ttl=120

6d51761f1acc1eedd956e9187a49dda297b1896b
Matthew Chu <matthew.chu@mobiledgex.com>
2019-08-26 13:33:26 -0700

    nginx stats working, need to fit into the refactor

--- shepherd/nginx-stats.go
+++ shepherd/nginx-stats.go
@@ -96,11 +96,10 @@ func NginxScraper() {
 
 				metrics, err := QueryNginx(v)
 				if err != nil {
-					log.DebugLog(log.DebugLevelMetrics, "Error retrieving nginx metrics", "clusterInst", v.Cluster, "error", err.Error())
+					log.DebugLog(log.DebugLevelMetrics, "Error retrieving nginx metrics", "appinst", v.App, "error", err.Error())
 				} else {
 					//send to crm->controller->influx
 					influxData := newNginxMetric(v, metrics)
-					fmt.Printf("ASDF sending %+v\n", influxData)
 					MetricSender.Update(ctx, influxData)
 				}
 				span.Finish()
@@ -112,28 +111,23 @@ func NginxScraper() {
 
 func QueryNginx(scrapePoint NginxScrapePoint) (*NginxMetrics, error) {
 	//build the query
-	request := fmt.Sprintf("docker exec -it %s curl http://127.0.0.1:8080/nginx_metrics", scrapePoint.Container)
+	request := fmt.Sprintf("docker exec %s curl http://127.0.0.1:8080/nginx_metrics", scrapePoint.Container)
 	resp, err := scrapePoint.Client.Output(request)
-	if err != nil {
-		errstr := fmt.Sprintf("Failed to run <%s>", request)
-		log.DebugLog(log.DebugLevelMetrics, errstr, "err", err.Error())
-		return nil, err
-	}
 	//if this is the first time, or the container got restarted, install curl
 	if strings.Contains(resp, "executable file not found") {
-		log.DebugLog(log.DebugLevelMexos, "Installing curl onto docker container for metrics collection")
-		installer := fmt.Sprintf("docker exec -it %s apt-get update; docker exec -it %s apt-get --assume-yes install curl", scrapePoint.Container, scrapePoint.Container)
+		log.DebugLog(log.DebugLevelMexos, "Installing curl onto docker container "+scrapePoint.Container+" for metrics collection")
+		installer := fmt.Sprintf("docker exec %s apt-get update; docker exec %s apt-get --assume-yes install curl", scrapePoint.Container, scrapePoint.Container)
 		resp, err = scrapePoint.Client.Output(installer)
 		if err != nil {
 			return nil, fmt.Errorf("can't install curl on nginx container %s, %s, %v", *name, resp, err)
 		}
 		//now retry curling
 		resp, err = scrapePoint.Client.Output(request)
-		if err != nil {
-			errstr := fmt.Sprintf("Failed to run <%s>", request)
-			log.DebugLog(log.DebugLevelMetrics, errstr, "err", err.Error())
-			return nil, err
-		}
+	}
+	if err != nil {
+		errstr := fmt.Sprintf("Failed to run <%s>", request)
+		log.DebugLog(log.DebugLevelMetrics, errstr, "err", err.Error())
+		return nil, err
 	}
 	metrics := &NginxMetrics{}
 	err = parseNginxResp(resp, metrics)
@@ -144,40 +138,51 @@ func QueryNginx(scrapePoint NginxScrapePoint) (*NginxMetrics, error) {
 }
 
 func parseNginxResp(resp string, metrics *NginxMetrics) error {
-	trimmedResp := OutputTrim(resp)
-	lines := strings.Split(trimmedResp, "\n")
+	//sometimes the response lines get cycled around, so break it up based on the start of the actual content
+	trimmedResp := strings.Split(resp, "Active connections:")
+	if len(trimmedResp) < 2 {
+		return fmt.Errorf("Unexpected output format")
+	}
+	lines := strings.Split(trimmedResp[1], "\n")
 
 	var err error
 	//fix this hardcoding later
-	if len(lines) != 4 {
+	if len(lines) < 5 {
 		return fmt.Errorf("Unexpected output format")
 	}
 
 	//first line has active connections
 	stats := strings.Split(lines[0], " ")
-	if metrics.ActiveConn, err = strconv.ParseUint(stats[2], 10, 64); err != nil {
+	if metrics.ActiveConn, err = strconv.ParseUint(stats[1], 10, 64); err != nil {
+		fmt.Printf("1: %s\n", stats[2])
 		return err
 	}
 	//third line for accepts, handled, and requests
 	stats = strings.Split(lines[2], " ")
 	if metrics.Accepts, err = strconv.ParseUint(stats[1], 10, 64); err != nil {
+		fmt.Printf("2: '%s'\n", stats[1])
 		return err
 	}
 	if metrics.HandledConn, err = strconv.ParseUint(stats[2], 10, 64); err != nil {
+		fmt.Printf("3: '%s'\n", stats[2])
 		return err
 	}
 	if metrics.Requests, err = strconv.ParseUint(stats[3], 10, 64); err != nil {
+		fmt.Printf("4: '%s'\n", stats[3])
 		return err
 	}
 	//last line has reading, writing, waiting
 	stats = strings.Split(lines[3], " ")
 	if metrics.Reading, err = strconv.ParseUint(stats[1], 10, 64); err != nil {
+		fmt.Printf("5: '%s'\n", stats[1])
 		return err
 	}
 	if metrics.Writing, err = strconv.ParseUint(stats[3], 10, 64); err != nil {
+		fmt.Printf("6: '%s'\n", stats[3])
 		return err
 	}
 	if metrics.Waiting, err = strconv.ParseUint(stats[5], 10, 64); err != nil {
+		fmt.Printf("7: '%s'\n", stats[5])
 		return err
 	}
 	metrics.Ts, _ = types.TimestampProto(time.Now())

9f221aacefb6b1f30215db706c9127c871ce44bb
Lev Shvarts <lev@mobiledgex.com>
2019-09-04 16:37:14 -0700

    Address code review comments - unit test data refactor

--- shepherd/docker-stats_test.go
+++ shepherd/docker-stats_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"testing"
 	"time"
 
@@ -11,6 +12,34 @@ import (
 	"github.com/mobiledgex/edge-cloud/edgeproto"
 	"github.com/stretchr/testify/assert"
 )
+
+// Sample output of formatted 'docker stats' cmd
+var testDockerResult = `{
+	"App": "DockerApp1",
+	"memory": {
+	  "raw": "11.11MiB / 1.111GiB",
+	  "percent": "1.11%"
+	},
+	"cpu": "1.11%",
+	"io": {
+	  "network": "111B / 1KB",
+	  "block": "1B / 1B"
+	}
+  }`
+
+// Example output of resource-tracker
+var testDockerClusterData = shepherd_common.ClusterMetrics{
+	Cpu:        10.10101010,
+	Mem:        11.111111,
+	Disk:       12.12121212,
+	NetSent:    1313131313,
+	NetRecv:    1414141414,
+	TcpConns:   1515,
+	TcpRetrans: 16,
+	UdpSent:    1717,
+	UdpRecv:    1818,
+	UdpRecvErr: 19,
+}
 
 func TestDockerStats(t *testing.T) {
 
@@ -45,7 +74,14 @@ func TestDockerStats(t *testing.T) {
 		Deployment: cloudcommon.AppDeploymentTypeDocker,
 	}
 
-	testPromStats, err := NewClusterWorker("", time.Second*1, nil, &testClusterInst, &shepherd_unittest.Platform{})
+	buf, err := json.Marshal(testDockerClusterData)
+	assert.Nil(t, err, "marshal cluster metrics")
+	platform := shepherd_unittest.Platform{
+		DockerAppMetrics:     testDockerResult,
+		DockerClusterMetrics: string(buf),
+	}
+
+	testPromStats, err := NewClusterWorker("", time.Second*1, nil, &testClusterInst, &platform)
 	assert.Nil(t, err, "Get a patform client for unit test cloudlet")
 	clusterMetrics := testPromStats.clusterStat.GetClusterStats()
 	appsMetrics := testPromStats.clusterStat.GetAppStats()
@@ -65,10 +101,15 @@ func TestDockerStats(t *testing.T) {
 	}
 
 	// Check ClusterStats
-	assert.Equal(t, float64(10.10101010), clusterMetrics.Cpu)
+	assert.Equal(t, testDockerClusterData.Cpu, clusterMetrics.Cpu)
 	assert.NotNil(t, clusterMetrics.CpuTS, "CPU timestamp for cluster")
-	assert.Equal(t, float64(11.111111), clusterMetrics.Mem)
-	assert.Equal(t, float64(12.12121212), clusterMetrics.Disk)
-	assert.Equal(t, uint64(1313131313), clusterMetrics.NetSent)
-	assert.Equal(t, uint64(1414141414), clusterMetrics.NetRecv)
+	assert.Equal(t, testDockerClusterData.Mem, clusterMetrics.Mem)
+	assert.Equal(t, testDockerClusterData.Disk, clusterMetrics.Disk)
+	assert.Equal(t, testDockerClusterData.NetSent, clusterMetrics.NetSent)
+	assert.Equal(t, testDockerClusterData.NetRecv, clusterMetrics.NetRecv)
+	assert.Equal(t, testDockerClusterData.TcpConns, clusterMetrics.TcpConns)
+	assert.Equal(t, testDockerClusterData.TcpRetrans, clusterMetrics.TcpRetrans)
+	assert.Equal(t, testDockerClusterData.UdpSent, clusterMetrics.UdpSent)
+	assert.Equal(t, testDockerClusterData.UdpRecv, clusterMetrics.UdpRecv)
+	assert.Equal(t, testDockerClusterData.UdpRecvErr, clusterMetrics.UdpRecvErr)
 }
--- shepherd/shepherd_platform/shepherd_unittest/shepherd_unittest.go
+++ shepherd/shepherd_platform/shepherd_unittest/shepherd_unittest.go
@@ -1,11 +1,20 @@
 package shepherd_unittest
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"strings"
+
 	"github.com/mobiledgex/edge-cloud/cloud-resource-manager/platform/pc"
 	"github.com/mobiledgex/edge-cloud/edgeproto"
 )
 
 type Platform struct {
+	// Contains the response string for a given type of a request
+	DockerAppMetrics     string
+	DockerClusterMetrics string
+	// TODO - add Prometheus/nginx strings here EDGECLOUD-1252
 }
 
 func (s *Platform) GetType() string {
@@ -21,19 +30,39 @@ func (s *Platform) GetClusterIP(clusterInst *edgeproto.ClusterInst) (string, err
 }
 
 func (s *Platform) GetPlatformClient(clusterInst *edgeproto.ClusterInst) (pc.PlatformClient, error) {
-	return &UTClient{}, nil
+	return &UTClient{pf: s}, nil
 }
 
 // UTClient hijacks a set of commands and returns predetermined output
 // For all other commands it just calls pc.LocalClient equivalents
 type UTClient struct {
 	pc.LocalClient
+	pf *Platform
 }
 
 func (s *UTClient) Output(command string) (string, error) {
-	out, err := GetUTData(command)
+	out, err := s.getUTData(command)
 	if err != nil {
 		return s.Output(command)
 	}
 	return out, nil
+}
+
+func (s *UTClient) getUTData(command string) (string, error) {
+	str := ""
+	// docker stats unit test
+	if strings.Contains(command, "docker stats ") {
+		// take the json with line breaks and compact it, as that's what the command expects
+		str = s.pf.DockerAppMetrics
+	} else if strings.Contains(command, "resource-tracker") {
+		str = s.pf.DockerClusterMetrics
+	}
+	if str != "" {
+		buf := new(bytes.Buffer)
+		if err := json.Compact(buf, []byte(str)); err != nil {
+			return "", err
+		}
+		return buf.String(), nil
+	}
+	return "", fmt.Errorf("No UT Data found")
 }

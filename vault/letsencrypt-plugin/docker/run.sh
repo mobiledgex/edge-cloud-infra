4f77a673d4d15fff4ab5c830e8f0a6f058e04786
Venky <venky.tumkur@mobiledgex.com>
2019-09-09 10:45:21 +0530

    Support for geotargeted DME domain certs (#352)

--- vault/letsencrypt-plugin/docker/run.sh
+++ vault/letsencrypt-plugin/docker/run.sh
@@ -4,9 +4,17 @@ if [ -z "$CF_USER" -o -z "$CF_APIKEY" ]; then
 	echo "ERROR: Cloudflare credentials not provided" >&2
 	exit 2
 fi
+if [ -z "$NS1_APIKEY" ]; then
+	echo "ERROR: NS1 api key not provided" >&2
+	exit 2
+fi
 cat >/etc/cloudflare.ini <<EOT
 dns_cloudflare_email = $CF_USER
 dns_cloudflare_api_key = $CF_APIKEY
 EOT
 chmod 400 /etc/cloudflare.ini
+cat >/etc/ns1.ini <<EOT
+dns_nsone_api_key = $NS1_APIKEY
+EOT
+chmod 400 /etc/ns1.ini
 exec /usr/bin/supervisord -c /etc/supervisord.conf
--- vault/letsencrypt-plugin/letsencrypt/path_info.go
+++ vault/letsencrypt-plugin/letsencrypt/path_info.go
@@ -3,8 +3,8 @@ package letsencrypt
 import (
 	"context"
 
-	"github.com/hashicorp/vault/logical"
-	"github.com/hashicorp/vault/logical/framework"
+	"github.com/hashicorp/vault/sdk/logical"
+	"github.com/hashicorp/vault/sdk/framework"
 )
 
 func (b *backend) pathInfo(_ context.Context, req *logical.Request, _ *framework.FieldData) (*logical.Response, error) {

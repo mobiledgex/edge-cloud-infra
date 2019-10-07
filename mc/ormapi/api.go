74de26ae8486829fc02efdfd055a766a5663fca1
mchu1195 <matthew.chu@mobiledgex.com>
2019-10-07 14:48:48 -0700

    fixed deleting of last manager (#395)
    * fixed deleting of last manager

--- mc/ormapi/api.go
+++ mc/ormapi/api.go
@@ -25,13 +25,12 @@ type User struct {
 }
 
 type Organization struct {
-	Name          string `gorm:"primary_key;type:citext"`
-	Type          string `gorm:"not null"`
-	Address       string
-	Phone         string
-	AdminUsername string    `gorm:"type:text;type:citext REFERENCES users(name)"`
-	CreatedAt     time.Time `json:",omitempty"`
-	UpdatedAt     time.Time `json:",omitempty"`
+	Name      string `gorm:"primary_key;type:citext"`
+	Type      string `gorm:"not null"`
+	Address   string
+	Phone     string
+	CreatedAt time.Time `json:",omitempty"`
+	UpdatedAt time.Time `json:",omitempty"`
 }
 
 type Controller struct {

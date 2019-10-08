bf812a7cc0f5629a303fa4c03c96bb627a8699de
Jon <38672162+gainsley@users.noreply.github.com>
2019-10-08 09:19:56 -0700

    EDGECLOUD-1110 prevent orgs from using other org images (#387)

---
+++ mc/orm/authz_app_test.go
@@ -0,0 +1,86 @@
+package orm
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/mobiledgex/edge-cloud-infra/mc/ormapi"
+	"github.com/mobiledgex/edge-cloud-infra/mc/ormclient"
+	"github.com/mobiledgex/edge-cloud/edgeproto"
+	"github.com/stretchr/testify/require"
+)
+
+func testImagePaths(t *testing.T, ctx context.Context, mcClient *ormclient.Client, uri, tokenAd string) {
+	org1 := ormapi.Organization{
+		Type:    "developer",
+		Name:    "org1",
+		Address: "org1",
+		Phone:   "123-123-1234",
+	}
+	status, err := mcClient.CreateOrg(uri, tokenAd, &org1)
+	require.Nil(t, err)
+	require.Equal(t, http.StatusOK, status)
+	org2 := org1
+	org2.Name = "org2"
+	status, err = mcClient.CreateOrg(uri, tokenAd, &org2)
+	require.Nil(t, err)
+	require.Equal(t, http.StatusOK, status)
+	org3 := org1
+	org3.Name = "org3"
+	org3.PublicImages = true
+	status, err = mcClient.CreateOrg(uri, tokenAd, &org3)
+	require.Nil(t, err)
+	require.Equal(t, http.StatusOK, status)
+
+	// non-mobiledgex paths always succeed
+	testImagePath(t, ctx, "org1", "http://foobar.com/blah/blah", true)
+	testImagePath(t, ctx, "org1", "http://foobar.com/artifactory/repo-blah/blah", true)
+	// mobiledgex paths that should succeed - gitlab
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/org1/app", true)
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/org1/app:1.0", true)
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/org1/app:latest", true)
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/org1/extra/app:latest", true)
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/org1/", true)
+	// mobiledgex paths that should succeed - artifactory
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/artifactory/repo-org1/cirros-0.4.0-arm-disk.img#md5:7e9cfcb763e83573a4b9d9315f56cc5f", true)
+	// public orgs should succeed
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/org3/someapp", true)
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/artifactory/repo-org3/someapp", true)
+
+	// should fail
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/org2/app", false)
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/org2/app:1.0", false)
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/org2/app:latest", false)
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net", false)
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/", false)
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/artifactory/repo-org2/cirros-0.4.0-arm-disk.img#md5:7e9cfcb763e83573a4b9d9315f56cc5f", false)
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/artifactory/org1/cirros-0.4.0-arm-disk.img#md5:7e9cfcb763e83573a4b9d9315f56cc5f", false)
+	// missing orgs should fail
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/org4/someapp", false)
+	testImagePath(t, ctx, "org1", "http://foobar.mobiledgex.net/artifactory/repo-org4/someapp", false)
+
+	status, err = mcClient.DeleteOrg(uri, tokenAd, &org1)
+	require.Nil(t, err)
+	require.Equal(t, http.StatusOK, status)
+	status, err = mcClient.DeleteOrg(uri, tokenAd, &org2)
+	require.Nil(t, err)
+	require.Equal(t, http.StatusOK, status)
+	status, err = mcClient.DeleteOrg(uri, tokenAd, &org3)
+	require.Nil(t, err)
+	require.Equal(t, http.StatusOK, status)
+}
+
+func testImagePath(t *testing.T, ctx context.Context, org, imagepath string, ok bool) {
+	app := edgeproto.App{}
+	app.Key.DeveloperKey.Name = org
+	app.ImagePath = imagepath
+	err := checkImagePath(ctx, &app)
+	if ok {
+		require.Nil(t, err)
+	} else {
+		require.NotNil(t, err)
+		fmt.Printf("%v\n", err)
+	}
+}

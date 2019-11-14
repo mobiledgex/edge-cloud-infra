9ad513ebb59ac96f028240fcc33abea0132bfc64
Venky <venky.tumkur@mobiledgex.com>
2019-11-14 10:52:45 +0530

    Baseimage rework (#441)
    * Base image cleanup
    
    Introduced a top-level mobiledgex package which bundles the installed
    scripts and also captures all package dependencies and locks them down
    to explicit versions.  Also, created packages for docker-compose,
    crictl, and helm which are not distrubuted as deb packages.
    
    * Upgrade Ubuntu QCOW2 image to latest build ("20191024")

--- openstack-tenant/packer/setup.sh
+++ openstack-tenant/packer/setup.sh
@@ -6,7 +6,6 @@ LOGFILE="${LOGDIR}/creation_log.txt"
 DEFAULT_INTERFACE=ens3
 ARTIFACTORY_BASEURL='https://artifactory.mobiledgex.net'
 DEFAULT_ROOT_PASS=sandhill
-DEFAULT_HELM_VERSION=v2.15.1
 MEX_RELEASE=/etc/mex-release
 
 TMPLOG="/var/tmp/creation_log.txt"
@@ -27,7 +26,6 @@ trap 'archive_log' EXIT
 # Defaults for environment variables
 : ${TAG:=master}
 : ${ROOT_PASS:=$DEFAULT_ROOT_PASS}
-: ${HELM_VERSION:=$DEFAULT_HELM_VERSION}
 if [[ -z "$INTERFACE" ]]; then
 	INTERFACE=$( ls -d /sys/class/net/*/device 2>/dev/null \
 			| head -n 1 \
@@ -60,7 +58,7 @@ download_artifactory_file() {
 	local src="$1"
 	local dst="$2"
 	local mode="$3"
-	local arturl="${ARTIFACTORY_BASEURL}/artifactory/baseimage-build/$TAG/$src"
+	local arturl="${ARTIFACTORY_BASEURL}/artifactory/baseimage-build/${ARTIFACTORY_ARTIFACTS_TAG}/${src}"
 	log "Downloading $arturl"
 	sudo curl -s -u "$ARTIFACTORY_CREDS" -o "$dst" "$arturl"
 	sudo test -f "$dst" || die "Failed to download file: $arturl"
@@ -76,51 +74,11 @@ log_file_contents /etc/hosts
 echo "nameserver 1.1.1.1" | sudo tee -a /etc/resolv.conf >/dev/null
 log_file_contents /etc/resolv.conf
 
-log "Setting up $MEX_RELEASE"
-sudo tee "$MEX_RELEASE" <<EOT
-MEX_BUILD="$MEX_BUILD $( TZ=UTC date +'%Y/%m/%d %H:%M %Z' )"
-MEX_BUILD_TAG=$TAG
-MEX_BUILD_FLAVOR=$FLAVOR
-MEX_BUILD_SRC_IMG=$SRC_IMG
-MEX_BUILD_SRC_IMG_CHECKSUM=$SRC_IMG_CHECKSUM
-EOT
-
-log "Installing extra packages"
-sudo add-apt-repository universe
-sudo apt-get update
-# avoid interactive for iptables-persistent
-sudo apt-get install -y debconf-utils
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-sudo apt-get install -y \
-	ipvsadm=1:1.28-3 \
-	jq=1.5+dfsg-1ubuntu0.1 \
-        cloud-init \
-        iptables-persistent 
-
-[[ $? -ne 0 ]] && die "Failed to install extra packages"
-
-log "dhclient $INTERFACE"
-sudo dhclient "$INTERFACE"
-ip addr
-ip route
-
-log "Downloading files from artifactory for tag $TAG"
-download_artifactory_file holepunch /etc/mobiledgex/holepunch a+rx
-download_artifactory_file holepunch.json /etc/mobiledgex/holepunch.json a+r
-download_artifactory_file mobiledgex-init.sh /usr/local/bin/mobiledgex-init.sh a+rx
-download_artifactory_file mobiledgex.service /etc/systemd/system/mobiledgex.service a+r
-download_artifactory_file docker-compose /usr/local/bin/docker-compose a+rx
-download_artifactory_file helm-${HELM_VERSION}.tar.gz /tmp/helm.tar.gz a+r
-
+log "Downloading files from artifactory for tag $ARTIFACTORY_ARTIFACTS_TAG"
 download_artifactory_file ssh.config /root/.ssh/config 600
 download_artifactory_file keys/id_rsa_mex /etc/mobiledgex/id_rsa_mex 600
 download_artifactory_file keys/id_rsa_mex.pub /tmp/id_rsa_mex.pub
 download_artifactory_file keys/id_rsa_mobiledgex.pub /tmp/id_rsa_mobiledgex.pub
-
-download_artifactory_file install-k8s-base.sh /etc/mobiledgex/install-k8s-base.sh a+rx
-download_artifactory_file install-k8s-master.sh /etc/mobiledgex/install-k8s-master.sh a+rx
-download_artifactory_file install-k8s-node.sh /etc/mobiledgex/install-k8s-node.sh a+rx
 
 log "Setting up SSH"
 sudo cp /etc/mobiledgex/id_rsa_mex /root/id_rsa_mex
@@ -131,21 +89,54 @@ sudo chmod 700 /root/.ssh
 sudo chmod 600 /root/.ssh/authorized_keys
 sudo rm -f /root/.ssh/known_hosts
 
+log "Setting up $MEX_RELEASE"
+sudo tee "$MEX_RELEASE" <<EOT
+MEX_BUILD="$MEX_BUILD $( TZ=UTC date +'%Y/%m/%d %H:%M %Z' )"
+MEX_BUILD_TAG=$TAG
+MEX_BUILD_FLAVOR=$FLAVOR
+MEX_BUILD_SRC_IMG=$SRC_IMG
+MEX_BUILD_SRC_IMG_CHECKSUM=$SRC_IMG_CHECKSUM
+EOT
+
+log "Setting up APT sources"
+sudo rm -rf /etc/apt/sources.list.d
+sudo tee /etc/apt/sources.list <<EOT
+deb https://${APT_USER}:${APT_PASS}@artifactory.mobiledgex.net/artifactory/packages stratus main
+deb https://${APT_USER}:${APT_PASS}@artifactory.mobiledgex.net/artifactory/docker xenial stable
+deb https://${APT_USER}:${APT_PASS}@artifactory.mobiledgex.net/artifactory/ubuntu xenial main restricted universe multiverse
+deb https://${APT_USER}:${APT_PASS}@artifactory.mobiledgex.net/artifactory/ubuntu xenial-updates main restricted universe multiverse
+deb https://${APT_USER}:${APT_PASS}@artifactory.mobiledgex.net/artifactory/ubuntu-security xenial-security main restricted universe multiverse
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOT
+
+log "Set up the APT keys"
+curl -s https://${APT_USER}:${APT_PASS}@artifactory.mobiledgex.net/artifactory/api/gpg/key/public | sudo apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get update
+
+log "Install mobiledgex ${TAG#v}"
+# avoid interactive for iptables-persistent
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+sudo apt-get install -y mobiledgex=${TAG#v}
+[[ $? -ne 0 ]] && die "Failed to install extra packages"
+
+log "dhclient $INTERFACE"
+sudo dhclient "$INTERFACE"
+ip addr
+ip route
+
 log "Enabling the mobiledgex service"
 sudo systemctl enable mobiledgex
 
 log "Setting the root password"
 echo "root:$ROOT_PASS" | sudo chpasswd
 
-log "Installing k8s base"
-sudo env ARTIFACTORY_CREDS="$ARTIFACTORY_CREDS" TAG="$TAG" sh -x /etc/mobiledgex/install-k8s-base.sh 
-sudo chmod a+rw /var/run/docker/sock
-sudo groupadd docker
+log "System setup"
+sudo swapoff -a
+sudo sed -i "s/cgroup-driver=systemd/cgroup-driver=cgroupfs/g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+sudo kubeadm config images pull
 sudo usermod -aG docker root
-
-log "Installing helm $HELM_VERSION"
-tar xf /tmp/helm.tar.gz linux-amd64/helm
-sudo mv linux-amd64/helm /usr/local/bin/helm
-sudo chmod a+rx /usr/local/bin/helm
 
 echo "[$(date)] Done setup.sh ($( pwd ))"

9ebf44679a4d46c636c6ab62e90ce2ef34865ffd
Jon <38672162+gainsley@users.noreply.github.com>
2019-11-27 10:05:37 -0800

    EDGECLOUD-1610 invalid image path error, vault handling tweaks (#499)

--- mc/mcctl/ormctl/flavor.mc2.go
+++ mc/mcctl/ormctl/flavor.mc2.go
@@ -155,14 +155,3 @@ var FlavorComments = map[string]string{
 var FlavorSpecialArgs = map[string]string{
 	"optresmap": "StringToString",
 }
-var OptResMapEntryRequiredArgs = []string{}
-var OptResMapEntryOptionalArgs = []string{
-	"key",
-	"value",
-}
-var OptResMapEntryAliasArgs = []string{
-	"key=optresmapentry.key",
-	"value=optresmapentry.value",
-}
-var OptResMapEntryComments = map[string]string{}
-var OptResMapEntrySpecialArgs = map[string]string{}

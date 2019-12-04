45c5258ef3f29c9ca77aa5a0ddb9d31eeb9ad9b1
Venky <venky.tumkur@mobiledgex.com>
2019-12-04 16:03:32 +0530

    EDGECLOUD-1081 HA for Vault (#501)

---
+++ terraform/modules/fw_vault_gcp/main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_firewall" fw {
+  name          = "${var.firewall_name}"
+  network       = "${var.firewall_network}"
+
+  allow {
+    protocol    = "tcp"
+    ports       = [ "8200" ]
+  }
+
+  target_tags   = [ "${var.target_tag}" ]
+  source_ranges = [
+    "130.211.0.0/22",
+    "35.191.0.0/16"
+  ]
+}

43f76924d618ef712619c6e98df612720870e267
Jon <38672162+gainsley@users.noreply.github.com>
2020-01-22 09:55:58 -0800

    EDGECLOUD-1756 bad org in ImagePath better error message (#583)

--- mc/rbac/enforcer.go
+++ mc/rbac/enforcer.go
@@ -32,20 +32,24 @@ func (e *Enforcer) Init(ctx context.Context) error {
 	return e.adapter.Init(ctx)
 }
 
-func (e *Enforcer) Enforce(ctx context.Context, sub, org, obj, act string) (bool, error) {
+// Enforce checks that the action is allowed. The first boolean return
+// value indicates if the action is allowed or not, the second indicates
+// if it was allowed because the user is an admin (and thus the existence
+// of the org was not verified).
+func (e *Enforcer) Enforce(ctx context.Context, sub, org, obj, act string) (bool, bool, error) {
 	authz, err := e.adapter.GetAuthorized(ctx, obj, act)
 	if err != nil {
-		return false, err
+		return false, false, err
 	}
 
 	subj := GetCasbinGroup(org, sub)
 	_, found := authz[subj]
 	if found {
-		return true, nil
+		return true, false, nil
 	}
 	// may be admin so no org appended
 	_, found = authz[sub]
-	return found, nil
+	return found, true, nil
 }
 
 func (e *Enforcer) LogEnforce(on bool) {

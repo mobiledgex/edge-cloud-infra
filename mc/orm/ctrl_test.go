eea5f72f015ea9882f4f2e5e32bbf6137600b201
Jon <38672162+gainsley@users.noreply.github.com>
2020-01-22 09:55:58 -0800

    EDGECLOUD-1756 bad org in ImagePath better error message (#583)

--- mc/orm/ctrl_test.go
+++ mc/orm/ctrl_test.go
@@ -217,6 +217,20 @@ func TestController(t *testing.T) {
 	goodPermTestCloudletPool(t, mcClient, uri, tokenAd, ctrl.Region, org2, dcnt)
 	goodPermTestCloudletPoolMember(t, mcClient, uri, tokenAd, ctrl.Region, org1, dcnt)
 	goodPermTestCloudletPoolMember(t, mcClient, uri, tokenAd, ctrl.Region, org2, dcnt)
+	// make sure admin cannot create App against non-existent org,
+	// because enforcement check for admins doesn't look at org names in rbac.
+	badApp := &edgeproto.App{}
+	badApp.Key.DeveloperKey.Name = "nonexistent"
+	_, status, err = ormtestutil.TestCreateApp(mcClient, uri, token, ctrl.Region, badApp)
+	require.NotNil(t, err)
+	require.Equal(t, http.StatusForbidden, status)
+
+	// bug 1756 - better error message for nonexisting org in image path
+	badApp.ImagePath = "docker-qa.mobiledgex.net/nonexistent/images/server_ping_threaded:5.0"
+	_, status, err = ormtestutil.TestCreateApp(mcClient, uri, token, ctrl.Region, badApp)
+	require.NotNil(t, err)
+	require.Equal(t, http.StatusBadRequest, status)
+	require.Contains(t, err.Error(), "Organization nonexistent from ImagePath not found")
 
 	// flavors, clusterflavors are special - can be seen by all
 	goodPermTestShowFlavor(t, mcClient, uri, tokenDev, ctrl.Region, "", dcnt)

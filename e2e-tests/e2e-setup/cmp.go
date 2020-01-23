959b32808aee6da51cebeddf76db1a1e2a1131ec
Matthew Chu <matthew.chu@mobiledgex.com>
2020-01-22 17:14:00 -0800

    e2e-tests for showcloudletinfo

--- e2e-tests/e2e-setup/cmp.go
+++ e2e-tests/e2e-setup/cmp.go
@@ -111,6 +111,32 @@ func CompareYamlFiles(firstYamlFile string, secondYamlFile string, fileType stri
 		err1 = util.ReadYamlFile(firstYamlFile, &a1)
 		err2 = util.ReadYamlFile(secondYamlFile, &a2)
 
+		// remove cloudletinfos that are offline
+		for i, region := range a1.RegionData {
+			onlineCloudlets := make([]edgeproto.CloudletInfo, 0)
+			for _, cloudletinfo := range region.AppData.CloudletInfos {
+				if cloudletinfo.State != edgeproto.CloudletState_CLOUDLET_STATE_OFFLINE {
+					onlineCloudlets = append(onlineCloudlets, cloudletinfo)
+				}
+			}
+			a1.RegionData[i].AppData.CloudletInfos = onlineCloudlets
+			log.Printf("a1 online cloudlets: %+v\n", onlineCloudlets)
+			log.Printf("region version: %+v\n\n\n\n", region.AppData.CloudletInfos)
+		}
+		log.Printf("a1: %+v\n\n\n\n", a1.RegionData)
+		// remove cloudletinfos that are offline
+		for i, region := range a2.RegionData {
+			onlineCloudlets := make([]edgeproto.CloudletInfo, 0)
+			for _, cloudletinfo := range region.AppData.CloudletInfos {
+				if cloudletinfo.State != edgeproto.CloudletState_CLOUDLET_STATE_OFFLINE {
+					onlineCloudlets = append(onlineCloudlets, cloudletinfo)
+				}
+			}
+			a2.RegionData[i].AppData.CloudletInfos = onlineCloudlets
+			log.Printf("a2 online cloudlets: %+v\n", onlineCloudlets)
+		}
+		log.Printf("a2: %+v\n", a2.RegionData)
+
 		copts = []cmp.Option{
 			cmpopts.IgnoreTypes(time.Time{}, dmeproto.Timestamp{}),
 			IgnoreAdminRole,

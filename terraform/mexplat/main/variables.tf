e469722fa7825c8891291b29d4a98ad4116f116d
Venky <venky.tumkur@mobiledgex.com>
2020-02-08 16:03:45 +0530

    Alternate main console (#620)

--- terraform/mexplat/main/variables.tf
+++ terraform/mexplat/main/variables.tf
@@ -86,6 +86,15 @@ variable "mc_vm_domain_name" {
   default     = "mc.mobiledgex.net"
 }
 
+variable "console_instance_name" {
+  default     = "console-main"
+}
+
+variable "console_domain_name" {
+  description = "Console domain name"
+  type        = "string"
+}
+
 variable "ssh_public_key_file" {
   description = "SSH public key file for the ansible account"
   type        = "string"

78b80c7f9ce7575265f2e87546569134ea2eb244
Lev Shvarts <lev.shvarts@gmail.com>
2020-02-26 16:35:00 -0800

    EDGECLOUD-1859 AppInst Healthcheck not working for correctly for HealthCheckFailRootlbOffline (#648)
    * Changed health-check related PlatformClient calls from Output() to OutputWithTimeout() to prevent it from being possibly blocked.
       * added OutputWithTimeout() to shepherd unit-test platform client
    * Refactored Envoy health check to use the same same container name as proxy shepherd worker
       * it was using the wrong container name.

--- shepherd/shepherd_platform/shepherd_unittest/shepherd_unittest.go
+++ shepherd/shepherd_platform/shepherd_unittest/shepherd_unittest.go
@@ -81,6 +81,14 @@ func (s *UTClient) Output(command string) (string, error) {
 	return out, nil
 }
 
+func (s *UTClient) OutputWithTimeout(command string, timeout time.Duration) (string, error) {
+	out, err := s.getUTData(command)
+	if err != nil {
+		return s.LocalClient.OutputWithTimeout(command, timeout)
+	}
+	return out, nil
+}
+
 func (s *UTClient) getUTData(command string) (string, error) {
 	// docker stats unit test
 	if strings.Contains(command, "docker stats ") {
@@ -92,6 +100,13 @@ func (s *UTClient) getUTData(command string) (string, error) {
 	// nginx-stats and envoy-stats unit test
 	// "docker exec containername curl http://url"
 	if strings.Contains(command, "docker exec") && strings.Contains(command, "curl") {
+		split := strings.SplitN(command, " ", 4)
+		if len(split) == 4 {
+			return s.LocalClient.Output(split[3])
+		}
+	}
+	// "docker exec containername echo text"
+	if strings.Contains(command, "docker exec") && strings.Contains(command, "echo") {
 		split := strings.SplitN(command, " ", 4)
 		if len(split) == 4 {
 			return s.LocalClient.Output(split[3])

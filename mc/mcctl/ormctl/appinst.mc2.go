77f9595bb5982d28ccec9115dd3307fbd78248fd
mwilliams-mex <52012938+mwilliams-mex@users.noreply.github.com>
2020-03-09 14:15:26 -0700

    EDGECLOUD-2101: Reject createAppInst with flavor not supported on clo… (#674)
    * EDGECLOUD-2101: Reject createAppInst with flavor not supported on cloudlet
    
    * EGECLOUD-2101:appinst.proto nodeflavor to vmFlavor

--- mc/mcctl/ormctl/appinst.mc2.go
+++ mc/mcctl/ormctl/appinst.mc2.go
@@ -134,6 +134,9 @@ var CreateAppInstOptionalArgs = []string{
 	"sharedvolumesize",
 	"healthcheck",
 	"privacypolicy",
+	"externalvolumesize",
+	"availabilityzone",
+	"vmflavor",
 }
 var DeleteAppInstRequiredArgs = []string{
 	"developer",
@@ -183,6 +186,9 @@ var DeleteAppInstOptionalArgs = []string{
 	"sharedvolumesize",
 	"healthcheck",
 	"privacypolicy",
+	"externalvolumesize",
+	"availabilityzone",
+	"vmflavor",
 }
 var RefreshAppInstRequiredArgs = []string{
 	"developer",
@@ -200,6 +206,9 @@ var RefreshAppInstOptionalArgs = []string{
 	"sharedvolumesize",
 	"healthcheck",
 	"privacypolicy",
+	"externalvolumesize",
+	"availabilityzone",
+	"vmflavor",
 }
 var UpdateAppInstRequiredArgs = []string{
 	"developer",
@@ -218,6 +227,9 @@ var UpdateAppInstOptionalArgs = []string{
 	"healthcheck",
 	"privacypolicy",
 	"powerstate",
+	"externalvolumesize",
+	"availabilityzone",
+	"vmflavor",
 }
 var AppInstKeyRequiredArgs = []string{}
 var AppInstKeyOptionalArgs = []string{
@@ -270,6 +282,9 @@ var AppInstOptionalArgs = []string{
 	"healthcheck",
 	"privacypolicy",
 	"powerstate",
+	"externalvolumesize",
+	"availabilityzone",
+	"vmflavor",
 }
 var AppInstAliasArgs = []string{
 	"developer=appinst.key.appkey.developerkey.name",
@@ -318,6 +333,9 @@ var AppInstAliasArgs = []string{
 	"healthcheck=appinst.healthcheck",
 	"privacypolicy=appinst.privacypolicy",
 	"powerstate=appinst.powerstate",
+	"externalvolumesize=appinst.externalvolumesize",
+	"availabilityzone=appinst.availabilityzone",
+	"vmflavor=appinst.vmflavor",
 }
 var AppInstComments = map[string]string{
 	"developer":                      "Organization or Company Name that a Developer is part of",
@@ -358,6 +376,9 @@ var AppInstComments = map[string]string{
 	"healthcheck":                    "Health Check status, one of HealthCheckUnknown, HealthCheckFailRootlbOffline, HealthCheckFailServerFail, HealthCheckOk",
 	"privacypolicy":                  "Optional privacy policy name",
 	"powerstate":                     "Power State of the AppInst, one of PowerOn, PowerOff, Reboot",
+	"externalvolumesize":             "Size of external volume to be attached to nodes.  This is for the root partition",
+	"availabilityzone":               "Optional Availability Zone if any",
+	"vmflavor":                       "OS node flavor to use",
 }
 var AppInstSpecialArgs = map[string]string{
 	"errors":                   "StringArray",

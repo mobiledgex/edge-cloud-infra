80ce23960c5872bec25ac61daa410cc4cff73628
Venky <venky.tumkur@mobiledgex.com>
2020-03-19 23:07:49 +0530

    Generate API docs for Akraino (#699)

---
+++ ansible/files/apidocs/akraino-postprocess.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import argparse
+import json
+import re
+import sys
+
+def remove_mobiledgex_references(sw):
+    for item in list(sw):
+        if isinstance(sw[item], dict):
+            remove_mobiledgex_references(sw[item])
+        elif item == "x-go-package":
+            del sw[item]
+        elif item == "description":
+            sw[item] = re.sub(r'MobiledgeX\s*', '', sw[item]).capitalize()
+
+def fix_introduction(sw, description=None):
+    try:
+        sw['info']['title'] = re.sub(r'MobiledgeX\s*', '', sw['info']['title'])
+        if not description:
+            del sw['info']['description']
+        else:
+            sw['info']['description'] = description
+    except Exception:
+        pass
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--description", "-d", help="Replacement description text")
+    args = parser.parse_args()
+
+    sw = json.load(sys.stdin)
+
+    remove_mobiledgex_references(sw)
+    fix_introduction(sw, description=args.description)
+
+    print(json.dumps(sw, indent=2))
+
+if __name__ == "__main__":
+    main()

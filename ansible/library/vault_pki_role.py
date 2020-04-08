762bb1bc42718d287f819e9b4ac5a78d53e35005
Venky <venky.tumkur@mobiledgex.com>
2020-04-08 11:32:15 +0530

    Deployment support for the new vault PKI infrastructure (#723)
    Primary changes:
    * ansible/library/vault_pki.py: Ansible module to set up a PKI mount in vault
    * ansible/library/vault_pki_role.py: Ansible module to set up a vault PKI cert issuing role
    * ansible/vault-setup.yml: Set up the root CA, the three intermediate CAs, and the global PKI role
    * ansible/roles/vault/tasks/intermediate-ca.yml: Role to set up the intermediate CAs
    * ansible/roles/vault/tasks/pki-region-roles.yml: Set up the region PKI roles
    
    The rest of the changes are to set up the new vault policies/approles to control access to the PKI endpoints, and changes in playbooks to add region and vault parameters to services which did not use them before.

---
+++ ansible/library/vault_pki_role.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+from ansible.module_utils.basic import *
+from ansible.module_utils.vault import vault_request
+
+PARAM_DEFAULTS = {
+    "max_lease_ttl": 259200, # 72h
+}
+
+vault_addr = None
+
+def vault_pki_role_present(vault, data, check_mode=False):
+    has_changed = False
+    actions = []
+
+    path = data.pop("path")
+    rolename = data.pop("rolename")
+    role_params = {
+        "allow_localhost": True,
+        "allowed_domains": data.pop("allowed_domains"),
+        "allow_subdomains": True,
+        "allowed_uri_sans": data.pop("allowed_uri_sans")
+    }
+    try:
+        role = vault("{0}/roles/{1}".format(path, rolename))["data"]
+    except Exception:
+        role = {}
+
+    for param in role_params:
+        curr_param = role.get(param)
+        if isinstance(curr_param, list) and len(curr_param) > 0:
+            curr_param = curr_param[0]
+        if curr_param != role_params[param]:
+            # Create or update role
+            vault("{0}/roles/{1}".format(path, rolename), method="POST",
+                  json=role_params, success_code=204)
+            has_changed = True
+            actions.append("Created/updated role")
+            break
+
+    return (has_changed, {"actions": actions})
+
+def vault_pki_role_absent(vault, data, check_mode=False):
+    has_changed = False
+    actions = []
+
+    path = data.pop("path")
+    rolename = data.pop("rolename")
+
+    try:
+        role = vault("{0}/roles/{1}".format(path, rolename))
+        has_changed = True
+    except Exception:
+        pass
+
+    if has_changed:
+        vault("{0}/roles/{1}".format(path, rolename), method="DELETE",
+              success_code=204)
+        actions.append("Deleted role")
+
+    return (has_changed, {"actions": actions})
+
+def main():
+    global vault_addr
+
+    fields = {
+        "path": {"required": True, "type": "str"},
+        "rolename": {"required": True, "type": "str"},
+        "allowed_domains": {"required": True, "type": "str"},
+        "allowed_uri_sans": {"required": True, "type": "str"},
+        "vault_addr": {"required": True, "type": "str"},
+        "vault_token": {"required": True, "type": "str"},
+        "state": {
+            "default": "present",
+            "choices": ["present", "absent"],
+            "type": "str",
+        },
+    }
+    choice_map = {
+        "present": vault_pki_role_present,
+        "absent": vault_pki_role_absent,
+    }
+
+    module = AnsibleModule(argument_spec=fields, supports_check_mode=True)
+    vault_addr = module.params.pop("vault_addr")
+    vault = vault_request(vault_addr, module.params.pop("vault_token"))
+    has_changed, result = choice_map.get(module.params.pop("state"))(
+        vault, data=module.params, check_mode=module.check_mode)
+    module.exit_json(changed=has_changed, meta=result)
+
+if __name__ == '__main__':
+    main()

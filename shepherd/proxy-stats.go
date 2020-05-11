b45e8d6560f667c0263d7990ea5e2ef922ad4863
Matthew Chu <matthew.chu@mobiledgex.com>
2020-05-11 15:29:31 -0700

    added verision to shepherd proxyMap

--- shepherd/proxy-stats.go
+++ shepherd/proxy-stats.go
@@ -60,7 +60,7 @@ func StartProxyScraper() {
 }
 
 func getProxyKey(appInstKey *edgeproto.AppInstKey) string {
-	return appInstKey.AppKey.Name + "-" + appInstKey.ClusterInstKey.ClusterKey.Name + "-" + appInstKey.AppKey.Organization
+	return appInstKey.AppKey.Name + "-" + appInstKey.ClusterInstKey.ClusterKey.Name + "-" + appInstKey.AppKey.Organization + "-" + appInstKey.AppKey.Version
 }
 
 // Figure out envoy proxy container name

71a373e5f5804de65ba250377c331decd5e91a70
Matthew Chu <matthew.chu@mobiledgex.com>
2020-05-11 16:40:57 -0700

    pr comments

--- shepherd/proxy-stats.go
+++ shepherd/proxy-stats.go
@@ -60,7 +60,7 @@ func StartProxyScraper() {
 }
 
 func getProxyKey(appInstKey *edgeproto.AppInstKey) string {
-	return appInstKey.AppKey.Name + "-" + appInstKey.ClusterInstKey.ClusterKey.Name + "-" + appInstKey.AppKey.Organization + "-" + appInstKey.AppKey.Version
+	return appInstKey.GetKeyString()
 }
 
 // Figure out envoy proxy container name

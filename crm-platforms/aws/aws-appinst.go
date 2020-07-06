3b95e299cbbfea0698821aff928c158efd95ac25
anand12gupta <anand.pb.gupta@gmail.com>
2020-07-06 14:20:21 -0500

    Edgecloud 2705 (#872)
    * initial aws plugin code
    
    * aws plugin compile problem fixes.
    
    * Create/Delete EKS Cluster
    
    * Changes needed to support app-inst creation
    
    * vault changes
    
    * vault setup
    
    * vault access
    
    * access to chef policy in vault
    
    * Add aws read policy
    
    * fix aws secret path
    
    * eksctl should pick number of worker nodes and node flavor from clusterinst object
    
    * Add clienttype to GetClusterPlatformClient
    
    * handle managed cluster not allowed with 0 numnodes. Also code review comments.
    
    * consolidate two createclusterinst debug log messages into one
    
    * code review comments

--- crm-platforms/aws/aws-appinst.go
+++ crm-platforms/aws/aws-appinst.go
@@ -1,4 +1,4 @@
-package azure
+package aws
 
 import (
 	"context"
@@ -9,17 +9,18 @@ import (
 	"github.com/mobiledgex/edge-cloud/cloud-resource-manager/k8smgmt"
 	"github.com/mobiledgex/edge-cloud/cloudcommon"
 	"github.com/mobiledgex/edge-cloud/edgeproto"
+	"github.com/mobiledgex/edge-cloud/log"
+
 	v1 "k8s.io/api/core/v1"
 )
 
-func (a *AzurePlatform) CreateAppInst(ctx context.Context, clusterInst *edgeproto.ClusterInst, app *edgeproto.App, appInst *edgeproto.AppInst, flavor *edgeproto.Flavor, privacyPolicy *edgeproto.PrivacyPolicy, updateCallback edgeproto.CacheUpdateCallback) error {
-	updateCallback(edgeproto.UpdateTask, "Creating AppInst")
-
+func (a *AWSPlatform) CreateAppInst(ctx context.Context, clusterInst *edgeproto.ClusterInst, app *edgeproto.App, appInst *edgeproto.AppInst, flavor *edgeproto.Flavor, privacyPolicy *edgeproto.PrivacyPolicy, updateCallback edgeproto.CacheUpdateCallback) error {
 	var err error
 	// regenerate kconf if missing because CRM in container was restarted
-	if err = a.SetupKconf(ctx, clusterInst); err != nil {
+	if err = SetupKconf(ctx, clusterInst); err != nil {
 		return fmt.Errorf("can't set up kconf, %s", err.Error())
 	}
+
 	client, err := a.GetClusterPlatformClient(ctx, clusterInst, cloudcommon.ClientTypeRootLB)
 	if err != nil {
 		return err
@@ -29,8 +30,6 @@ func (a *AzurePlatform) CreateAppInst(ctx context.Context, clusterInst *edgeprot
 	if err != nil {
 		return err
 	}
-	updateCallback(edgeproto.UpdateTask, "Creating Registry Secret")
-
 	for _, imagePath := range names.ImagePaths {
 		err = infracommon.CreateDockerRegistrySecret(ctx, client, clusterInst, imagePath, a.commonPf.VaultConfig, names)
 		if err != nil {
@@ -42,8 +41,6 @@ func (a *AzurePlatform) CreateAppInst(ctx context.Context, clusterInst *edgeprot
 	case cloudcommon.DeploymentTypeKubernetes:
 		err = k8smgmt.CreateAppInst(ctx, a.commonPf.VaultConfig, client, names, app, appInst)
 		if err == nil {
-			updateCallback(edgeproto.UpdateTask, "Waiting for AppInst to Start")
-
 			err = k8smgmt.WaitForAppInst(ctx, client, names, app, k8smgmt.WaitRunning)
 		}
 	default:
@@ -52,32 +49,30 @@ func (a *AzurePlatform) CreateAppInst(ctx context.Context, clusterInst *edgeprot
 	if err != nil {
 		return err
 	}
-	updateCallback(edgeproto.UpdateTask, "Waiting for Load Balancer External IP")
 
 	// set up dns
 	getDnsAction := func(svc v1.Service) (*infracommon.DnsSvcAction, error) {
 		action := infracommon.DnsSvcAction{}
-		externalIP,_,err := infracommon.GetSvcExternalIpOrHost(ctx, client, names, svc.ObjectMeta.Name)
+		_, dnsName, err := infracommon.GetSvcExternalIpOrHost(ctx, client, names, svc.ObjectMeta.Name)
 		if err != nil {
 			return nil, err
 		}
-		action.ExternalIP = externalIP
-		// no patching needed since Azure already does it.
+
+		action.Hostname = dnsName
+		// no patching needed since AWS already does it.
 		// Should only add DNS for external ports
 		action.AddDNS = !app.InternalPorts
 		return &action, nil
 	}
-	err = a.commonPf.CreateAppDNSAndPatchKubeSvc(ctx, client, names, infracommon.NoDnsOverride, getDnsAction)
-	if err != nil {
-		return nil
+	if err = a.commonPf.CreateAppDNSAndPatchKubeSvc(ctx, client, names, infracommon.NoDnsOverride, getDnsAction); err != nil {
+		return err
 	}
 	return nil
 }
 
-func (a *AzurePlatform) DeleteAppInst(ctx context.Context, clusterInst *edgeproto.ClusterInst, app *edgeproto.App, appInst *edgeproto.AppInst) error {
-	var err error
+func (a *AWSPlatform) DeleteAppInst(ctx context.Context, clusterInst *edgeproto.ClusterInst, app *edgeproto.App, appInst *edgeproto.AppInst) error {
 	// regenerate kconf if missing because CRM in container was restarted
-	if err = a.SetupKconf(ctx, clusterInst); err != nil {
+	if err := SetupKconf(ctx, clusterInst); err != nil {
 		return fmt.Errorf("can't set up kconf, %s", err.Error())
 	}
 	client, err := a.GetClusterPlatformClient(ctx, clusterInst, cloudcommon.ClientTypeRootLB)
@@ -106,19 +101,17 @@ func (a *AzurePlatform) DeleteAppInst(ctx context.Context, clusterInst *edgeprot
 	return a.commonPf.DeleteAppDNS(ctx, client, names, infracommon.NoDnsOverride)
 }
 
-func (a *AzurePlatform) SetupKconf(ctx context.Context, clusterInst *edgeproto.ClusterInst) error {
-	targetFile := infracommon.GetLocalKconfName(clusterInst)
+func SetupKconf(ctx context.Context, clusterInst *edgeproto.ClusterInst) error {
+	targetFile := k8smgmt.GetKconfName(clusterInst)
+	log.SpanLog(ctx, log.DebugLevelInfra, "SetupKconf", "targetFile", targetFile)
+
 	if _, err := os.Stat(targetFile); err == nil {
 		// already exists
 		return nil
 	}
-	if err := a.AzureLogin(ctx); err != nil {
-		return err
-	}
-	clusterName := AzureSanitize(clusterInst.Key.ClusterKey.Name)
-	rg := GetResourceGroupForCluster(clusterInst)
-	if err := GetAKSCredentials(rg, clusterName); err != nil {
-		return fmt.Errorf("unable to get AKS credentials %v", err)
+	clusterName := clusterInst.Key.ClusterKey.Name
+	if err := GetEKSCredentials(clusterName); err != nil {
+		return fmt.Errorf("unable to get EKS credentials %v", err)
 	}
 	src := infracommon.DefaultKubeconfig()
 	if err := infracommon.CopyFile(src, targetFile); err != nil {
@@ -127,25 +120,8 @@ func (a *AzurePlatform) SetupKconf(ctx context.Context, clusterInst *edgeproto.C
 	return nil
 }
 
-func (s *AzurePlatform) GetAppInstRuntime(ctx context.Context, clusterInst *edgeproto.ClusterInst, app *edgeproto.App, appInst *edgeproto.AppInst) (*edgeproto.AppInstRuntime, error) {
-	// regenerate kconf if missing because CRM in container was restarted
-	if err := s.SetupKconf(ctx, clusterInst); err != nil {
-		return nil, fmt.Errorf("can't set up kconf, %s", err.Error())
-	}
-	client, err := s.GetClusterPlatformClient(ctx, clusterInst, cloudcommon.ClientTypeRootLB)
-	if err != nil {
-		return nil, err
-	}
-
-	names, err := k8smgmt.GetKubeNames(clusterInst, app, appInst)
-	if err != nil {
-		return nil, err
-	}
-	return k8smgmt.GetAppInstRuntime(ctx, client, names, app, appInst)
-}
-
-func (a *AzurePlatform) UpdateAppInst(ctx context.Context, clusterInst *edgeproto.ClusterInst, app *edgeproto.App, appInst *edgeproto.AppInst, updateCallback edgeproto.CacheUpdateCallback) error {
-	updateCallback(edgeproto.UpdateTask, "Updating Azure AppInst")
+func (a *AWSPlatform) UpdateAppInst(ctx context.Context, clusterInst *edgeproto.ClusterInst, app *edgeproto.App, appInst *edgeproto.AppInst, updateCallback edgeproto.CacheUpdateCallback) error {
+	updateCallback(edgeproto.UpdateTask, "Updating AWS AppInst")
 	names, err := k8smgmt.GetKubeNames(clusterInst, app, appInst)
 	if err != nil {
 		return err
@@ -163,14 +139,31 @@ func (a *AzurePlatform) UpdateAppInst(ctx context.Context, clusterInst *edgeprot
 	return err
 }
 
-func (a *AzurePlatform) GetContainerCommand(ctx context.Context, clusterInst *edgeproto.ClusterInst, app *edgeproto.App, appInst *edgeproto.AppInst, req *edgeproto.ExecRequest) (string, error) {
+func (a *AWSPlatform) GetAppInstRuntime(ctx context.Context, clusterInst *edgeproto.ClusterInst, app *edgeproto.App, appInst *edgeproto.AppInst) (*edgeproto.AppInstRuntime, error) {
+	// regenerate kconf if missing because CRM in container was restarted
+	if err := SetupKconf(ctx, clusterInst); err != nil {
+		return nil, fmt.Errorf("can't set up kconf, %s", err.Error())
+	}
+	client, err := a.GetClusterPlatformClient(ctx, clusterInst, cloudcommon.ClientTypeRootLB)
+	if err != nil {
+		return nil, err
+	}
+
+	names, err := k8smgmt.GetKubeNames(clusterInst, app, appInst)
+	if err != nil {
+		return nil, err
+	}
+	return k8smgmt.GetAppInstRuntime(ctx, client, names, app, appInst)
+}
+
+func (a *AWSPlatform) GetContainerCommand(ctx context.Context, clusterInst *edgeproto.ClusterInst, app *edgeproto.App, appInst *edgeproto.AppInst, req *edgeproto.ExecRequest) (string, error) {
 	return k8smgmt.GetContainerCommand(ctx, clusterInst, app, appInst, req)
 }
 
-func (a *AzurePlatform) GetConsoleUrl(ctx context.Context, app *edgeproto.App) (string, error) {
+func (a *AWSPlatform) GetConsoleUrl(ctx context.Context, app *edgeproto.App) (string, error) {
 	return "", fmt.Errorf("Unsupported command for platform")
 }
 
-func (a *AzurePlatform) SetPowerState(ctx context.Context, app *edgeproto.App, appInst *edgeproto.AppInst, updateCallback edgeproto.CacheUpdateCallback) error {
+func (a *AWSPlatform) SetPowerState(ctx context.Context, app *edgeproto.App, appInst *edgeproto.AppInst, updateCallback edgeproto.CacheUpdateCallback) error {
 	return fmt.Errorf("Unsupported command for platform")
 }

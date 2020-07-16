e0be936f1c82dc643dd992ac17ef65547ed642d2
Ashish Jain <ashish.jain@mobiledgex.com>
2020-07-16 19:51:00 +0530

    autogen'd files

--- mc/mcctl/ormctl/cloudlet.mc2.go
+++ mc/mcctl/ormctl/cloudlet.mc2.go
@@ -218,7 +218,6 @@ var UpdateCloudletOptionalArgs = []string{
 	"crmoverride",
 	"accessvars",
 	"maintenancestate",
-	"overridepolicycontainerversion",
 }
 
 var ShowCloudletInfoCmd = &cli.Command{

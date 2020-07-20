9011757e4bef133588953047aafaf9924fc7de74
Matthew Chu <matthew.chu@mobiledgex.com>
2020-07-20 15:40:33 -0700

    add support for new collectioninterval

--- e2e-tests/int-process/process_local.go
+++ e2e-tests/int-process/process_local.go
@@ -66,6 +66,10 @@ func (p *MC) StartLocal(logfile string, opts ...process.StartOp) error {
 		args = append(args, "--billingPath")
 		args = append(args, p.BillingPath)
 	}
+	if p.TestCollectionInterval != "" {
+		args = append(args, "--testCollectionInterval")
+		args = append(args, p.TestCollectionInterval)
+	}
 	if p.UseVaultCerts {
 		args = append(args, "--useVaultCerts")
 	}

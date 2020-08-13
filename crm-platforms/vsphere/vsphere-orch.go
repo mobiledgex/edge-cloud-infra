0e0e6b9a77b3f4c65a3fd40639233792a91afdd8
Jim <jim.morris@mobiledgex.com>
2020-08-13 08:59:31 -0500

    fix case of multi datastores per vdc (#947)

--- crm-platforms/vsphere/vsphere-orch.go
+++ crm-platforms/vsphere/vsphere-orch.go
@@ -366,6 +366,7 @@ func (v *VSpherePlatform) CreateVM(ctx context.Context, vm *vmlayer.VMOrchestrat
 	log.SpanLog(ctx, log.DebugLevelInfra, "CreateVM", "vmName", vm.Name, "poolName", poolName)
 
 	dcName := v.GetDatacenterName(ctx)
+	dsName := v.GetDataStore()
 	computeCluster := v.GetComputeCluster()
 	pathPrefix := fmt.Sprintf("/%s/host/%s/Resources/", dcName, computeCluster)
 	poolPath := pathPrefix + poolName
@@ -398,6 +399,7 @@ func (v *VSpherePlatform) CreateVM(ctx context.Context, vm *vmlayer.VMOrchestrat
 		}
 		out, err := v.TimedGovcCommand(ctx, "govc", "vm.clone",
 			"-dc", dcName,
+			"-ds", dsName,
 			"-on=false",
 			"-vm", vm.ImageName,
 			"-pool", poolPath,

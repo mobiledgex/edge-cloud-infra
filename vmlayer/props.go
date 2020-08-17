5b3a0f8e676c0a74f9e1f32749bc4c988c5e33be
Venky <venky.tumkur@mobiledgex.com>
2020-08-17 11:43:35 +0530

    18.04 baseimage (#961)
    * Changes to support the 18.04 baseimage
    
    * Updated APT repos to bionic
    
    * Switch to resolved, replace netplan with ifupdown
    
    * CIS-CAT hardening
    
    * More CIS-CAT fixes
    
    * More CIS-CAT fixes
    
    * More hardening
    
    * More hardening
    
    * TOTP for root console logins
    
    * Persist iptables changes
    
    * Vault setup for the TOTP engine
    
    * Fix vault roles, add NTP to base image
    
    * Don't need NTP, systemd-timesyncd is sufficient
    
    * Add nvidia-440
    
    Minor changes to better support 18.04 apt changes
    
    * Remove unnecessary packages

--- vmlayer/props.go
+++ vmlayer/props.go
@@ -28,7 +28,7 @@ type VMProperties struct {
 var ImageFormatQcow2 = "qcow2"
 var ImageFormatVmdk = "vmdk"
 
-var MEXInfraVersion = "3.1.6"
+var MEXInfraVersion = "4.0.0"
 var ImageNamePrefix = "mobiledgex-v"
 var DefaultOSImageName = ImageNamePrefix + MEXInfraVersion
 

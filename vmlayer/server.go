47a7c833a73fc5a195d4d967ea5514fe736d32a6
Jim <jim.morris@mobiledgex.com>
2020-08-21 11:09:38 -0500

    fix vsphere work with new 18.04 baseimage (#970)
    * make vsphere work in 18.04
    
    * update default infra version
    
    * fix router case to add route to internal net
    
    * address comments
    
    * move netplan stuff to a separate function
    
    * after testing, change order of portname, ifname

--- vmlayer/server.go
+++ vmlayer/server.go
@@ -81,8 +81,12 @@ func GetIPFromServerDetails(ctx context.Context, networkName string, portName st
 	return nil, fmt.Errorf("unable to find IP for server: %s on network: %s port: %s", sd.Name, networkName, portName)
 }
 
-func GetCloudletNetworkIfaceFile() string {
-	return "/etc/network/interfaces.d/50-cloud-init.cfg"
+func GetCloudletNetworkIfaceFile(netplanEnabled bool) string {
+	if netplanEnabled {
+		return "/etc/netplan/50-cloud-init.yaml"
+	} else {
+		return "/etc/network/interfaces.d/50-cloud-init.cfg"
+	}
 }
 
 func (v *VMPlatform) GetConsoleUrl(ctx context.Context, app *edgeproto.App) (string, error) {

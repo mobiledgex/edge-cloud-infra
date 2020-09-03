2d8d7eb96a34695703f03c291195cb667f709522
Venky <venky.tumkur@mobiledgex.com>
2020-09-03 15:49:00 +0530

    Alertmanager terraform manifests (#996)
    Also:
    - Fix a typo in the MC command line argument
    - Enable alertmanager in QA and main
    - Sync actual VM state with terraform
    - Unrelated terraform cleanup

--- terraform/mexplat/main/output.tf
+++
@@ -1,9 +0,0 @@
-output "registry_replicas" {
-  value = [
-    {
-      "location"  = "${module.docker_replica_west_eu.zone}"
-      "hostname"  = "${module.docker_replica_west_eu_dns.hostname}"
-      "ip"        = "${module.docker_replica_west_eu.external_ip}"
-    }
-  ]
-}

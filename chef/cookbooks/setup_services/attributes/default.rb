565002fb609a9186ef34166e652e9b9892d167cc
Ashish Jain <ashish.jain@mobiledgex.com>
2020-10-09 20:39:05 +0530

    move comment

--- chef/cookbooks/setup_services/attributes/default.rb
+++ chef/cookbooks/setup_services/attributes/default.rb
@@ -2,8 +2,8 @@
 services = ['crmserver', 'shepherd']
 services.each { |service|
   deployTag = node.normal[service][:args][:deploymentTag]
-  Chef::Log.info("Setting ES_SERVER_URLS env var for #{service} belonging to #{deployTag} setup...")
   unless node.normal[service][:env].any? { |s| s.include?('ES_SERVER_URLS') }
+    Chef::Log.info("Setting ES_SERVER_URLS env var for #{service} belonging to #{deployTag} setup...")
     if deployTag == "main"
       node.normal[service]['env'].append("ES_SERVER_URLS=https://events.es.mobiledgex.net/")
     else

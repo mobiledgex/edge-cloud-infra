22de8c3ddd5db4154ac0e46c5ade6a21d2c03358
Ashish Jain <ashish.jain@mobiledgex.com>
2020-10-19 23:41:57 +0530

    New policyFile for TELUS based nodes (#1096)
    * New policyFile for TELUS env + run_cmd cookbook to execute command on platform node
    
    * update readme
    
    * update verions + lock files

--- chef/policyfiles/docker_crm.rb
+++ chef/policyfiles/docker_crm.rb
@@ -7,7 +7,7 @@ name 'docker_crm'
 default_source :chef_server, "https://chef.mobiledgex.net/organizations/mobiledgex"
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'recipe[runstatus_handler]', 'recipe[setup_infra]', 'recipe[preflight_crm_checks]', 'recipe[setup_services::docker]', 'recipe[run_diagnostics]'
+run_list 'recipe[runstatus_handler]', 'recipe[setup_infra]', 'recipe[preflight_crm_checks]', 'recipe[run_diagnostics]', 'recipe[setup_services::docker]', 'recipe[run_cmd]'
 
 # Specify a custom source for a single cookbook:
 cookbook 'runstatus_handler', '= 1.0.0'
@@ -16,6 +16,7 @@ cookbook 'preflight_crm_checks', '= 1.0.0'
 cookbook 'setup_services', '= 1.0.14'
 cookbook 'docker', '= 6.0.3'
 cookbook 'run_diagnostics', '= 1.0.0'
+cookbook 'run_cmd', '= 1.0.0'
 
 # Set edgeCloudVersion (i.e. edge-cloud docker base image version) for all the cloudlets
 override['main']['edgeCloudVersion'] = '2020-09-23-5'

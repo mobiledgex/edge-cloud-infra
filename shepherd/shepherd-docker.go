a6a284ba2a9ca6818fe26dda47620212a13b8f49
Lev Shvarts <lev@mobiledgex.com>
2020-10-27 12:44:36 -0700

    Collect docker cluster metrics from docker node instead of LB node

--- shepherd/shepherd-docker.go
+++ shepherd/shepherd-docker.go
@@ -389,7 +389,7 @@ func (c *DockerClusterStats) collectDockerAppMetrics(ctx context.Context, p *Doc
 
 func collectDockerClusterMetrics(ctx context.Context, p *DockerClusterStats) error {
 	// VM stats from Openstack might be a better idea going forward, but for now use a simple script to scrape the metrics on the RootLB
-	resp, err := p.client.Output(shepherd_common.ResTrackerCmd)
+	resp, err := p.clusterClient.Output(shepherd_common.ResTrackerCmd)
 	if err != nil {
 		log.SpanLog(ctx, log.DebugLevelMetrics, "Failed to run", "cmd", shepherd_common.ResTrackerCmd, "err", err.Error())
 		return err

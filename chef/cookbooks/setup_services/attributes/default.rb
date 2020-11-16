c21a537b8c46cb893963a54efe3effae5c7412a3
Ashish Jain <ashish.jain@mobiledgex.com>
2020-11-16 13:01:30 +0530

    EDGECLOUD-3959 CRM upgrade via chef fails due to missing accessApiAddr attribute (#1160)

--- chef/cookbooks/setup_services/attributes/default.rb
+++ chef/cookbooks/setup_services/attributes/default.rb
@@ -1,6 +1,12 @@
-# Set ES_SERVER_URLS environment variable
+# controller notify address
+ctrlNotifyAddrs = node.normal['crmserver']['args']['notifyAddrs']
+ctrlAddr = ctrlNotifyAddrs.split(':')[0]
+# access API address
+accessApiAddr = ctrlAddr + ':41001'
+
 services = ['crmserver', 'shepherd']
 services.each { |service|
+  # Set ES_SERVER_URLS environment variable
   deployTag = node.normal[service]['args']['deploymentTag']
   unless node.normal[service]['env'].any? { |s| s.include?('ES_SERVER_URLS') }
     Chef::Log.info("Setting ES_SERVER_URLS env var for #{service} belonging to #{deployTag} setup...")
@@ -9,5 +15,11 @@ services.each { |service|
     else
       node.normal[service]['env'].append("ES_SERVER_URLS=https://events-#{deployTag}.es.mobiledgex.net/")
     end
+  end
+
+  # Set accessApiAddr attribute for all the services
+  unless node.normal[service]['args'].any? { |s| s.include?('accessApiAddr') }
+    Chef::Log.info("Setting accessApiAddr attribute var for #{service} to #{accessApiAddr}...")
+    node.normal[service]['args']['accessApiAddr'] = accessApiAddr
   end
 }

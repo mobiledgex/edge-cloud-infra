e4309d95987d73cd5d7f1f1626e404fbf0552964
Lev Shvarts <lev@mobiledgex.com>
2020-12-04 12:53:30 -0800

    Fixed alert Description Annotations

--- shepherd/cloudlet-prom.go
+++ shepherd/cloudlet-prom.go
@@ -58,7 +58,7 @@ var promHealthCheckAlerts = `groups:
       ` + cloudcommon.AlertScopeTypeTag + ": " + cloudcommon.AlertScopeApp + `
     annotations:
       ` + cloudcommon.AlertAnnotationTitle + ": " + cloudcommon.AlertAppInstDown + `
-      ` + cloudcommon.AlertAnnotationDescription + ": Application server port is not responding" + `
+      ` + cloudcommon.AlertAnnotationDescription + ": Root Load Balancer is not responding" + `
   - alert: ` + cloudcommon.AlertAppInstDown + `
     expr: envoy_cluster_health_check_healthy == 0
     labels:
@@ -66,7 +66,7 @@ var promHealthCheckAlerts = `groups:
       ` + cloudcommon.AlertScopeTypeTag + ": " + cloudcommon.AlertScopeApp + `
     annotations:
       ` + cloudcommon.AlertAnnotationTitle + ": " + cloudcommon.AlertAppInstDown + `
-      ` + cloudcommon.AlertAnnotationDescription + ": Root Load Balancer is not responding."
+      ` + cloudcommon.AlertAnnotationDescription + ": Application server port is not responding"
 
 type targetData struct {
 	MetricsProxyAddr string

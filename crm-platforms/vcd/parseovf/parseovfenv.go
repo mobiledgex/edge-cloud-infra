49df5bb350a1514926b46dc2ba41a697af350031
Jim <jim.morris@mobiledgex.com>
2021-01-28 07:31:33 -0600

    vCD Metadata fix (#1264)
    * metadata fix
    
    * updates to make vcd init work without affecting other platforms
    
    * per review:
    - remove obsolete function getValue
    - add error checking when reading vmware guestinfo
    
    * also add error check for parseovfenv

--- crm-platforms/vcd/parseovf/parseovfenv.go
+++ crm-platforms/vcd/parseovf/parseovfenv.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"encoding/xml"
 	"flag"
@@ -9,33 +8,8 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
-
-	"github.com/vmware/go-vcloud-director/v2/types/v56"
 )
 
-// Parse the result of vcd's ProductSection ovfenv produced via
-// vmtoolsd --cmd "info-get guestinfo.ovfenv"
-// and just write to stdout as mobiledgex-init will
-// send to output to the existing /mnt/mobiledgex/openstack/latest/meta-data.json file
-//
-func getValue(line string) string {
-	value := strings.SplitAfter(line, "value=")
-	if len(value) > 0 {
-		val := value[1]
-		v := strings.Split(val, "/")
-		s := v[0]
-		if len(s) > 1 {
-			s = s[1 : len(s)-1]
-			return s
-		} else {
-			return ""
-		}
-	}
-	return ""
-}
-
-// XXX This still uses the Contains operation, which can match unwanted tokens
-// Revisit
 func main() {
 	// default read from /var/log but use -ovf foo.txt to override
 	ovfile := flag.String("ovf", "/var/log/userdata.log", "default ovfenv file")
@@ -46,7 +20,7 @@ func main() {
 	envars := map[string]string{
 		"ROLE":           "",
 		"SKIPK8S":        "",
-		"MASTERADDR":     "",
+		"K8SMASTER":      "",
 		"HOSTNAME":       "",
 		"UPDATE":         "",
 		"SKIPINIT":       "",
@@ -56,9 +30,15 @@ func main() {
 		"NETMASK":        "",
 		"NETTYPE":        "",
 	}
-
+	type EnvProperty struct {
+		Key   string `xml:"key,attr"`
+		Value string `xml:"value,attr"`
+	}
+	type PropertySection struct {
+		Properties []EnvProperty `xml:"Property"`
+	}
 	type Environment struct {
-		PropertySection types.ProductSection `xml:"PropertySection,omitempty"`
+		Property *PropertySection `xml:"PropertySection"`
 	}
 	info := Environment{}
 
@@ -80,29 +60,19 @@ func main() {
 		fmt.Printf("Error unmarshal: %s\n", err.Error())
 		os.Exit(-1)
 	}
-	fmt.Printf("Unmarshalled: %+v\n", info.PropertySection)
-
-	scanner := bufio.NewScanner(ovf)
-	if err != nil {
-		fmt.Printf("could not create scanner! :%s\n", err.Error())
-		os.Exit(1)
-	}
-	vars := make(map[string]interface{}) // Jon alt.
-	for scanner.Scan() {
-		nextline := scanner.Text()
-		for key, _ := range envars {
-			if strings.Contains(nextline, key) {
-				value := getValue(nextline)
-				vars[strings.ToLower(key)] = value // Jon's alt. add to vars map
-			}
+	vars := make(map[string]interface{})
+	for _, p := range info.Property.Properties {
+		_, ok := envars[strings.ToUpper(p.Key)]
+		if ok {
+			vars[p.Key] = p.Value
 		}
 	}
-	metadata := map[string]interface{}{ // Jons impl
-		"meta": vars, // jon
+	metadata := map[string]interface{}{
+		"meta": vars,
 	}
 	out, err := json.Marshal(metadata)
 	if err != nil {
-		fmt.Printf("Error marsheling meta data %s\n", err.Error())
+		fmt.Printf("Error marshalling meta data %s\n", err.Error())
 		os.Exit(-1)
 	}
 	fmt.Printf("%s", string(out))

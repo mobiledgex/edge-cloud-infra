dcee14f534f8ebab1fc3b4e340496299d39eebff
Jim <jim.morris@mobiledgex.com>
2021-02-18 08:36:09 -0600

    prep work (#1326)

---
+++ infracommon/common-ssh.go
@@ -0,0 +1,140 @@
+package infracommon
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/mobiledgex/edge-cloud/cloud-resource-manager/platform"
+	"github.com/mobiledgex/edge-cloud/cloud-resource-manager/platform/pc"
+	"github.com/mobiledgex/edge-cloud/log"
+	ssh "github.com/mobiledgex/golang-ssh"
+	"github.com/tmc/scp"
+)
+
+var CloudletSSHKeyRefreshInterval = 24 * time.Hour
+
+func SetCloudletSignedSSHKey(ctx context.Context, accessApi platform.AccessApi, sshKey *CloudletSSHKey) error {
+	log.SpanLog(ctx, log.DebugLevelInfra, "Sign cloudlet public key from Vault")
+
+	signedKey, err := accessApi.SignSSHKey(ctx, sshKey.PublicKey)
+	if err != nil {
+		return err
+	}
+
+	sshKey.Mux.Lock()
+	sshKey.Mux.Unlock()
+	sshKey.SignedPublicKey = signedKey
+
+	return nil
+}
+
+func TriggerRefreshCloudletSSHKeys(sshKey *CloudletSSHKey) {
+	select {
+	case sshKey.RefreshTrigger <- true:
+	default:
+	}
+}
+
+func (cp *CommonPlatform) RefreshCloudletSSHKeys(accessApi platform.AccessApi) {
+	interval := CloudletSSHKeyRefreshInterval
+	for {
+		select {
+		case <-time.After(interval):
+		case <-cp.SshKey.RefreshTrigger:
+		}
+		span := log.StartSpan(log.DebugLevelInfra, "refresh Cloudlet SSH Key")
+		ctx := log.ContextWithSpan(context.Background(), span)
+		err := SetCloudletSignedSSHKey(ctx, accessApi, &cp.SshKey)
+		if err != nil {
+			log.SpanLog(ctx, log.DebugLevelInfra, "refresh cloudlet ssh key failure", "err", err)
+			// retry again soon
+			interval = time.Hour
+		} else {
+			interval = CloudletSSHKeyRefreshInterval
+		}
+		span.Finish()
+	}
+}
+
+func (cp *CommonPlatform) InitCloudletSSHKeys(ctx context.Context, accessApi platform.AccessApi) error {
+	// Generate Cloudlet SSH Keys
+	cloudletPubKey, cloudletPrivKey, err := ssh.GenKeyPair()
+	if err != nil {
+		return fmt.Errorf("failed to generate cloudlet SSH key pair: %v", err)
+	}
+	cp.SshKey.PublicKey = cloudletPubKey
+	cp.SshKey.PrivateKey = cloudletPrivKey
+	err = SetCloudletSignedSSHKey(ctx, accessApi, &cp.SshKey)
+	if err != nil {
+		return err
+	}
+	cp.SshKey.RefreshTrigger = make(chan bool, 1)
+	return nil
+}
+
+//GetSSHClientFromIPAddr returns ssh client handle for the given IP.
+func (cp *CommonPlatform) GetSSHClientFromIPAddr(ctx context.Context, ipaddr string, ops ...pc.SSHClientOp) (ssh.Client, error) {
+	opts := pc.SSHOptions{Timeout: DefaultConnectTimeout, User: SSHUser}
+	opts.Apply(ops)
+	var client ssh.Client
+	var err error
+
+	if cp.SshKey.PrivateKey == "" {
+		return nil, fmt.Errorf("missing cloudlet private key")
+	}
+	if cp.SshKey.PrivateKey == "" {
+		return nil, fmt.Errorf("missing cloudlet signed public Key")
+	}
+
+	cp.SshKey.Mux.Lock()
+	auth := ssh.Auth{
+		KeyPairs: []ssh.KeyPair{
+			{
+				PublicRawKey:  []byte(cp.SshKey.SignedPublicKey),
+				PrivateRawKey: []byte(cp.SshKey.PrivateKey),
+			},
+		},
+	}
+	cp.SshKey.Mux.Unlock()
+
+	if cp.SshKey.UseMEXPrivateKey {
+		auth = ssh.Auth{RawKeys: [][]byte{
+			[]byte(cp.SshKey.MEXPrivateKey),
+		}}
+	}
+	gwhost, gwport := cp.Properties.GetCloudletCRMGatewayIPAndPort()
+	if gwhost != "" {
+		// start the client to GW and add the addr as next hop
+		client, err = ssh.NewNativeClient(opts.User, ClientVersion, gwhost, gwport, &auth, opts.Timeout, nil)
+		if err != nil {
+			return nil, err
+		}
+		client, err = client.AddHop(ipaddr, 22)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		var err error
+		client, err = ssh.NewNativeClient(SSHUser, ClientVersion, ipaddr, 22, &auth, opts.Timeout, nil)
+		if err != nil {
+			return nil, fmt.Errorf("cannot get ssh client for addr %s, %v", ipaddr, err)
+		}
+	}
+	log.SpanLog(ctx, log.DebugLevelInfra, "Created SSH Client", "ipaddr", ipaddr, "gwhost", gwhost, "timeout", opts.Timeout)
+	return client, nil
+}
+
+func SCPFilePath(sshClient ssh.Client, srcPath, dstPath string) error {
+	client, ok := sshClient.(*ssh.NativeClient)
+	if !ok {
+		return fmt.Errorf("unable to cast client to native client")
+	}
+	session, sessionInfo, err := client.Session(client.DefaultClientConfig.Timeout)
+	if err != nil {
+		return err
+	}
+	defer sessionInfo.CloseAll()
+	err = scp.CopyPath(srcPath, dstPath, session)
+	return err
+}

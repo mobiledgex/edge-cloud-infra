eff2c11697fdb036ff64978595995010ead86d10
Lev Shvarts <lev.shvarts@gmail.com>
2021-03-09 16:03:50 -0800

    EDGECLOUD-3455  PagerDuty Receiver for Alertmanager notifications (#1298)
    * PagerDuty alert receiver integration
    * e2e harness for pagerduty alerts
    * e2e-tests for pagerduty alert receivers

--- mc/orm/alertmgr/alertmgr.go
+++ mc/orm/alertmgr/alertmgr.go
@@ -361,6 +361,26 @@ func (s *AlertMgrServer) CreateReceiver(ctx context.Context, receiver *ormapi.Al
 			Name:         receiverName,
 			SlackConfigs: []*alertmanager_config.SlackConfig{&slackCfg},
 		}
+	case AlertReceiverTypePagerDuty:
+		pagerDutyCfg := alertmanager_config.PagerdutyConfig{
+			NotifierConfig: notifierCfg,
+			Client:         alertmanagerConfigPagerDutyClient,
+			ClientURL:      alertmanagerConfigSlackTitleLink,
+			Severity:       receiver.Severity,
+		}
+		if receiver.PagerDutyApiVersion == "" || receiver.PagerDutyApiVersion == "v2" {
+			pagerDutyCfg.RoutingKey = alertmanager_config.Secret(receiver.PagerDutyIntegrationKey)
+		} else if receiver.PagerDutyApiVersion == "v1" {
+			// Prometheus integration
+			pagerDutyCfg.ServiceKey = alertmanager_config.Secret(receiver.PagerDutyIntegrationKey)
+		} else {
+			return fmt.Errorf("PagerDuty Integration Api version must be \"v1\" or \"v2\"(\"v2\" will be used if not specified)")
+		}
+		rec = alertmanager_config.Receiver{
+			// to make the name unique - construct it with all the fields and username
+			Name:             receiverName,
+			PagerdutyConfigs: []*alertmanager_config.PagerdutyConfig{&pagerDutyCfg},
+		}
 	default:
 		log.SpanLog(ctx, log.DebugLevelInfo, "Unsupported receiver type", "type", receiver.Type,
 			"receiver", receiver)
@@ -487,7 +507,15 @@ func (s *AlertMgrServer) ShowReceivers(ctx context.Context, filter *ormapi.Alert
 			receiver.Email = rec.Receiver.EmailConfigs[0].To
 		case AlertReceiverTypeSlack:
 			receiver.SlackChannel = rec.Receiver.SlackConfigs[0].Channel
-			receiver.SlackWebhook = AlertMgrSlackWebhookToken
+			receiver.SlackWebhook = AlertMgrDisplayHidden
+		case AlertReceiverTypePagerDuty:
+			if rec.Receiver.PagerdutyConfigs[0].ServiceKey != "" {
+				receiver.PagerDutyApiVersion = "v1"
+				receiver.PagerDutyIntegrationKey = AlertMgrDisplayHidden
+			} else {
+				receiver.PagerDutyApiVersion = "v2"
+				receiver.PagerDutyIntegrationKey = AlertMgrDisplayHidden
+			}
 		default:
 			log.SpanLog(ctx, log.DebugLevelApi, "Unknown receiver type", "type", receiver.Type)
 		}

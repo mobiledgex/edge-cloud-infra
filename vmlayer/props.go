063f2397a1038fc6184fa85ac4d63b1b9aecbe09
Venky Tumkur <venky.tumkur@mobiledgex.com>
2021-04-19 14:48:16 +0200

    Bump up base image version

--- vmlayer/props.go
+++ vmlayer/props.go
@@ -33,7 +33,7 @@ type VMProperties struct {
 var ImageFormatQcow2 = "qcow2"
 var ImageFormatVmdk = "vmdk"
 
-var MEXInfraVersion = "4.3.2"
+var MEXInfraVersion = "4.3.4"
 var ImageNamePrefix = "mobiledgex-v"
 var DefaultOSImageName = ImageNamePrefix + MEXInfraVersion
 

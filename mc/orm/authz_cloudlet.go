edd33662415c3adcf4a448e9dfe46a21ef168dfc
Matthew Chu <matthew.chu@mobiledgex.com>
2021-06-04 18:43:55 -0400

    please

--- mc/orm/authz_cloudlet.go
+++ mc/orm/authz_cloudlet.go
@@ -158,7 +158,7 @@ func (s *AuthzCloudlet) populate(ctx context.Context, region, username, orgfilte
 					return echo.ErrForbidden
 				}
 			} else {
-				return fmt.Errorf("Billing Org must be set up to deploy to public cloudlets, contact MobiledgeX support")
+				return fmt.Errorf("Billing Org must be set up to deploy to public cloudlets, please contact MobiledgeX support")
 			}
 		}
 	}

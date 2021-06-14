85b38a975d786d711e91e1e4499201f827dc1dc5
Ashish Jain <ashish.jain@mobiledgex.com>
2021-06-14 18:44:41 +0530

    fix required args

--- mc/mcctl/ormctl/cloudlet.mc2.go
+++ mc/mcctl/ormctl/cloudlet.mc2.go
@@ -201,12 +201,12 @@ var AddGPUDriverBuildRequiredArgs = []string{
 	"build.name",
 	"build.driverpath",
 	"build.operatingsystem",
+	"build.md5sum",
 }
 var AddGPUDriverBuildOptionalArgs = []string{
 	"build.driverpathcreds",
 	"build.kernelversion",
 	"build.hypervisorinfo",
-	"build.md5sum",
 	"ignorestate",
 }
 var RemoveGPUDriverBuildRequiredArgs = []string{

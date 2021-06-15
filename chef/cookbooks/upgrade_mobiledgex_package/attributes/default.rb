06cb1b2f7aac736cc7bf7f9e6fdbe4e54819f468
Ashish Jain <ashish.jain@mobiledgex.com>
2021-06-15 22:18:02 +0530

    EDGECLOUD-5133 Failed to acquire VGPU license after cluster instance comes up (#1567)

--- chef/cookbooks/upgrade_mobiledgex_package/attributes/default.rb
+++ chef/cookbooks/upgrade_mobiledgex_package/attributes/default.rb
@@ -1,5 +1,5 @@
 # MobiledgeX package version
-defaultMobiledgeXPackageVersion = '4.4.2'
+defaultMobiledgeXPackageVersion = '4.4.3'
 
 if node.normal['tags'].include?('vmtype/rootlb')
   if !node.attribute?("mobiledgeXPackageVersion") || node.normal["mobiledgeXPackageVersion"] != defaultMobiledgeXPackageVersion

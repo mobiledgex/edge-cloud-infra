2179bf0e1a63a8269a60945018ad9df05d2c4e3e
mchu1195 <matthew.chu@mobiledgex.com>
2021-06-15 08:12:04 -0700

    EDGECLOUD-5108/5109: updated component naming for chargify (#1556)

--- billing/chargify/usage.go
+++ billing/chargify/usage.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/mobiledgex/edge-cloud-infra/billing"
@@ -12,39 +13,62 @@ import (
 	"github.com/mobiledgex/edge-cloud/edgeproto"
 )
 
-func (bs *BillingService) RecordUsage(ctx context.Context, account *ormapi.AccountInfo, usageRecords []billing.UsageRecord) error {
+var dedicatedLB = "dedicatedLB"
+
+func (bs *BillingService) RecordUsage(ctx context.Context, region string, account *ormapi.AccountInfo, usageRecords []billing.UsageRecord) error {
 	for _, record := range usageRecords {
 		var memo string
 		var cloudlet *edgeproto.CloudletKey
+		// chargify memo does not like '<' and '>' chars, so replace them with brackets
+		replacer := strings.NewReplacer(
+			"<", "{",
+			">", "}")
 		if record.AppInst == nil && record.ClusterInst == nil {
 			return fmt.Errorf("invalid usage record, either appinstkey or clusterinstkey must be specified")
 		} else if record.AppInst == nil {
 			cloudlet = &record.ClusterInst.CloudletKey
-			memo = fmt.Sprintf("{%s}, Flavor: %s, NumNodes %d, start: %s, end %s", record.ClusterInst.String(), record.FlavorName, record.NodeCount, record.StartTime.UTC().Format(time.RFC3339), record.EndTime.UTC().Format(time.RFC3339))
+			clusterStr := replacer.Replace(record.ClusterInst.String())
+			memo = fmt.Sprintf("{%s}, Flavor: %s, NumNodes %d, start: %s, end %s", clusterStr, record.FlavorName, record.NodeCount, record.StartTime.UTC().Format(time.RFC3339), record.EndTime.UTC().Format(time.RFC3339))
 		} else { //record.ClusterInst == nil
 			cloudlet = &record.AppInst.ClusterInstKey.CloudletKey
-			memo = fmt.Sprintf("{%s}, Flavor: %s, start: %s, end %s", record.AppInst.String(), record.FlavorName, record.StartTime.UTC().Format(time.RFC3339), record.EndTime.UTC().Format(time.RFC3339))
+			appStr := replacer.Replace(record.AppInst.String())
+			memo = fmt.Sprintf("{%s}, Flavor: %s, start: %s, end %s", appStr, record.FlavorName, record.StartTime.UTC().Format(time.RFC3339), record.EndTime.UTC().Format(time.RFC3339))
 		}
-		componentId := getComponentCode(record.FlavorName, cloudlet, record.StartTime, record.EndTime)
+		// in docker, nodeCount isn't used, but we can't have multiplication by 0, and we dont want to show a nodecount of 0 in the memo either
+		if record.NodeCount == 0 {
+			record.NodeCount = 1
+		}
+		componentId := getComponentCode(record.FlavorName, region, cloudlet, record.StartTime, record.EndTime)
 		endpoint := "/subscriptions/" + account.SubscriptionId + "/components/" + componentId + "/usages.json"
 
-		nodeCount := record.NodeCount
-		// in docker, nodeCount isn't used, but we can't have multiplication by 0
-		if nodeCount == 0 {
-			nodeCount = 1
-		}
-		duration := int(record.EndTime.Sub(record.StartTime).Minutes() * float64(nodeCount))
+		singleNodeDuration := int(record.EndTime.Sub(record.StartTime).Minutes())
 		newUsage := Usage{
-			Quantity: duration,
+			Quantity: singleNodeDuration * record.NodeCount,
 			Memo:     memo,
 		}
 		resp, err := newChargifyReq("POST", endpoint, UsageWrapper{Usage: &newUsage})
 		if err != nil {
 			return fmt.Errorf("Error sending request: %v\n", err)
 		}
+		defer resp.Body.Close()
 		if resp.StatusCode != http.StatusOK {
-			defer resp.Body.Close()
 			return infracommon.GetReqErr(resp.Body)
+		}
+		if record.IpAccess == edgeproto.IpAccess_IP_ACCESS_DEDICATED.String() {
+			lbUsage := Usage{
+				Quantity: singleNodeDuration,
+				Memo:     memo,
+			}
+			componentId = getComponentCode(dedicatedLB, region, cloudlet, record.StartTime, record.EndTime)
+			endpoint = "/subscriptions/" + account.SubscriptionId + "/components/" + componentId + "/usages.json"
+			lbResp, err := newChargifyReq("POST", endpoint, UsageWrapper{Usage: &lbUsage})
+			if err != nil {
+				return fmt.Errorf("Error sending request: %v\n", err)
+			}
+			defer lbResp.Body.Close()
+			if lbResp.StatusCode != http.StatusOK {
+				return infracommon.GetReqErr(lbResp.Body)
+			}
 		}
 	}
 	return nil

9c3d0d9f513f29fec42d03758f47cdb9544d1861
matt williams <matt.williams@mobiledgex.com>
2021-09-07 10:22:14 -0700

    EDGECLOUD-5433:Address e2e tests for new settings value

--- mc/mcctl/ormctl/settings.mc2.go
+++ mc/mcctl/ormctl/settings.mc2.go
@@ -120,6 +120,7 @@ var SettingsOptionalArgs = []string{
 	"disableratelimit",
 	"maxnumperipratelimiters",
 	"resourcesnapshotthreadinterval",
+	"flavorrefreshthreadinterval",
 }
 var SettingsAliasArgs = []string{
 	"fields=settings.fields",
@@ -163,6 +164,7 @@ var SettingsAliasArgs = []string{
 	"disableratelimit=settings.disableratelimit",
 	"maxnumperipratelimiters=settings.maxnumperipratelimiters",
 	"resourcesnapshotthreadinterval=settings.resourcesnapshotthreadinterval",
+	"flavorrefreshthreadinterval=settings.flavorrefreshthreadinterval",
 }
 var SettingsComments = map[string]string{
 	"fields":                                                      "Fields are used for the Update API to specify which fields to apply",
@@ -206,6 +208,7 @@ var SettingsComments = map[string]string{
 	"disableratelimit":                                                  "Disable rate limiting for APIs (default is false)",
 	"maxnumperipratelimiters":                                           "Maximum number of perip rate limiters for an endpoint (ie. number of ips stored to rate limit)",
 	"resourcesnapshotthreadinterval":                                    "ResourceSnapshot Refresh thread run interval",
+	"flavorrefreshthreadinterval":                                       "Flavor refresh thread cadence",
 }
 var SettingsSpecialArgs = map[string]string{
 	"settings.fields": "StringArray",

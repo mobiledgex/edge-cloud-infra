3439af13021fcbe085d3943da890e542caa460f8
Venky <venky.tumkur@mobiledgex.com>
2021-09-27 14:53:12 +0200

    Switch to component-specific docker images (#1757)
    See edge-cloud commit 97a3e14
    PR: https://github.com/mobiledgex/edge-cloud/pull/1499

--- infracommon/common-props.go
+++ infracommon/common-props.go
@@ -17,7 +17,7 @@ import (
 // Ansible should always specify the correct ones to the controller.
 // These are not used if running the CRM manually, because these are only
 // used by CreateCloudlet to set up the CRM VM and container.
-var DefaultContainerRegistryPath = "registry.mobiledgex.net:5000/mobiledgex/edge-cloud"
+var DefaultContainerRegistryPath = "registry.mobiledgex.net:5000/mobiledgex/edge-cloud-crm"
 
 // Cloudlet Infra Common Properties
 var InfraCommonProps = map[string]*edgeproto.PropertyInfo{

62241b29b4a2eea980037873573f51989fced9e1
Venky Tumkur <venky.tumkur@mobiledgex.com>
2021-10-08 15:29:22 +0200

    Upgrade base image in root LBs
    Pre-4.7.0 base images are unable to talk to our APT/Artifactory
    servers because of the expired Let's Encrypt cert in the CA store.
    Handle this by turning off cert validation in pre-4.7.0 before
    attempting to do an upgrade of the mobiledgex package.

--- chef/cookbooks/upgrade_mobiledgex_package/attributes/default.rb
+++ chef/cookbooks/upgrade_mobiledgex_package/attributes/default.rb
@@ -1,5 +1,14 @@
 # MobiledgeX package version
-defaultMobiledgeXPackageVersion = '4.5.0'
+defaultMobiledgeXPackageVersion = '4.7.0'
+
+node.default["aptCertValidation"] = false
+if node.attribute?("mobiledgeXPackageVersion")
+  curSemver = Gem::Version.new(node["mobiledgeXPackageVersion"])
+  certValSemver = Gem::Version.new("4.7.0")
+  if curSemver >= certValSemver
+    node.default["aptCertValidation"] = true
+  end
+end
 
 if node.normal['tags'].include?('vmtype/rootlb')
   if !node.attribute?("mobiledgeXPackageVersion") || node.normal["mobiledgeXPackageVersion"] != defaultMobiledgeXPackageVersion

79a5dabfd341346aec986a2f1e0de8284f0a698d
Devdatta <84735424+devdatta-MobiledgeX@users.noreply.github.com>
2021-11-02 22:07:28 +0530

    EDGECLOUD-5767:  vSphere CRM fails to initialized and panics trying to add IptablesRules (#1801)
    * EDGECLOUD-5767: nil pointer checks before deereferencing OutboundSecurityRules
    
    * EDGECLOUD-5767: Even if no TP, it should still call SetupIptablesRulesForRootLB for the ssh
    
    * EDGECLOUD-5767: PR comments : removed setting unnecessary explicit nil
    
    Co-authored-by: Devdatta <devdatta@mobiledgex.com>

--- crm-platforms/vsphere/vsphere-security.go
+++ crm-platforms/vsphere/vsphere-security.go
@@ -32,7 +32,12 @@ func (v *VSpherePlatform) PrepareRootLB(ctx context.Context, client ssh.Client, 
 	// allow our external vsphere network
 	sshCidrsAllowed := []string{infracommon.RemoteCidrAll}
 	isTrustPolicy := true
-	return v.vmProperties.SetupIptablesRulesForRootLB(ctx, client, sshCidrsAllowed, isTrustPolicy, infracommon.TrustPolicySecGrpNameLabel, TrustPolicy.OutboundSecurityRules)
+
+	var rules []edgeproto.SecurityRule
+	if TrustPolicy != nil {
+		rules = TrustPolicy.OutboundSecurityRules
+	}
+	return v.vmProperties.SetupIptablesRulesForRootLB(ctx, client, sshCidrsAllowed, isTrustPolicy, infracommon.TrustPolicySecGrpNameLabel, rules)
 }
 
 func (o *VSpherePlatform) ConfigureTrustPolicyExceptionSecurityRules(ctx context.Context, TrustPolicyException *edgeproto.TrustPolicyException, rootLbClients map[string]ssh.Client, action vmlayer.ActionType, updateCallback edgeproto.CacheUpdateCallback) error {

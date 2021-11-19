mobiledgex/edge-cloud-infra

commit 3372b56187635790faa48736ab754b25d075ec18

EDGECLOUD-5775  shepherd not starting on anthos (#1830)
* New shepherd k8s bare metal platform
* Cloudlet and ip usage metrics for bare metal k8s in shepherd based on prometheus running on the cluster.

shepherd/shepherd_platform/shepherd_k8sbm/shepherd_k8sbm.go
@@ -1,0 +1,188 @@
+package shepherd_k8sbm
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	k8sbm "github.com/mobiledgex/edge-cloud-infra/crm-platforms/k8s-baremetal"
+	"github.com/mobiledgex/edge-cloud-infra/infracommon"
+	"github.com/mobiledgex/edge-cloud-infra/promutils"
+	"github.com/mobiledgex/edge-cloud-infra/shepherd/shepherd_common"
+	"github.com/mobiledgex/edge-cloud/cloud-resource-manager/platform"
+	"github.com/mobiledgex/edge-cloud/edgeproto"
+	"github.com/mobiledgex/edge-cloud/log"
+	ssh "github.com/mobiledgex/golang-ssh"
+)
+
+const K8sBareMetalStatsCollectionInterval = 1 * time.Minute
+
+var caches *platform.Caches
+
+type ShepherdPlatform struct {
+	Pf             *k8sbm.K8sBareMetalPlatform
+	platformConfig *platform.PlatformConfig
+	promAddr       string
+	client         ssh.Client
+}
+
+func (s *ShepherdPlatform) Init(ctx context.Context, pc *platform.PlatformConfig, platformCaches *platform.Caches) error {
+	s.platformConfig = pc
+	return s.Pf.Init(ctx, pc, caches, nil)
+}
+
+func (s *ShepherdPlatform) SetVMPool(ctx context.Context, vmPool *edgeproto.VMPool) {
+	log.SpanLog(ctx, log.DebugLevelMetrics, "unsupported function for bare-metal k8s")
+}
+
+func (s *ShepherdPlatform) GetMetricsCollectInterval() time.Duration {
+	return K8sBareMetalStatsCollectionInterval
+}
+
+func (s *ShepherdPlatform) GetClusterIP(ctx context.Context, clusterInst *edgeproto.ClusterInst) (string, error) {
+	log.SpanLog(ctx, log.DebugLevelInfo, "no clusterIP for bare-metal k8s")
+	return "", nil
+}
+
+func (s *ShepherdPlatform) GetClusterPlatformClient(ctx context.Context, clusterInst *edgeproto.ClusterInst, clientType string) (ssh.Client, error) {
+	return s.Pf.GetClusterPlatformClient(ctx, clusterInst, clientType)
+}
+
+func (s *ShepherdPlatform) GetVmAppRootLbClient(ctx context.Context, app *edgeproto.AppInstKey) (ssh.Client, error) {
+	log.SpanLog(ctx, log.DebugLevelInfo, "VMs are unsupported on k8sbm")
+	return nil, fmt.Errorf("VMs are unsupported on k8sbm")
+}
+
+func (s *ShepherdPlatform) GetPlatformStats(ctx context.Context) (shepherd_common.CloudletMetrics, error) {
+	log.SpanLog(ctx, log.DebugLevelMetrics, "Platform Stats for bare metal k8s")
+	if s.promAddr == "" || s.client == nil {
+		return shepherd_common.CloudletMetrics{}, fmt.Errorf("Prometheus was not detected on the cluster")
+	}
+
+	cloudletMetrics := shepherd_common.CloudletMetrics{}
+	// Get Cloudlet CPU Max
+	resp, err := promutils.GetPromMetrics(ctx, s.promAddr, promutils.PromQCloudletCpuTotalEncoded, s.client)
+	if err == nil && resp.Status == "success" {
+		for _, metric := range resp.Data.Result {
+			cloudletMetrics.CollectTime = promutils.ParseTime(metric.Values[0].(float64))
+			// copy only if we can parse the value
+			if val, err := strconv.ParseUint(metric.Values[1].(string), 10, 64); err == nil {
+				cloudletMetrics.VCpuMax = val
+				// We should have only one value here
+				break
+			}
+		}
+	}
+	// Get Cloudlet CPU usage
+	if cloudletMetrics.VCpuMax != 0 {
+		resp, err = promutils.GetPromMetrics(ctx, s.promAddr, promutils.PromQCpuClustUrlEncoded, s.client)
+		if err == nil && resp.Status == "success" {
+			for _, metric := range resp.Data.Result {
+				if cloudletMetrics.CollectTime == nil {
+					cloudletMetrics.CollectTime = promutils.ParseTime(metric.Values[0].(float64))
+				}
+				// copy only if we can parse the value
+				if val, err := strconv.ParseFloat(metric.Values[1].(string), 64); err == nil {
+					// from the percentage, figure out number of vcpus used
+					cloudletMetrics.VCpuUsed = uint64(float64(cloudletMetrics.VCpuMax) * val / 100)
+					// Should be at least one cpu used
+					if cloudletMetrics.VCpuUsed == 0 {
+						cloudletMetrics.VCpuUsed = 1
+					}
+					// We should have only one value here
+					break
+				}
+			}
+		}
+	}
+	// Get max mem - in MBs
+	resp, err = promutils.GetPromMetrics(ctx, s.promAddr, promutils.PromQCloudletMemTotalEncoded, s.client)
+	if err == nil && resp.Status == "success" {
+		for _, metric := range resp.Data.Result {
+			cloudletMetrics.CollectTime = promutils.ParseTime(metric.Values[0].(float64))
+			// copy only if we can parse the value
+			if val, err := strconv.ParseUint(metric.Values[1].(string), 10, 64); err == nil {
+				cloudletMetrics.MemMax = uint64(val / (1024 * 1024))
+				// We should have only one value here
+				break
+			}
+		}
+	}
+	// Get mem used - in MBs
+	resp, err = promutils.GetPromMetrics(ctx, s.promAddr, promutils.PromQCloudletMemUseEncoded, s.client)
+	if err == nil && resp.Status == "success" {
+		for _, metric := range resp.Data.Result {
+			if cloudletMetrics.CollectTime == nil {
+				cloudletMetrics.CollectTime = promutils.ParseTime(metric.Values[0].(float64))
+			}
+			// copy only if we can parse the value
+			if val, err := strconv.ParseUint(metric.Values[1].(string), 10, 64); err == nil {
+				cloudletMetrics.MemUsed = uint64(val / (1024 * 1024))
+				// We should have only one value here
+				break
+			}
+		}
+	}
+	// Get fs usage - in GBs
+	resp, err = promutils.GetPromMetrics(ctx, s.promAddr, promutils.PromQCloudletDiskUseEncoded, s.client)
+	if err == nil && resp.Status == "success" {
+		for _, metric := range resp.Data.Result {
+			if cloudletMetrics.CollectTime == nil {
+				cloudletMetrics.CollectTime = promutils.ParseTime(metric.Values[0].(float64))
+			}
+			// copy only if we can parse the value
+			if val, err := strconv.ParseUint(metric.Values[1].(string), 10, 64); err == nil {
+				cloudletMetrics.DiskUsed = uint64(val / (1024 * 1024 * 1024))
+				// We should have only one value here
+				break
+			}
+		}
+	}
+	// In GBs
+	resp, err = promutils.GetPromMetrics(ctx, s.promAddr, promutils.PromQCloudletDiskTotalEncoded, s.client)
+	if err == nil && resp.Status == "success" {
+		for _, metric := range resp.Data.Result {
+			if cloudletMetrics.CollectTime == nil {
+				cloudletMetrics.CollectTime = promutils.ParseTime(metric.Values[0].(float64))
+			}
+			// copy only if we can parse the value
+			if val, err := strconv.ParseUint(metric.Values[1].(string), 10, 64); err == nil {
+				cloudletMetrics.DiskMax = uint64(val / (1024 * 1024 * 1024))
+				// We should have only one value here
+				break
+			}
+		}
+	}
+	// Get IPs range usage
+	externalIps, err := infracommon.ParseIpRanges(s.Pf.GetExternalIpRanges())
+	if err == nil {
+		cloudletMetrics.Ipv4Max = uint64(len(externalIps))
+	}
+	usedIps, err := s.Pf.GetUsedSecondaryIpAddresses(ctx, s.client, s.Pf.GetExternalEthernetInterface())
+	if err == nil {
+		cloudletMetrics.Ipv4Used = uint64(len(usedIps))
+	}
+	return cloudletMetrics, nil
+}
+
+func (s *ShepherdPlatform) GetVmStats(ctx context.Context, key *edgeproto.AppInstKey) (shepherd_common.AppMetrics, error) {
+	log.SpanLog(ctx, log.DebugLevelInfo, "VMs are unsupported for bare metal k8s")
+	return shepherd_common.AppMetrics{}, fmt.Errorf("VMs are unsupported for bare metals k8s")
+}
+
+func (s *ShepherdPlatform) VmAppChangedCallback(ctx context.Context, appInstKey *edgeproto.AppInstKey, newState edgeproto.TrackedState) {
+}
+
+func (s *ShepherdPlatform) SetUsageAccessArgs(ctx context.Context, addr string, client ssh.Client) error {
+	if addr == "" {
+		return fmt.Errorf("Address cannot be empty")
+	}
+	s.promAddr = addr
+	s.client = client
+	return nil
+}
+
+func (s *ShepherdPlatform) IsPlatformLocal(ctx context.Context) bool {
+	return false
+}

promutils/prom-utils.go
@@ -1,6 +1,7 @@
 package promutils
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"math"
@@ -10,6 +11,8 @@ import (
 
 	"github.com/gogo/protobuf/types"
 	"github.com/mobiledgex/edge-cloud/cloudcommon"
+	"github.com/mobiledgex/edge-cloud/log"
+	ssh "github.com/mobiledgex/golang-ssh"
 )
 
 var ClusterPrometheusAppLabel = "label_" + cloudcommon.MexAppNameLabel
@@ -28,6 +31,12 @@ const (
 	PromQUdpSentPktsClust   = "node_netstat_Udp_OutDatagrams"
 	PromQUdpRecvPktsClust   = "node_netstat_Udp_InDatagrams"
 	PromQUdpRecvErr         = "node_netstat_Udp_InErrors"
+
+	PromQCloudletCpuTotal  = "sum(machine_cpu_cores)"
+	PromQCloudletMemUse    = `sum(container_memory_working_set_bytes{id="/"})`
+	PromQCloudletMemTotal  = "sum(machine_memory_bytes)"
+	PromQCloudletDiskUse   = `sum(container_fs_usage_bytes{device=~"^/dev/[sv]d[a-z][1-9]$",id="/"})`
+	PromQCloudletDiskTotal = `sum(container_fs_limit_bytes{device=~"^/dev/[sv]d[a-z][1-9]$",id="/"})`
 
 	// This is a template which takes a pod query and adds instance label to it
 	PromQAppLabelsWrapperFmt = "max(kube_pod_labels%s)by(label_mexAppName,label_mexAppVersion,pod)*on(pod)group_right(label_mexAppName,label_mexAppVersion)(%s)"
@@ -56,6 +65,13 @@ var (
 	PromQUdpSentPktsClustUrlEncoded   = url.QueryEscape(PromQUdpSentPktsClust)
 	PromQUdpRecvPktsClustUrlEncoded   = url.QueryEscape(PromQUdpRecvPktsClust)
 	PromQUdpRecvErrUrlEncoded         = url.QueryEscape(PromQUdpRecvErr)
+
+	// For bare metal k8s CloudletMetrics
+	PromQCloudletCpuTotalEncoded  = url.QueryEscape(PromQCloudletCpuTotal)
+	PromQCloudletMemUseEncoded    = url.QueryEscape(PromQCloudletMemUse)
+	PromQCloudletMemTotalEncoded  = url.QueryEscape(PromQCloudletMemTotal)
+	PromQCloudletDiskUseEncoded   = url.QueryEscape(PromQCloudletDiskUse)
+	PromQCloudletDiskTotalEncoded = url.QueryEscape(PromQCloudletDiskTotal)
 
 	// For Pod metrics we need to join them with k8s pod labels
 	PromQCpuPodUrlEncoded         = url.QueryEscape(GetPromQueryWithK8sLabels(PromLabelsAllMobiledgeXApps, PromQCpuPod))
@@ -136,4 +152,19 @@ func ParseTime(timeFloat float64) *types.Timestamp {
 // Function also takes an optional label filter string of form "{label1="val1",label2="val2",..}"
 func GetPromQueryWithK8sLabels(labelFilter, podQuery string) string {
 	return fmt.Sprintf(PromQAppLabelsWrapperFmt, labelFilter, podQuery)
+}
+
+func GetPromMetrics(ctx context.Context, addr string, query string, client ssh.Client) (*PromResp, error) {
+	reqURI := "'http://" + addr + "/api/v1/query?query=" + query + "'"
+	resp, err := client.Output("curl -s -S " + reqURI)
+	if err != nil {
+		log.ForceLogSpan(log.SpanFromContext(ctx))
+		log.SpanLog(ctx, log.DebugLevelMetrics, "Failed to get prom metrics", "reqURI", reqURI, "err", err, "resp", resp)
+		return nil, err
+	}
+	PromResp := &PromResp{}
+	if err = json.Unmarshal([]byte(resp), PromResp); err != nil {
+		return nil, err
+	}
+	return PromResp, nil
 }

shepherd/cloudlet-worker.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	intprocess "github.com/mobiledgex/edge-cloud-infra/e2e-tests/int-process"
+	"github.com/mobiledgex/edge-cloud-infra/promutils"
 	"github.com/mobiledgex/edge-cloud-infra/shepherd/shepherd_common"
 	"github.com/mobiledgex/edge-cloud/cloud-resource-manager/platform/pc"
 	"github.com/mobiledgex/edge-cloud/edgeproto"
@@ -122,7 +123,7 @@ func getCloudletPrometheusStats(ctx context.Context, addr string, client ssh.Cli
 		}
 		q := "max_over_time(envoy_cluster_upstream_cx_active_total:avg{" + strings.Join(tags, ",") + "}[" + fmt.Sprintf("%d", policy.StabilizationWindowSec) + "s])"
 		q = url.QueryEscape(q)
-		resp, err := getPromMetrics(ctx, addr, q, client)
+		resp, err := promutils.GetPromMetrics(ctx, addr, q, client)
 		if err == nil && resp.Status == "success" {
 			for _, metric := range resp.Data.Result {
 				if val, err := strconv.ParseFloat(metric.Values[1].(string), 64); err == nil {

shepherd/prom-stats.go
@@ -14,22 +14,6 @@ import (
 	ssh "github.com/mobiledgex/golang-ssh"
 )
 
-func getPromMetrics(ctx context.Context, addr string, query string, client ssh.Client) (*promutils.PromResp, error) {
-	// escape the url, since promQL uses some non-compliant characters
-	reqURI := "'http://" + addr + "/api/v1/query?query=" + query + "'"
-	resp, err := client.Output("curl -s -S " + reqURI)
-	if err != nil {
-		log.ForceLogSpan(log.SpanFromContext(ctx))
-		log.SpanLog(ctx, log.DebugLevelMetrics, "Failed to get prom metrics", "reqURI", reqURI, "err", err, "resp", resp)
-		return nil, err
-	}
-	PromResp := &promutils.PromResp{}
-	if err = json.Unmarshal([]byte(resp), PromResp); err != nil {
-		return nil, err
-	}
-	return PromResp, nil
-}
-
 func getPromAlerts(ctx context.Context, addr string, client ssh.Client) ([]edgeproto.Alert, error) {
 	reqURI := "'http://" + addr + "/api/v1/alerts'"
 	out, err := client.Output("curl -s -S " + reqURI)
@@ -88,7 +72,7 @@ func collectAppPrometheusMetrics(ctx context.Context, p *K8sClusterStats) map[sh
 	log.SpanLog(ctx, log.DebugLevelMetrics, "collectAppPrometheusMetrics")
 
 	// Get Pod CPU usage percentage
-	resp, err := getPromMetrics(ctx, p.promAddr, promutils.PromQCpuPodUrlEncoded, p.client)
+	resp, err := promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQCpuPodUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			// skip system pods
@@ -104,7 +88,7 @@ func collectAppPrometheusMetrics(ctx context.Context, p *K8sClusterStats) map[sh
 		}
 	}
 	// Get Pod Mem usage
-	resp, err = getPromMetrics(ctx, p.promAddr, promutils.PromQMemPodUrlEncoded, p.client)
+	resp, err = promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQMemPodUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			// skip system pods
@@ -120,7 +104,7 @@ func collectAppPrometheusMetrics(ctx context.Context, p *K8sClusterStats) map[sh
 		}
 	}
 	// Get Pod Disk usage
-	resp, err = getPromMetrics(ctx, p.promAddr, promutils.PromQDiskPodUrlEncoded, p.client)
+	resp, err = promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQDiskPodUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			// skip system pods
@@ -136,7 +120,7 @@ func collectAppPrometheusMetrics(ctx context.Context, p *K8sClusterStats) map[sh
 		}
 	}
 	// Get Pod NetRecv bytes rate averaged over 1m
-	resp, err = getPromMetrics(ctx, p.promAddr, promutils.PromQNetRecvRateUrlEncoded, p.client)
+	resp, err = promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQNetRecvRateUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			// skip system pods
@@ -152,7 +136,7 @@ func collectAppPrometheusMetrics(ctx context.Context, p *K8sClusterStats) map[sh
 		}
 	}
 	// Get Pod NetRecv bytes rate averaged over 1m
-	resp, err = getPromMetrics(ctx, p.promAddr, promutils.PromQNetSentRateUrlEncoded, p.client)
+	resp, err = promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQNetSentRateUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			// skip system pods
@@ -172,7 +156,7 @@ func collectAppPrometheusMetrics(ctx context.Context, p *K8sClusterStats) map[sh
 
 func collectClusterPrometheusMetrics(ctx context.Context, p *K8sClusterStats) error {
 	// Get Cluster CPU usage
-	resp, err := getPromMetrics(ctx, p.promAddr, promutils.PromQCpuClustUrlEncoded, p.client)
+	resp, err := promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQCpuClustUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			p.CpuTS = promutils.ParseTime(metric.Values[0].(float64))
@@ -185,7 +169,7 @@ func collectClusterPrometheusMetrics(ctx context.Context, p *K8sClusterStats) er
 		}
 	}
 	// Get Cluster Mem usage
-	resp, err = getPromMetrics(ctx, p.promAddr, promutils.PromQMemClustUrlEncoded, p.client)
+	resp, err = promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQMemClustUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			p.MemTS = promutils.ParseTime(metric.Values[0].(float64))
@@ -198,7 +182,7 @@ func collectClusterPrometheusMetrics(ctx context.Context, p *K8sClusterStats) er
 		}
 	}
 	// Get Cluster Disk usage percentage
-	resp, err = getPromMetrics(ctx, p.promAddr, promutils.PromQDiskClustUrlEncoded, p.client)
+	resp, err = promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQDiskClustUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			p.DiskTS = promutils.ParseTime(metric.Values[0].(float64))
@@ -211,7 +195,7 @@ func collectClusterPrometheusMetrics(ctx context.Context, p *K8sClusterStats) er
 		}
 	}
 	// Get Cluster NetRecv bytes rate averaged over 1m
-	resp, err = getPromMetrics(ctx, p.promAddr, promutils.PromQRecvBytesRateClustUrlEncoded, p.client)
+	resp, err = promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQRecvBytesRateClustUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			p.NetRecvTS = promutils.ParseTime(metric.Values[0].(float64))
@@ -224,7 +208,7 @@ func collectClusterPrometheusMetrics(ctx context.Context, p *K8sClusterStats) er
 		}
 	}
 	// Get Cluster NetSent bytes rate averaged over 1m
-	resp, err = getPromMetrics(ctx, p.promAddr, promutils.PromQSentBytesRateClustUrlEncoded, p.client)
+	resp, err = promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQSentBytesRateClustUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			p.NetSentTS = promutils.ParseTime(metric.Values[0].(float64))
@@ -238,7 +222,7 @@ func collectClusterPrometheusMetrics(ctx context.Context, p *K8sClusterStats) er
 	}
 
 	// Get Cluster Established TCP connections
-	resp, err = getPromMetrics(ctx, p.promAddr, promutils.PromQTcpConnClustUrlEncoded, p.client)
+	resp, err = promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQTcpConnClustUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			p.TcpConnsTS = promutils.ParseTime(metric.Values[0].(float64))
@@ -251,7 +235,7 @@ func collectClusterPrometheusMetrics(ctx context.Context, p *K8sClusterStats) er
 		}
 	}
 	// Get Cluster TCP retransmissions
-	resp, err = getPromMetrics(ctx, p.promAddr, promutils.PromQTcpRetransClustUrlEncoded, p.client)
+	resp, err = promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQTcpRetransClustUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			p.TcpRetransTS = promutils.ParseTime(metric.Values[0].(float64))
@@ -264,7 +248,7 @@ func collectClusterPrometheusMetrics(ctx context.Context, p *K8sClusterStats) er
 		}
 	}
 	// Get Cluster UDP Sent Datagrams
-	resp, err = getPromMetrics(ctx, p.promAddr, promutils.PromQUdpSentPktsClustUrlEncoded, p.client)
+	resp, err = promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQUdpSentPktsClustUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			p.UdpSentTS = promutils.ParseTime(metric.Values[0].(float64))
@@ -277,7 +261,7 @@ func collectClusterPrometheusMetrics(ctx context.Context, p *K8sClusterStats) er
 		}
 	}
 	// Get Cluster UDP Recv Datagrams
-	resp, err = getPromMetrics(ctx, p.promAddr, promutils.PromQUdpRecvPktsClustUrlEncoded, p.client)
+	resp, err = promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQUdpRecvPktsClustUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			p.UdpRecvTS = promutils.ParseTime(metric.Values[0].(float64))
@@ -290,7 +274,7 @@ func collectClusterPrometheusMetrics(ctx context.Context, p *K8sClusterStats) er
 		}
 	}
 	// Get Cluster UDP Recv Errors
-	resp, err = getPromMetrics(ctx, p.promAddr, promutils.PromQUdpRecvErrUrlEncoded, p.client)
+	resp, err = promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQUdpRecvErrUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			p.UdpRecvErrTS = promutils.ParseTime(metric.Values[0].(float64))
@@ -307,7 +291,7 @@ func collectClusterPrometheusMetrics(ctx context.Context, p *K8sClusterStats) er
 
 func collectClusterAutoScaleMetrics(ctx context.Context, p *K8sClusterStats) error {
 	// Get Stabilized max total worker node cpu utilization
-	resp, err := getPromMetrics(ctx, p.promAddr, promutils.PromQAutoScaleCpuTotalUUrlEncoded, p.client)
+	resp, err := promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQAutoScaleCpuTotalUUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			//copy only if we can parse the value
@@ -319,7 +303,7 @@ func collectClusterAutoScaleMetrics(ctx context.Context, p *K8sClusterStats) err
 		}
 	}
 	// Get Stabilized max total worker node memory utilization
-	resp, err = getPromMetrics(ctx, p.promAddr, promutils.PromQAutoScaleMemTotalUUrlEncoded, p.client)
+	resp, err = promutils.GetPromMetrics(ctx, p.promAddr, promutils.PromQAutoScaleMemTotalUUrlEncoded, p.client)
 	if err == nil && resp.Status == "success" {
 		for _, metric := range resp.Data.Result {
 			//copy only if we can parse the value

shepherd/proxy-stats.go
@@ -123,7 +123,13 @@ func getProxyContainerAndNetworkType(ctx context.Context, scrapePoint ProxyScrap
 	if pfType == "fake" {
 		return "fakeEnvoy", DockerNetworkHost, nil
 	}
-	container := proxy.GetEnvoyContainerName(scrapePoint.App)
+	// for baremetal k8s has a cluster prefix
+	container := ""
+	if pfType == "k8sbaremetal" {
+		container += k8smgmt.GetKconfName(&edgeproto.ClusterInst{Key: scrapePoint.ClusterInstKey})
+	}
+	container += "-" + dockermgmt.GetContainerName(&scrapePoint.Key.AppKey)
+	container = proxy.GetEnvoyContainerName(container)
 	request := fmt.Sprintf("docker exec %s echo hello", container)
 	resp, err := scrapePoint.Client.Output(request)
 	if err != nil && strings.Contains(resp, "No such container") {

shepherd/shepherd-k8s.go
@@ -51,6 +51,9 @@ func (c *K8sClusterStats) GetAppStats(ctx context.Context) map[shepherd_common.M
 			log.SpanLog(ctx, log.DebugLevelMetrics, "error updating UpdatePrometheusAddr", "err", err)
 			return make(map[shepherd_common.MetricAppInstKey]*shepherd_common.AppMetrics)
 		}
+		// Update platform if it depends on the cluster-level metrics
+		log.DebugLog(log.DebugLevelInfo, "Setting prometheus addr", "addr", c.promAddr)
+		myPlatform.SetUsageAccessArgs(ctx, c.promAddr, c.client)
 	}
 	metrics := collectAppPrometheusMetrics(ctx, c)
 	if metrics == nil {

shepherd/shepherd.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	awsec2 "github.com/mobiledgex/edge-cloud-infra/crm-platforms/aws/aws-ec2"
+	k8sbm "github.com/mobiledgex/edge-cloud-infra/crm-platforms/k8s-baremetal"
 	"github.com/mobiledgex/edge-cloud-infra/crm-platforms/openstack"
 	"github.com/mobiledgex/edge-cloud-infra/crm-platforms/vcd"
 	"github.com/mobiledgex/edge-cloud-infra/crm-platforms/vmpool"
@@ -19,6 +20,7 @@ import (
 	"github.com/mobiledgex/edge-cloud-infra/infracommon"
 	platform "github.com/mobiledgex/edge-cloud-infra/shepherd/shepherd_platform"
 	"github.com/mobiledgex/edge-cloud-infra/shepherd/shepherd_platform/shepherd_fake"
+	"github.com/mobiledgex/edge-cloud-infra/shepherd/shepherd_platform/shepherd_k8sbm"
 	"github.com/mobiledgex/edge-cloud-infra/shepherd/shepherd_platform/shepherd_vmprovider"
 	"github.com/mobiledgex/edge-cloud-infra/shepherd/shepherd_platform/shepherd_xind"
 	"github.com/mobiledgex/edge-cloud-infra/version"
@@ -390,6 +392,10 @@ func getPlatform() (platform.Platform, error) {
 		}
 		plat = &shepherd_vmprovider.ShepherdPlatform{
 			VMPlatform: &vmPlatform,
+		}
+	case "PLATFORM_TYPE_K8S_BARE_METAL":
+		plat = &shepherd_k8sbm.ShepherdPlatform{
+			Pf: &k8sbm.K8sBareMetalPlatform{},
 		}
 	case "PLATFORM_TYPE_FAKEINFRA":
 		plat = &shepherd_fake.Platform{}

shepherd/shepherd_platform/shepherd_fake/shepherd_fake.go
@@ -49,6 +49,10 @@ func (s *Platform) GetMetricsCollectInterval() time.Duration {
 	return 0
 }
 
+func (s *Platform) SetUsageAccessArgs(ctx context.Context, addr string, client ssh.Client) error {
+	return nil
+}
+
 func (s *Platform) IsPlatformLocal(ctx context.Context) bool {
 	return true
 }

commit 22d897679b4714177b09152bfa29ce0b078c1962

Fix build (#1831)
Broke the build with #1830 - fixing it

shepherd/shepherd_platform/shepherd_k8sbm/shepherd_k8sbm.go
@@ -29,7 +29,7 @@ type ShepherdPlatform struct {
 
 func (s *ShepherdPlatform) Init(ctx context.Context, pc *platform.PlatformConfig, platformCaches *platform.Caches) error {
 	s.platformConfig = pc
-	return s.Pf.Init(ctx, pc, caches, nil)
+	return s.Pf.Init(ctx, pc, caches, true, edgeproto.DummyUpdateCallback)
 }
 
 func (s *ShepherdPlatform) SetVMPool(ctx context.Context, vmPool *edgeproto.VMPool) {
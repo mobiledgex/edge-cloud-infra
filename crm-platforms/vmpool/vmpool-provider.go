fa7d7f1cb8a72d48001e707a0c96292ef02702bb
Jon <38672162+gainsley@users.noreply.github.com>
2021-11-23 12:47:30 -0800

    EDGECLOUD-5802 URGENT - VM App Deployment can delete cluster for another deployment in Shared IaaS (#1837)

--- crm-platforms/vmpool/vmpool-provider.go
+++ crm-platforms/vmpool/vmpool-provider.go
@@ -597,7 +597,7 @@ func (o *VMPoolPlatform) SyncVMs(ctx context.Context, vmGroupOrchestrationParams
 	return nil
 }
 
-func (s *VMPoolPlatform) GetVMStats(ctx context.Context, key *edgeproto.AppInstKey) (*vmlayer.VMMetrics, error) {
+func (s *VMPoolPlatform) GetVMStats(ctx context.Context, appInst *edgeproto.AppInst) (*vmlayer.VMMetrics, error) {
 	log.SpanLog(ctx, log.DebugLevelMetrics, "GetVMStats not supported")
 	return &vmlayer.VMMetrics{}, nil
 }

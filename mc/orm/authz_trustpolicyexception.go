bad62924f2896cf4033bf1c971a15cf531b1d872
Devdatta <84735424+devdatta-MobiledgeX@users.noreply.github.com>
2021-12-13 23:17:25 +0530

    TrustPolicyException Cli : various bugfixes (#1854)
    * TrustPolicyException Cli : various bugfixes
    
    * authz changes for update and authz unit tests
    
    * e2e-tests related change
    
    Co-authored-by: Devdatta Ajgaonkar <devdatta@mobiledgex.com>

--- mc/orm/authz_trustpolicyexception.go
+++ mc/orm/authz_trustpolicyexception.go
@@ -25,7 +25,7 @@ func (s *AuthzOrgTpe) populate(ctx context.Context, region, username, orgfilter,
 	if err != nil {
 		return err
 	}
-	authDevOrgs, err := enforcer.GetAuthorizedOrgs(ctx, username, ResourceUsers, action)
+	authDevOrgs, err := enforcer.GetAuthorizedOrgs(ctx, username, ResourceApps, action)
 	if err != nil {
 		return err
 	}
@@ -77,7 +77,7 @@ func newAuthzGetOrgsTpe(ctx context.Context, region, username, action string) (*
 	if err != nil {
 		return nil, err
 	}
-	authDevOrgs, err := enforcer.GetAuthorizedOrgs(ctx, username, ResourceUsers, action)
+	authDevOrgs, err := enforcer.GetAuthorizedOrgs(ctx, username, ResourceApps, action)
 	if err != nil {
 		return nil, err
 	}
@@ -96,21 +96,51 @@ func newAuthzGetOrgsTpe(ctx context.Context, region, username, action string) (*
 }
 
 func authzUpdateTrustPolicyException(ctx context.Context, region, username string, tpe *edgeproto.TrustPolicyException, resource, action string) error {
-	if err := tpe.Key.ValidateKey(); err != nil {
-		return err
-	}
-	authz, err := newAuthzGetOrgsTpe(ctx, region, username, action)
 
+	authz, err := newAuthzGetOrgsTpe(ctx, region, username, action)
 	if err != nil {
 		return err
 	}
 
-	if _, found := authz.allowedOperOrgs[tpe.Key.CloudletPoolKey.Organization]; found {
-		// Operator
-		if err := authorized(ctx, username, tpe.Key.CloudletPoolKey.Organization, resource, action); err != nil {
-			return err
+	_, isOper := authz.allowedOperOrgs[tpe.Key.CloudletPoolKey.Organization]
+	_, isDev := authz.allowedDevOrgs[tpe.Key.AppKey.Organization]
+
+	if isOper && isDev {
+		return nil
+	}
+
+	if isOper || authz.allowAll {
+		// Operator/Admin can only update state
+		for _, field := range tpe.Fields {
+			if tpe.IsKeyField(field) {
+				continue
+			}
+			if field != edgeproto.TrustPolicyExceptionFieldState {
+				return fmt.Errorf("Operator can only update state field")
+			}
+		}
+		if tpe.State != edgeproto.TrustPolicyExceptionState_TRUST_POLICY_EXCEPTION_STATE_ACTIVE &&
+			tpe.State != edgeproto.TrustPolicyExceptionState_TRUST_POLICY_EXCEPTION_STATE_REJECTED {
+			return fmt.Errorf("User not allowed to update TrustPolicyException state to %s", tpe.State.String())
 		}
 		return nil
 	}
-	return fmt.Errorf("User not allowed to update TrustPolicyException")
+
+	if isDev {
+		// Developer can not update state
+		for _, field := range tpe.Fields {
+			if tpe.IsKeyField(field) {
+				continue
+			}
+			if field == edgeproto.TrustPolicyExceptionFieldState {
+				if tpe.State == edgeproto.TrustPolicyExceptionState_TRUST_POLICY_EXCEPTION_STATE_ACTIVE ||
+					tpe.State == edgeproto.TrustPolicyExceptionState_TRUST_POLICY_EXCEPTION_STATE_REJECTED {
+					return fmt.Errorf("User not allowed to update TrustPolicyException state to %s", tpe.State.String())
+				}
+			}
+		}
+		return nil
+	}
+
+	return echo.ErrForbidden
 }

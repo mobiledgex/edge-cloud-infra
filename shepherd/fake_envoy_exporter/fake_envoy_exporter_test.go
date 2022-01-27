b743bf53023b2d695e04de1e3240d7e9fbd39e5a
Lev Shvarts <lev.shvarts@gmail.com>
2022-01-27 14:44:11 -0800

    Remove cluster option from fake_envoy_exporter (#1920)
    * fix unit-tests

--- shepherd/fake_envoy_exporter/fake_envoy_exporter_test.go
+++ shepherd/fake_envoy_exporter/fake_envoy_exporter_test.go
@@ -14,34 +14,6 @@ import (
 
 func TestExporter(t *testing.T) {
 	*sockFile = "/tmp/fake_envoy_exporter_unit_test"
-	*cluster = "testclust"
-
-	ch := make(chan bool, 0)
-	run(ch)
-
-	client := &http.Client{
-		Transport: &http.Transport{
-			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
-				return net.Dial("unix", *sockFile)
-			},
-		},
-	}
-
-	ensureMeas(t, client, `envoy_cluster_upstream_cx_active{envoy_cluster_name="testclust"} 50`)
-
-	// update measure
-	params := url.Values{}
-	params.Set("measure", `envoy_cluster_upstream_cx_active{envoy_cluster_name="testclust"}`)
-	params.Set("val", "11")
-	resp, err := client.PostForm("http://unix/setval", params)
-	require.Nil(t, err)
-	require.Equal(t, http.StatusOK, resp.StatusCode)
-	defer resp.Body.Close()
-
-	ensureMeas(t, client, `envoy_cluster_upstream_cx_active{envoy_cluster_name="testclust"} 11`)
-}
-func TestExporterPortArg(t *testing.T) {
-	*sockFile = "/tmp/fake_envoy_exporter_unit_test"
 	portFlags = append(portFlags, "80")
 
 	ch := make(chan bool, 0)

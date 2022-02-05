2f3a41aed4de052b54c290d6af24c132311b2518
Lev Shvarts <lev@mobiledgex.com>
2022-02-04 17:20:25 -0800

    Metrics object validation, alert names renaming and time error change (#1934)
    * EDGECLOUD-6032 cloudletusage metrics should require cloudlets:x.cloudlet-org with cloudlets:x.cloudlet
       - added object validation call that was missing
    * EDGECLOUD-5895 AppInst Health Check fail values should be renamed
       - Autogen files and modifications to cloudlet prom rules for the new names(see main PR in edge-proto: EDGECLOUD-5895 AppInst Health Check fail values should be renamed edge-proto#54)
    * EDGECLOUD-5880 mcctl usage command time format example doesnt work
       - fix error format in unit-tests

--- mc/orm/alertmgr/alertmgr_test.go
+++ mc/orm/alertmgr/alertmgr_test.go
@@ -147,7 +147,7 @@ func TestAlertMgrServer(t *testing.T) {
 		require.Equal(t, "testcloudlet", val)
 		val, found = alert.Labels[cloudcommon.AlertHealthCheckStatus]
 		require.True(t, found)
-		require.Equal(t, dme.HealthCheck_CamelName[int32(dme.HealthCheck_HEALTH_CHECK_FAIL_ROOTLB_OFFLINE)], val)
+		require.Equal(t, dme.HealthCheck_CamelName[int32(dme.HealthCheck_HEALTH_CHECK_ROOTLB_OFFLINE)], val)
 		region, ok := alert.Labels["region"]
 		require.True(t, ok)
 		if ok {

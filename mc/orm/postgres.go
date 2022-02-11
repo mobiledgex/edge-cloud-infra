e45af2580b9a1d73bda68d1e89e5931b7d6b0f48
Lev Shvarts <lev@mobiledgex.com>
2022-02-11 15:57:51 -0800

    Add creation of thanos_metrics to upgradeCustom.

--- mc/orm/postgres.go
+++ mc/orm/postgres.go
@@ -444,6 +444,14 @@ func upgradeCustom(ctx context.Context, db *gorm.DB) error {
 	if res.Error != nil {
 		return res.Error
 	}
+
+	// add Thanos column as empty string to the controller table
+	cmd = `ALTER TABLE IF EXISTS "controllers" ADD IF NOT EXISTS "thanos_metrics" text`
+	res = db.Exec(cmd)
+	if res.Error != nil {
+		return res.Error
+	}
+
 	// change value to unique, desired values
 	ctrls := []ormapi.Controller{}
 	err := db.Find(&ctrls).Error

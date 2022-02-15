b882004cb20a8a329d8bcdc27c45df8d8fd36fc9
Lev Shvarts <lev@mobiledgex.com>
2022-02-14 16:42:41 -0800

    Update dns_region instead of saving the structure

--- mc/orm/postgres.go
+++ mc/orm/postgres.go
@@ -445,13 +445,6 @@ func upgradeCustom(ctx context.Context, db *gorm.DB) error {
 		return res.Error
 	}
 
-	// add Thanos column as empty string to the controller table
-	cmd = `ALTER TABLE IF EXISTS "controllers" ADD IF NOT EXISTS "thanos_metrics" text`
-	res = db.Exec(cmd)
-	if res.Error != nil {
-		return res.Error
-	}
-
 	// change value to unique, desired values
 	ctrls := []ormapi.Controller{}
 	err := db.Find(&ctrls).Error
@@ -472,9 +465,10 @@ func upgradeCustom(ctx context.Context, db *gorm.DB) error {
 				if conflict, found := dnsRegions[ctrl.DnsRegion]; found {
 					return fmt.Errorf("dns region name conflict, regions %s and %s both dns sanitizes to %s, please fix region names", conflict.Region, ctrl.Region, ctrl.DnsRegion)
 				}
-				err = db.Save(&ctrl).Error
-				if err != nil {
-					return err
+				cmd := fmt.Sprintf(`UPDATE "controllers" SET "dns_region" = '%s' WHERE "region" = '%s'`, ctrl.DnsRegion, ctrl.Region)
+				res := db.Exec(cmd)
+				if res.Error != nil {
+					return res.Error
 				}
 			}
 			dnsRegions[ctrl.DnsRegion] = &ctrl

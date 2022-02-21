2753b5324111706e2c59b4a93d380da8ee7f090b
Venky Tumkur <venky.tumkur@mobiledgex.com>
2022-02-21 12:32:06 +0530

    Cleanup using custom resources

--- chef/cookbooks/setup_teleport/recipes/default.rb
+++ chef/cookbooks/setup_teleport/recipes/default.rb
@@ -1,74 +1,15 @@
-if node.attribute?('teleport_token')
+if node.attribute?('teleport_token') and node.attribute['teleport_token'] and node.attribute['teleport_token'].length > 0
 
-    # Install teleport
-    remote_file '/usr/local/bin/teleport' do
-        source 'https://apt:AP2XYr1wBzePUAiKENupjzzB9ki@artifactory.mobiledgex.net/artifactory/downloads/teleport/v8.2.0/teleport'
-        checksum 'ed2e0d6282597a1aa4d8963a2bd3ed9a270d6f177108087eb5b962d249dbdfa5'
-        mode '0755'
-        action :create
-    end
-
-
-    begin
-
-        # Write the teleport initial token to a file
-        file '/etc/teleport.token' do
-            content "#{node.attribute['teleport_token']}"
-            mode '0400'
-            owner 'root'
-            group 'root'
-
-            notifies :restart, 'systemd_unit[teleport.service]', :delayed
-        end
-
-        # Get teleport operator label from cloudlet org
-        cloudletorg = node.normal['tags'].find {|t| t.start_with?('cloudletorg')}.split("/")[1].downcase
-
-        # Set up systemd service
-        systemd_unit 'teleport.service' do
-            content({
-                Unit: {
-                    Description: 'Teleport SSH Service',
-                    After: 'network.target',
-                },
-                Service: {
-                    Type: 'simple',
-                    Restart: 'on-failure',
-                    ExecStart: "/usr/local/bin/teleport start --roles=node --labels=env=ops,operator=#{cloudletorg} --token=/etc/teleport.token --auth-server=teleport.mobiledgex.net:443",
-                    ExecReload: '/bin/kill -HUP $MAINPID',
-                    PIDFile: '/run/teleport.pid',
-                },
-                Install: {
-                    WantedBy: 'multi-user.target',
-                }
-            })
-            action [:create, :enable, :start]
-        end
-
-    rescue => error
-
-        log 'error' do
-            message "Caught error #{error}"
-            level :error
-        end
-
-        # Remove systemd service
-        systemd_unit 'teleport.service' do
-            action [:stop, :disable, :delete]
-        end
-
+    teleport node.name do
+        action          :setup
+        initial_token   node.attribute['teleport_token']
+        operator        node.normal['tags'].find {|t| t.start_with?('cloudletorg')}.split("/")[1].downcase
     end
 
 else
-    # Not a teleport node; clean up stuff
 
-    # Delete teleport file
-    file '/usr/local/bin/teleport' do
-        action :delete
+    teleport node.name do
+        action  :destroy
     end
 
-end
-
-systemd_unit 'teleport.service' do
-    action :nothing
 end

567dbbf06bdcae7973e215fa0136f99208297964
Venky Tumkur <venky.tumkur@mobiledgex.com>
2022-02-22 11:14:44 +0530

    Update all policies to include setup_teleport

--- chef/policyfiles/docker_crm.rb
+++ chef/policyfiles/docker_crm.rb
@@ -7,7 +7,7 @@ name 'docker_crm'
 default_source :chef_server, "https://chef.mobiledgex.net/organizations/mobiledgex"
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'chef_client_updater::default', 'recipe[runstatus_handler]', 'recipe[run_cmd]', 'recipe[setup_infra]', 'recipe[preflight_crm_checks]', 'recipe[run_diagnostics]', 'recipe[setup_services::docker]', 'recipe[set_security_policies]', 'recipe[copy_third_party_notice]'
+run_list 'chef_client_updater::default', 'recipe[runstatus_handler]', 'recipe[run_cmd]', 'recipe[setup_infra]', 'recipe[preflight_crm_checks]', 'recipe[run_diagnostics]', 'recipe[setup_services::docker]', 'recipe[set_security_policies]', 'recipe[setup_teleport]', 'recipe[copy_third_party_notice]'
 
 # Specify a custom source for a single cookbook:
 cookbook 'runstatus_handler', '= 1.0.0'
@@ -19,6 +19,7 @@ cookbook 'run_diagnostics', '= 1.0.0'
 cookbook 'run_cmd', '= 1.0.0'
 cookbook 'chef_client_updater', '= 3.11.0'
 cookbook 'set_security_policies', '= 1.0.0'
+cookbook 'setup_teleport', '= 1.0.0'
 cookbook 'copy_third_party_notice', '= 1.0.1'
 
 # Set chef-client version

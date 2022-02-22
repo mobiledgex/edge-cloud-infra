68d55f91ef3abd52aaa1b64caaf6f0a2d8fc88a4
Jim <jim.morris@mobiledgex.com>
2022-02-22 09:43:47 -0600

    CRM H/A chef changes for master node down (#1968)
    * redis failure: instead of untainting master, add tolerates to redis.
    
    * update coredns toleration

--- chef/cookbooks/setup_services/resources/platform_k8s.rb
+++ chef/cookbooks/setup_services/resources/platform_k8s.rb
@@ -11,6 +11,12 @@ action :prep_cluster do
     returns 0
   end
 
+  execute('patch-coredns, prevent from running on master and add pod anti-affinity') do
+    action :run
+    command 'kubectl patch -n kube-system deployment coredns  -p \'{"spec": {"template": {"spec": {"tolerations": [{"key": "CriticalAddonsOnly"}], "affinity": {"podAntiAffinity": {"requiredDuringSchedulingIgnoredDuringExecution": [{"labelSelector": {"matchExpressions": [{"key": "k8s-app", "operator": "In", "values": ["kube-dns"]}]}, "topologyKey": "kubernetes.io/hostname"}]}}}}}}\' --kubeconfig=/home/ubuntu/.kube/config'
+    returns 0
+  end
+
   execute('Setup docker registry secrets') do
     action :run
     retries 2
@@ -18,15 +24,6 @@ action :prep_cluster do
     regsecrets = data_bag_item('mexsecrets', 'docker_registry')
     Chef::Log.info("Create secret to access #{node['edgeCloudImage']} as user #{regsecrets['mex_docker_username']}")
     command "kubectl create secret docker-registry mexreg-secret --docker-server=#{node['edgeCloudImage']} --docker-username=#{regsecrets['mex_docker_username']} --docker-password=#{regsecrets['mex_docker_password']} --docker-email=mobiledgex.ops@mobiledgex.com --kubeconfig=/home/ubuntu/.kube/config"
-    returns 0
-    ignore_failure true
-  end
-
-  execute('Remove master taint') do
-    action :run
-    retries 2
-    retry_delay 2
-    command 'kubectl taint nodes -l node-role.kubernetes.io/master node-role.kubernetes.io/master:NoSchedule- --kubeconfig=/home/ubuntu/.kube/config'
     returns 0
     ignore_failure true
   end
@@ -98,6 +95,12 @@ action :deploy_simplex_platform do
 end # deploy-simplex-platform
 
 action :deploy_ha_platform do
+  # verify all nodes are still up, no retries at this stage
+  execute("check-k8s-cluster, looking for #{node['k8sNodeCount']} nodes") do
+    action :run
+    command "kubectl get nodes --kubeconfig=/home/ubuntu/.kube/config| grep ' Ready' | wc -l | grep -w #{node['k8sNodeCount']}"
+    returns 0
+  end
 
   # to affect a switchover, delete the primary deployment and re-create
   execute('delete-primary') do
@@ -112,7 +115,7 @@ action :deploy_ha_platform do
     returns 0
   end
 
-  execute('wait-primary') do
+  execute('wait-primary-running') do
     Chef::Log.info('Wait for primary platform pod to come up')
     action :run
     retries 30
@@ -121,9 +124,13 @@ action :deploy_ha_platform do
     returns 0
   end
 
-  chef_sleep('sleep-after-primary') do
-    seconds      30
-    action       :sleep
+  execute('wait-primary-init-done') do
+    Chef::Log.info('Wait for primary platform pod to be ready to become active')
+    action :run
+    retries 30
+    retry_delay 15
+    command 'kubectl logs deployment/platform-primary -c crmserver --kubeconfig=/home/ubuntu/.kube/config | grep "waiting for platform to become active"'
+    returns 0
   end
 
   execute('delete-secondary') do

a4aea06bd43905cf8b74377133df5cd1d2e73fce
Venky <venky.tumkur@mobiledgex.com>
2022-02-23 14:51:15 +0100

    EDGECLOUD-5325 Set up teleport using chef (#1970)
    * Set up teleport service on demand
    
    If the `teleport_token` attribute is set for a node, this cookbooks sets
    up the service using that value as the initial setup token.
    
    * Add a healthcheck service for teleport
    
    - Enable diagnostics in teleport
    - Cronjob to do a healthcheck every five minutes and restart service is
      not ready

---
+++ chef/cookbooks/setup_teleport/recipes/default.rb
@@ -0,0 +1,15 @@
+if node.attribute.fetch('teleport_token', '').length > 0
+
+    teleport node.name do
+        action          :setup
+        initial_token   node.attribute['teleport_token']
+        operator        node.normal['tags'].find {|t| t.start_with?('cloudletorg')}.split("/")[1].downcase
+    end
+
+else
+
+    teleport node.name do
+        action  :destroy
+    end
+
+end

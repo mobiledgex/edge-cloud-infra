c276c65cf64356c0d27c9dce85481c0585f3676c
Bruce Armstrong <41596890+brucearmstrong@users.noreply.github.com>
2022-02-24 10:56:22 -0600

    EDGECLOUD-6151 Add error checking for IP address parameters (#1960)
    * EDGECLOUD-6151 Add error checking for IP address parameters
    
    * Update to only accept IPv4
    
    * Remove e2e-tests and add unit tests

--- operator-api-gw/tdg/tdg.go
+++ operator-api-gw/tdg/tdg.go
@@ -2,6 +2,7 @@ package tdg
 
 import (
 	"context"
+	"net"
 	"net/http"
 
 	"google.golang.org/grpc"
@@ -103,6 +104,10 @@ func (*OperatorApiGw) GetVersionProperties() map[string]string {
 	return version.InfraBuildProps("TDGOperator")
 }
 
+func (o *OperatorApiGw) SetQosSessionsApiKey(key string) {
+	qosSessionsApiKey = key
+}
+
 func (o *OperatorApiGw) CreatePrioritySession(ctx context.Context, req *dme.QosPrioritySessionCreateRequest) (*dme.QosPrioritySessionReply, error) {
 	var reply *dme.QosPrioritySessionReply
 	var err error
@@ -127,6 +132,12 @@ func (o *OperatorApiGw) CreatePrioritySession(ctx context.Context, req *dme.QosP
 	reqBody.ProtocolOut = req.ProtocolOut.String()
 	reqBody.Qos = req.Profile.String()
 	reqBody.Duration = int64(req.SessionDuration)
+	if net.ParseIP(req.IpUserEquipment).To4() == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "Invalid Address for IpUserEquipment: %s", req.IpUserEquipment)
+	}
+	if net.ParseIP(req.IpApplicationServer).To4() == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "Invalid Address for IpApplicationServer: %s", req.IpApplicationServer)
+	}
 	reply, err = sessionsclient.CallTDGQosPriorityAPI(ctx, "", http.MethodPost, o.Servers.QosSesAddr, qosSessionsApiKey, reqBody)
 	return reply, err
 }

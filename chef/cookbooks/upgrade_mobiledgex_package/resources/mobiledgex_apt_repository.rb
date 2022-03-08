cc883589f3874ed6ba466b309a0db171b0fc0612
Venky Tumkur <venky.tumkur@mobiledgex.com>
2022-03-08 14:37:52 +0530

    Use data bags for repo credentials

--- chef/cookbooks/upgrade_mobiledgex_package/resources/mobiledgex_apt_repository.rb
+++ chef/cookbooks/upgrade_mobiledgex_package/resources/mobiledgex_apt_repository.rb
@@ -7,14 +7,10 @@ property :cert_validation, [TrueClass, FalseClass], default: true
 property :main_repo_url, String, name_property: true
 property :main_repo_distribution, String, default: "bionic"
 property :main_repo_components, Array, default: ["main"]
-property :main_repo_username, String, default: "apt"
-property :main_repo_password, String, default: "mobiledgex"
 
 property :artifactory_repo_url, String, default: "https://artifactory.mobiledgex.net/artifactory/packages"
 property :artifactory_repo_distribution, String, default: "cirrus"
 property :artifactory_repo_components, Array, default: ["main"]
-property :artifactory_repo_username, String, default: "apt"
-property :artifactory_repo_password, String, default: "mobiledgex"
 
 # Set up the apt repository
 action :setup do
@@ -38,6 +34,16 @@ action :setup do
         action  :create
     end
 
+    # Set up credentials for apt repositories
+    apt_repos = data_bag('apt_repos').map {|r| data_bag_item('apt_repos', r)}
+    template '/etc/apt/auth.conf.d/mobiledgex.net.conf' do
+        source  "apt-auth.erb"
+        owner   "root"
+        group   "root"
+        mode    "0400"
+        variables(repos: apt_repos)
+    end
+
     # Set up the main apt repository
     apt_repository new_resource.main_repo_distribution do
         uri             new_resource.main_repo_url
@@ -50,26 +56,6 @@ action :setup do
         uri             new_resource.artifactory_repo_url
         distribution    new_resource.artifactory_repo_distribution
         components      new_resource.artifactory_repo_components
-    end
-
-    # Set up credentials for apt repositories
-    template '/etc/apt/auth.conf.d/mobiledgex.net.conf' do
-        source  "apt-auth.erb"
-        owner   "root"
-        group   "root"
-        mode    "0400"
-        variables(
-            repos: [
-                { :url      => new_resource.main_repo_url,
-                  :user     => new_resource.main_repo_username,
-                  :password => new_resource.main_repo_password,
-                },
-                { :url      => new_resource.artifactory_repo_url,
-                  :user     => new_resource.artifactory_repo_username,
-                  :password => new_resource.artifactory_repo_password,
-                },
-            ],
-        )
     end
 
 end

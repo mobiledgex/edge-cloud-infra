47414b10ef4e245c2359e8581839b84beed13a91
Jon Gainsley <jon.gainsley@mobiledgex.com>
2022-03-07 20:53:31 -0800

    EDGECLOUD-6235 Redact unneccessary info from MC API controller show

--- mc/mcctl/ormctl/controller.go
+++ mc/mcctl/ormctl/controller.go
@@ -37,7 +37,7 @@ func init() {
 		Name:         "ShowController",
 		Use:          "show",
 		Short:        "Show regional controllers",
-		OptionalArgs: "region address notifyaddr influxdb",
+		OptionalArgs: "region",
 		Comments:     ormapi.ControllerComments,
 		ReqData:      &ormapi.Controller{},
 		ReplyData:    &[]ormapi.Controller{},

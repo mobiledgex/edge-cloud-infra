83de04831eab424ab2cbc5d272bf60dc69cd04a7
Jim <jim.morris@mobiledgex.com>
2022-03-14 15:20:50 -0500

    vcd vm status (#2029)
    * add vm status
    
    * change bool per review

--- crm-platforms/vcd/vcd.go
+++ crm-platforms/vcd/vcd.go
@@ -18,6 +18,11 @@ import (
 	"github.com/vmware/go-vcloud-director/v2/types/v56"
 )
 
+type Refresh bool
+
+var DoRefresh Refresh = true
+var NoRefresh Refresh = false
+
 // Note regarding govcd SDK:
 // Most all types in govcd are arranged as a containerized type in govcd that compose Client and
 // specific methods, with the business end of the type in types.go. example found in vdc.go:
@@ -314,6 +319,13 @@ func (v *VcdPlatform) GetServerDetail(ctx context.Context, serverName string) (*
 	return v.GetServerDetailWithVdc(ctx, serverName, vdc, vcdClient)
 }
 
+func (v *VcdPlatform) GetVmStatus(ctx context.Context, vm *govcd.VM, refresh Refresh) (string, error) {
+	if refresh == DoRefresh {
+		return vm.GetStatus()
+	} else {
+		return types.VAppStatuses[vm.VM.Status], nil
+	}
+}
 func (v *VcdPlatform) GetServerDetailWithVdc(ctx context.Context, serverName string, vdc *govcd.Vdc, vcdClient *govcd.VCDClient) (*vmlayer.ServerDetail, error) {
 	log.SpanLog(ctx, log.DebugLevelInfra, "GetServerDetailWithVdc", "serverName", serverName)
 

e14bda65dff675d9af8eddb36b6f0d5f4f20a263
Ashish Jain <ashish.jain@mobiledgex.com>
2022-03-14 11:14:01 +0530

    Use internal network for k8s control IP if multiple networks available (#2021)

--- vmlayer/props.go
+++ vmlayer/props.go
@@ -44,7 +44,7 @@ const MINIMUM_VCPUS uint64 = 2
 var ImageFormatQcow2 = "qcow2"
 var ImageFormatVmdk = "vmdk"
 
-var MEXInfraVersion = "4.9.1"
+var MEXInfraVersion = "4.9.2"
 var ImageNamePrefix = "mobiledgex-v"
 var DefaultOSImageName = ImageNamePrefix + MEXInfraVersion
 

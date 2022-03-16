b7c418823e929d3a21b97e253c8387fef930b00e
Venky Tumkur <venky.tumkur@mobiledgex.com>
2022-03-16 22:11:03 +0530

    Pick newest console version for deployments
    Right now, ansible picks the last published console release for
    deployment, which is fine until a branch/hotfix build is released.  At
    this point, deployments end up picking the branch release during
    automatic deployments like the ones QA performs daily.
    
    This change skips branch releases and picks releases from "master" only.

---
+++ ansible/lookup_plugins/github_release.py
@@ -0,0 +1,102 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = """
+lookup: github_release
+short_description: get requested release from github
+description:
+  - Get latest release from requested branch
+  - If no branch specified, get latest release
+options:
+  _terms:
+    description: Github org/repo
+    required: True
+  github_user:
+    description: Github username
+    required: False
+  github_token:
+    description: Github access token
+    required: False
+  deploy_environ:
+    description:
+        - Test
+  branch:
+    description: Branch to pick release from
+    required: False
+"""
+
+EXAMPLES="""
+  - name: Get latest UI release from master branch
+    set_fact:
+      rel_lookup: "{{ lookup('github_release', 'mobiledgex/edge-cloud-ui', branch='master') }}"
+"""
+
+from ansible.errors import AnsibleError, AnsibleParserError
+from ansible.plugins.lookup import LookupBase
+from ansible.utils.display import Display
+
+import json
+
+try:
+    import requests
+    HAS_REQUESTS = True
+    from requests.auth import HTTPBasicAuth
+except ImportError as e:
+    HAS_REQUESTS = False
+
+display = Display()
+
+class LookupModule(LookupBase):
+
+    def get_latest_release(self, repo, auth):
+        url = "https://api.github.com/repos/{0}/releases/latest".format(repo)
+        r = requests.get(url, auth=auth)
+        r.raise_for_status()
+        return r.json().get("tag_name")
+
+    def get_latest_release_in_branch(self, repo, branch, auth):
+        for page in range(1, 10):
+            display.vvvv("Page: {}".format(page))
+            url = "https://api.github.com/repos/{repo}/releases?page={page}".format(
+                repo=repo, page=page)
+            r = requests.get(url, auth=auth)
+            r.raise_for_status()
+            for rel in r.json():
+                if rel["target_commitish"] == branch:
+                    return rel["tag_name"]
+
+        display.v("Could not find release in branch: {0}".format(branch))
+        return None
+
+    def run(self, terms, variables=None, **kwargs):
+        if not HAS_REQUESTS:
+            raise AnsibleError(
+                'python requests package is required for vault lookups')
+
+        if variables is not None:
+            self._templar.available_variables = variables
+        myvars = getattr(self._templar, '_available_variables', {})
+
+        def get_option(v):
+            return kwargs.get(v, myvars.get(v))
+
+        user, token = get_option("github_user"), get_option("github_token")
+        if user and token:
+            auth = HTTPBasicAuth(user, token)
+        else:
+            auth = None
+
+        branch = get_option("branch")
+        if branch:
+            display.vv("Looking for release in branch: {}".format(branch))
+
+        ret = []
+        for repo in terms:
+            display.vvvv("Github repo: {0}".format(repo))
+
+            if branch:
+                ret.append(self.get_latest_release_in_branch(repo, branch, auth))
+            else:
+                ret.append(self.get_latest_release(repo, auth))
+
+        return ret

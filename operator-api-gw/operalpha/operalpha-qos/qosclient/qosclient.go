b5c84445a6a713bd04a83ab7cfaa7ef776a44175
Jonathan Gainsley <gainsley@gmail.com>
2022-04-25 13:05:56 -0700

    operator-gw-api prototype

---
+++ operator-api-gw/operalpha/operalpha-qos/qosclient/qosclient.go
@@ -0,0 +1,162 @@
+// Copyright 2022 MobiledgeX, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package qosclient
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/mobiledgex/edge-cloud-infra/infracommon"
+	operalphaproto "github.com/mobiledgex/edge-cloud-infra/operator-api-gw/operalpha/proto"
+	dme "github.com/mobiledgex/edge-cloud/d-match-engine/dme-proto"
+	"github.com/mobiledgex/edge-cloud/log"
+	edgetls "github.com/mobiledgex/edge-cloud/tls"
+	"github.com/mobiledgex/edge-cloud/vault"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+)
+
+var clientCert = "qosclient.crt"
+var clientKey = "qosclient.key"
+var serverCert = "qosserver.crt"
+
+var nextRequestId int64 = 1
+
+func GetQosCertsFromVault(vaultConfig *vault.Config) error {
+	log.DebugLog(log.DebugLevelDmereq, "GetQosCertsFromVault", "vaultAddr", vaultConfig.Addr)
+
+	certs := []string{clientCert, clientKey, serverCert}
+	for _, cert := range certs {
+
+		certPath := fmt.Sprintf("/secret/data/accounts/operalpha/qosapi/%s", cert)
+		log.DebugLog(log.DebugLevelDmereq, "Fetching Cert", "certPath", certPath)
+		fileName := "/tmp/" + cert
+		err := infracommon.GetVaultDataToFile(vaultConfig, certPath, fileName)
+		if err != nil {
+			return grpc.Errorf(codes.Internal, "Unable to get cert from file: %s, %v", cert, err)
+		}
+	}
+	return nil
+}
+
+func GetQOSPositionFromApiGW(serverUrl string, mreq *dme.QosPositionRequest, qosKpiServer dme.MatchEngineApi_GetQosPositionKpiServer) error {
+	serverCertFile := "/tmp/" + serverCert
+	clientCertFile := "/tmp/" + clientCert
+
+	if mreq.Positions == nil {
+		return grpc.Errorf(codes.InvalidArgument, "No positions requested")
+	}
+
+	// in case the responses come put of order, we need to be able to lookup the GPS coordinate of the request positionid
+	var positionIdToGps = make(map[int64]*dme.Loc)
+
+	log.DebugLog(log.DebugLevelDmereq, "Connecting to QOS API GW", "serverUrl", serverUrl)
+
+	tlsConfig, err := edgetls.GetTLSClientConfig(edgetls.MutualAuthTLS, serverUrl, nil, clientCertFile, serverCertFile, false)
+	if err != nil {
+		return grpc.Errorf(codes.Unavailable, "Unable get TLS Client config: %v", err)
+	}
+
+	transportCreds := credentials.NewTLS(tlsConfig)
+	dialOption := grpc.WithTransportCredentials(transportCreds)
+	conn, err := grpc.Dial(serverUrl, dialOption)
+	if err != nil {
+		return grpc.Errorf(codes.Unavailable, "Unable to connect to API GW: %s, %v", serverUrl, err)
+	}
+	defer conn.Close()
+	ctx := context.TODO()
+	defaultTimestamp := time.Now().Unix() + 1000
+	var request operalphaproto.QoSKPIRequest
+	for _, p := range mreq.Positions {
+		var posreq operalphaproto.PositionKpiRequest
+		posreq.Positionid = p.Positionid
+		if p.GpsLocation == nil {
+			return grpc.Errorf(codes.InvalidArgument, "Missing GPS Location in request")
+		}
+		positionIdToGps[posreq.Positionid] = p.GpsLocation
+		posreq.Latitude = float32(p.GpsLocation.Latitude)
+		posreq.Longitude = float32(p.GpsLocation.Longitude)
+		posreq.Altitude = float32(p.GpsLocation.Altitude)
+		if p.GpsLocation.Timestamp == nil {
+			posreq.Timestamp = defaultTimestamp
+		} else {
+			posreq.Timestamp = p.GpsLocation.Timestamp.Seconds
+		}
+		request.Requests = append(request.Requests, &posreq)
+	}
+	if mreq.BandSelection != nil {
+		request.Bandselection = new(operalphaproto.BandSelection)
+		request.Bandselection.RAT2G = mreq.BandSelection.Rat2G
+		request.Bandselection.RAT3G = mreq.BandSelection.Rat3G
+		request.Bandselection.RAT4G = mreq.BandSelection.Rat4G
+		request.Bandselection.RAT5G = mreq.BandSelection.Rat5G
+	}
+	request.Ltecategory = mreq.LteCategory
+	request.Requestid = nextRequestId
+	nextRequestId++
+
+	log.DebugLog(log.DebugLevelDmereq, "Sending request to API GW", "request", request)
+
+	qosClient := operalphaproto.NewQueryQoSClient(conn)
+	stream, err := qosClient.QueryQoSKPI(ctx, &request)
+	if err != nil {
+		return fmt.Errorf("QueryQoSKPI error: %v", err)
+	}
+	stream.CloseSend()
+	for {
+		// convert the DT format to the MEX format and stream the replies
+		var mreply dme.QosPositionKpiReply
+		res, err := stream.Recv()
+		if err == io.EOF {
+			log.DebugLog(log.DebugLevelDmereq, "EOF received")
+			err = nil
+			break
+		}
+		if err != nil {
+			break
+		}
+		log.DebugLog(log.DebugLevelDmereq, "Recv done", "resultLen", len(res.Results), "err", err)
+
+		for _, r := range res.Results {
+			var qosres dme.QosPositionKpiResult
+			qosres.Positionid = r.Positionid
+			gps, ok := positionIdToGps[qosres.Positionid]
+			if !ok {
+				return grpc.Errorf(codes.Internal, "PositionId %d found in response but not request", qosres.Positionid)
+			}
+			qosres.GpsLocation = gps
+			qosres.UluserthroughputMin = r.UluserthroughputMin
+			qosres.UluserthroughputMax = r.UluserthroughputMax
+			qosres.UluserthroughputAvg = r.UluserthroughputAvg
+			qosres.DluserthroughputMin = r.DluserthroughputMin
+			qosres.DluserthroughputMax = r.DluserthroughputMax
+			qosres.DluserthroughputAvg = r.DluserthroughputAvg
+			qosres.LatencyMin = r.LatencyMin
+			qosres.LatencyMin = r.LatencyMax
+			qosres.LatencyMin = r.LatencyAvg
+
+			mreply.PositionResults = append(mreply.PositionResults, &qosres)
+		}
+		mreply.Status = dme.ReplyStatus_RS_SUCCESS
+		qosKpiServer.Send(&mreply)
+	}
+
+	log.DebugLog(log.DebugLevelDmereq, "Done receiving responses")
+	return err
+
+}
---
+++ operator-api-gw/operalpha/operalpha-sessions/sessionsclient/sessionsclient.go
@@ -0,0 +1,301 @@
+// Copyright 2022 MobiledgeX, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sessionsclient
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	dme "github.com/mobiledgex/edge-cloud/d-match-engine/dme-proto"
+	"github.com/mobiledgex/edge-cloud/log"
+	"github.com/mobiledgex/edge-cloud/vault"
+)
+
+func GetApiKeyFromVault(ctx context.Context, vaultConfig *vault.Config) (string, error) {
+	apiKeyPath := "/secret/data/accounts/operalpha/sessionsapi"
+	log.SpanLog(ctx, log.DebugLevelDmereq, "GetApiKeyFromVault", "vaultAddr", vaultConfig.Addr, "apiKeyPath", apiKeyPath)
+	type ApiKeyData struct {
+		Data string
+	}
+	var apiKeyData ApiKeyData
+	err := vault.GetData(vaultConfig, apiKeyPath, 0, &apiKeyData)
+	if err != nil {
+		return "", fmt.Errorf("unable to fetch QOS API key from vault. err=%v", err)
+	}
+	apiKey := apiKeyData.Data
+
+	return apiKey, err
+}
+
+// From https://staging-portal.hubraum.opsfactory.dev/de/products/617bd0928431ba00019948f4/summary
+type QosSessionCommon struct {
+	Duration              int64  `json:"duration" yaml:"duration"`
+	UeAddr                string `json:"ueAddr" yaml:"ueAddr"`
+	AsAddr                string `json:"asAddr" yaml:"asAddr"`
+	UePorts               string `json:"uePorts" yaml:"uePorts"`
+	AsPorts               string `json:"asPorts" yaml:"asPorts"`
+	ProtocolIn            string `json:"protocolIn" yaml:"protocolIn"`
+	ProtocolOut           string `json:"protocolOut" yaml:"protocolOut"`
+	Qos                   string `json:"qos" yaml:"qos"`
+	NotificationUri       string `json:"notificationUri" yaml:"notificationUri"`
+	NotificationAuthToken string `json:"notificationAuthToken" yaml:"notificationAuthToken"`
+}
+
+type QosSessionRequest struct {
+	QosSessionCommon
+}
+
+type QosSessionResponse struct {
+	QosSessionCommon
+	Id        string `json:"id" yaml:"id"`
+	StartedAt int64  `json:"startedAt" yaml:"startedAt"`
+	ExpiresAt int64  `json:"expiresAt" yaml:"expiresAt"`
+}
+
+// Build and send the request to the OPERALPHA API server
+func sendRequest(ctx context.Context, method string, reqUrl string, apiKey string, body *bytes.Buffer) (int, string, error) {
+	log.SpanLog(ctx, log.DebugLevelDmereq, "Sending to OPERALPHA:", "method", method, "reqUrl:", reqUrl, "body:", body)
+
+	var req *http.Request
+	var err error
+
+	// I'm surprised this is necessary, and surprised that it works.
+	// See https://github.com/golang/go/issues/32897
+	if body == nil {
+		req, err = http.NewRequest(method, reqUrl, nil)
+	} else {
+		req, err = http.NewRequest(method, reqUrl, body)
+	}
+
+	if err != nil {
+		log.WarnLog("error in http.NewRequest", "err", err)
+		return 0, "", err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("Authorization", apiKey)
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.WarnLog("Error in REST call to OPERALPHA QOS session priority service", "error", err)
+		return 0, "", err
+	}
+	defer resp.Body.Close()
+
+	log.SpanLog(ctx, log.DebugLevelDmereq, "Received response", "statusCode:", resp.StatusCode)
+
+	respBytes, resperr := ioutil.ReadAll(resp.Body)
+	if resperr != nil {
+		log.WarnLog("Error reading response body", "resperr", resperr)
+		return 0, "", resperr
+	}
+	respString := string(respBytes)
+	log.SpanLog(ctx, log.DebugLevelDmereq, "Converted response to string")
+
+	return resp.StatusCode, respString, nil
+}
+
+func buildQosUrl(ctx context.Context, profileName string, qosSesAddr string) (string, error) {
+	var priorityType string
+	if profileName == "LOW_LATENCY" { // LOW_LATENCY is the only valid latency profile.
+		priorityType = "latency"
+	} else if strings.HasPrefix(profileName, "THROUGHPUT") {
+		priorityType = "throughput"
+	} else {
+		log.SpanLog(ctx, log.DebugLevelDmereq, "Received invalid value", "profileName", profileName)
+		return "", errors.New("Received invalid profileName " + profileName)
+	}
+	url := fmt.Sprintf("%s/5g-%s/sessions/", qosSesAddr, priorityType) // Inserts either "latency" or "throughput".
+	log.SpanLog(ctx, log.DebugLevelDmereq, "buildQosUrl", "url", url)
+	return url, nil
+}
+
+// CallOPERALPHAQosPriorityAPI REST API client for the OPERALPHA implementation of QOS session priority API.
+// If a matching session ((IPs, Ports, Protcol) is found, and the requested profile is also the same,
+// that session is kept unchanged.
+// If a matching session ((IPs, Ports, Protcol) is found, and the requested profile is different,
+// the existing session is deleted, and a new session is created with the requested profile name.
+func CallOPERALPHAQosPriorityAPI(ctx context.Context, sesId string, method string, qosSesAddr string, apiKey string, reqBody QosSessionRequest) (*dme.QosPrioritySessionReply, error) {
+	reply := new(dme.QosPrioritySessionReply)
+	qos := QosProtoToOperalpha(reqBody.Qos)
+	log.SpanLog(ctx, log.DebugLevelDmereq, "OPERALPHA CallOPERALPHAQosPriorityAPI", "method", method, "qosSesAddr", qosSesAddr, "qos", qos)
+	if qos == "QOS_NO_PRIORITY" {
+		return nil, errors.New("Operation not permitted with profileName " + qos)
+	}
+	reqUrl, err := buildQosUrl(ctx, qos, qosSesAddr)
+	if err != nil {
+		return nil, err
+	}
+	log.SpanLog(ctx, log.DebugLevelDmereq, "Converting QOS Profile name from proto to OPERALPHA", "old", reqBody.Qos, "new", qos)
+	reqBody.Qos = qos
+	log.SpanLog(ctx, log.DebugLevelDmereq, "OPERALPHA CallOPERALPHAQosPriorityAPI", "reqUrl", reqUrl, "reqBody", reqBody)
+	if err != nil {
+		return nil, err
+	}
+	out, err := json.Marshal(reqBody)
+	if err != nil {
+		return nil, err
+	}
+	body := bytes.NewBuffer(out)
+
+	// For DELETE, we need to add the session ID to the URL
+	if method == http.MethodDelete {
+		reqUrl += sesId
+	}
+	status, respBody, err := sendRequest(ctx, method, reqUrl, apiKey, body)
+	if err != nil {
+		return nil, err
+	}
+
+	var qsiResp QosSessionResponse
+
+	if status == http.StatusConflict {
+		log.SpanLog(ctx, log.DebugLevelDmereq, "409 Conflict received")
+		// In this case, the session already exists. Look up the session info and see if it needs updated.
+		// Example respBody:
+		// "Found session 9aa00f58-38f6-4ed9-be8f-d375aad95721 already active until 2021-12-03T21:06:02Z"
+		if strings.HasPrefix(respBody, "Found session") {
+			words := strings.Split(respBody, " ")
+			if len(words) < 3 {
+				return nil, fmt.Errorf(fmt.Sprintf("Could not parse response: %s", respBody))
+			}
+			sessionId := words[2]
+			url := fmt.Sprintf("%s/%s", reqUrl, sessionId)
+			status, respBody, err = sendRequest(ctx, http.MethodGet, url, apiKey, nil)
+			if err != nil {
+				return nil, err
+			}
+			if status == http.StatusOK {
+				respBytes := []byte(respBody)
+				err = json.Unmarshal(respBytes, &qsiResp)
+				if err != nil {
+					log.WarnLog("Error unmarshalling response", "respBytes", respBytes, "err", err)
+					return nil, err
+				}
+				if qsiResp.Qos == reqBody.Qos {
+					log.SpanLog(ctx, log.DebugLevelDmereq, "Requested QOS session already exists. Keeping it.", "qsiResp.Qos", qsiResp.Qos)
+				} else {
+					log.SpanLog(ctx, log.DebugLevelDmereq, "Existing QOS profile doesn't match. Deleting session.")
+					oldQos := qsiResp.Qos
+					url, err := buildQosUrl(ctx, oldQos, qosSesAddr)
+					url = fmt.Sprintf("%s%s", url, sessionId)
+					if err != nil {
+						return nil, err
+					}
+					status, _, err = sendRequest(ctx, http.MethodDelete, url, apiKey, nil)
+					if err != nil {
+						return nil, err
+					}
+					if status == http.StatusNoContent {
+						log.SpanLog(ctx, log.DebugLevelDmereq, "Successfully deleted QOS session")
+					} else {
+						return nil, fmt.Errorf(fmt.Sprintf("Failed to delete existing QOS session: Error code: %d", status))
+					}
+
+					// Send new request to create session with desired QOS profile.
+					body := bytes.NewBuffer(out)
+					status, respBody, err = sendRequest(ctx, method, reqUrl, apiKey, body)
+					log.SpanLog(ctx, log.DebugLevelDmereq, "Result of re-send received")
+
+					if err != nil {
+						return nil, err
+					}
+				}
+			}
+		}
+	}
+
+	// This value of 'status' can be from the initial call, or from the delete/retry attempt.
+	if status == http.StatusCreated || status == http.StatusOK {
+		if status == http.StatusCreated {
+			log.SpanLog(ctx, log.DebugLevelDmereq, "201 Session Created received")
+		} else {
+			log.SpanLog(ctx, log.DebugLevelDmereq, "200 OK received")
+		}
+		respBytes := []byte(respBody)
+		err = json.Unmarshal(respBytes, &qsiResp)
+		if err != nil {
+			log.WarnLog("Error unmarshalling response", "respBytes", respBytes, "err", err)
+			return nil, err
+		}
+		reply.Profile, err = dme.ParseQosSessionProfile(QosOperalphaToProto(qsiResp.Qos))
+		if err != nil {
+			log.WarnLog("Failed to ParseQosSessionProfile", "qsiResp.Qos", qsiResp.Qos, "QosOperalphaToProto(qsiResp.Qos)", QosOperalphaToProto(qsiResp.Qos), "err", err)
+			return nil, err
+		}
+		reply.SessionDuration = uint32(qsiResp.Duration)
+		reply.SessionId = qsiResp.Id
+		reply.StartedAt = uint32(qsiResp.StartedAt)
+		reply.ExpiresAt = uint32(qsiResp.ExpiresAt)
+	} else if status == http.StatusNoContent {
+		log.SpanLog(ctx, log.DebugLevelDmereq, "204 No Content received (session deleted)")
+		// This status will be returned in the reply.
+	} else if status == http.StatusNotFound {
+		log.SpanLog(ctx, log.DebugLevelDmereq, "404 Session not found")
+		// This status will be returned in the reply.
+	} else if status == http.StatusBadRequest {
+		log.SpanLog(ctx, log.DebugLevelDmereq, "400 Bad request")
+		return nil, fmt.Errorf(respBody)
+	} else {
+		log.WarnLog("returning error", "received ", status)
+		return nil, fmt.Errorf(fmt.Sprintf("API call received unknown status: %d", status))
+	}
+
+	reply.HttpStatus = uint32(status)
+	return reply, nil
+}
+
+// Convert from the QOS profile names defined in the proto to those used by DTG.
+func QosProtoToOperalpha(qosProto string) string {
+	switch qosProto {
+	case "QOS_NO_PRIORITY":
+		return "QOS_NO_PRIORITY"
+	case "QOS_LOW_LATENCY":
+		return "LOW_LATENCY"
+	case "QOS_THROUGHPUT_DOWN_S":
+		return "THROUGHPUT_S"
+	case "QOS_THROUGHPUT_DOWN_M":
+		return "THROUGHPUT_M"
+	case "QOS_THROUGHPUT_DOWN_L":
+		return "THROUGHPUT_L"
+	default:
+		return ""
+	}
+}
+
+// Convert from OPERALPHA's QOS profile names to those defined in the proto.
+func QosOperalphaToProto(qosOperalpha string) string {
+	switch qosOperalpha {
+	case "LOW_LATENCY":
+		return "QOS_LOW_LATENCY"
+	case "THROUGHPUT_S":
+		return "QOS_THROUGHPUT_DOWN_S"
+	case "THROUGHPUT_M":
+		return "QOS_THROUGHPUT_DOWN_M"
+	case "THROUGHPUT_L":
+		return "QOS_THROUGHPUT_DOWN_L"
+	default:
+		return ""
+	}
+}
